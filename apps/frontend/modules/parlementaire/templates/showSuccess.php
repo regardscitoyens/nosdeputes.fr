e934aeb69789231e86b866967d7f7216f6ce0525
RouxRC <b.ooghe@gmail.com>
2009-07-07 19:58:49 +0000

    test d'affichages
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/trunk/project@28 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

--- apps/frontend/modules/parlementaire/templates/showSuccess.php
+++ apps/frontend/modules/parlementaire/templates/showSuccess.php
@@ -1,2 +1,8 @@
 <h1><? echo $parlementaire->nom; ?></h1>
-<h2>Député de la <? echo $parlementaire->num_circo; ?> circonscription de <? echo $parlementaire->nom_circo; ?></h2>
+<h2>Député<? if ($parlementaire->sexe == 'F') echo 'e'; ?> de la <? echo $parlementaire->num_circo; if ($parlementaire->num_circo == 1) echo 'ère'; else echo 'ème'; ?> circonscription de <? echo $parlementaire->nom_circo; ?></h2>
+<p>Mandat en cours débuté le <? echo $parlementaire->debut_mandat ?></p>
+<? if ($parlementaire->site_web) : ?>
+<p><a href="<? echo $parlementaire->site_web ?>">Site web</a></p>
+<? endif; ?>
+<p><a href="<? echo $parlementaire->url_an ?>">Fiche sur le site de l'Assemblée Nationale</a></p>
+<p>Profession : <? echo $parlementaire->profession ?></p>
