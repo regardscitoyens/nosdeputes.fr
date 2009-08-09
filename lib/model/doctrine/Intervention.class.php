48f325a610f4e32f59121c584def863d101e3ee3
Tangui Morlier <tangui@tangui.eu.org>
2009-08-09 13:25:40 +0000

    Reconnaissance d'amendement identique
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/trunk/project@197 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

--- lib/model/doctrine/Intervention.class.php
+++ lib/model/doctrine/Intervention.class.php
@@ -103,7 +103,7 @@ class Intervention extends BaseIntervention
   public function getIntervention($args = array()) {
     $inter = $this->_get('intervention');
     if (isset($args['linkify_amendements'])) {
-      if (preg_match_all('/(amendements?[,\s]+(identiques)?[,\s]*)((n[°os\s]*|\d+\s*|,\s*|à\s*|et\s*|rectifié\s*)+)/', $inter, $match)) {
+      if (preg_match_all('/(amendements?[,\s]+(identiques?)?[,\s]*)((n[°os\s]*|\d+\s*|,\s*|à\s*|et\s*|rectifié\s*)+)/', $inter, $match)) {
 	$lois = implode(',', $this->getTags(array('is_triple' => true,
 						  'namespace' => 'loi',
 						  'key' => 'numero',
