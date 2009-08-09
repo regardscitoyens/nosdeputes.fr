1a2abeebac7151616d6028af8f4d52d8d9daca26
bjperson <brice@ideeslibres.org>
2009-08-09 00:28:02 +0000

    Quelques modifs de la page d'un député
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/trunk/project@189 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

--- apps/frontend/modules/parlementaire/templates/showSuccess.php
+++ apps/frontend/modules/parlementaire/templates/showSuccess.php
@@ -6,7 +6,10 @@
     </div>
   </div>
   <div class="graph_depute">
-			 <?php //echo include_component('plot', 'parlementairePresence', array('parlementaire' => $parlementaire)); ?>    
+    <div class="info_depute">
+      <h1><?php echo $parlementaire->nom.' '.$parlementaire->getLongStatut(); ?></h1>
+    </div>
+    <?php echo include_component('plot', 'parlementairePresence', array('parlementaire' => $parlementaire)); ?>    
   </div>
   <div class="barre_activite">
     <h2>Activité parlementaire : </h2>
@@ -28,22 +31,16 @@
     <div class="b_d_h"><div class="b_d_hg"></div><div class="b_d_hd"></div></div>
     <div class="b_d_cont">
       <div class="b_d_infos">
-      <h1><?php echo $parlementaire->nom; ?></h1>
     <p>Né le ... (... ans) à ... (...)</p>
     <ul>
-      <li><?php echo $parlementaire->getLongStatut(); ?> depuis le <?php echo $parlementaire->debut_mandat ?></li>
       <li>Groupe politique : <?php echo link_to($parlementaire->getGroupe()->getNom(), '@list_parlementaires_organisme?slug='.$parlementaire->getGroupe()->getSlug()); ?> (<?php echo $parlementaire->getGroupe()->getFonction(); ?>)</li>
       <li>Profession : <?php if ($parlementaire->profession) : echo link_to($parlementaire->profession, '@list_parlementaires_profession?profession='.$parlementaire->profession); else : ?>Non communiquée<?php endif; ?></li>
       <li><?php echo link_to('Fiche sur le site de l\'Assemblée Nationale', $parlementaire->url_an, array('title' => 'Lien externe', 'onclick' => 'window.open(this.href); return false;')); ?></li>
+      <li><a href="http://fr.wikipedia.org/wiki/<?php echo $parlementaire->nom; ?>">Page sur Wikipédia</a></li>
       <?php if ($parlementaire->site_web) : ?>
       <li><?php echo link_to('Site web', $parlementaire->site_web, array('title' => 'Lien externe', 'onclick' => 'window.open(this.href); return false;')); ?></li>
       <?php endif; ?>
-      <li>Suppléant : Mr Toto</li>
     </ul>
-      
-      <h3>Le mot de <?php echo $parlementaire->nom; ?></h3>
-      <p class="mot_dep">Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam eaque ipsa, quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt, explicabo. Nemo enim ipsam voluptatem, quia voluptas sit, aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos.</p>
-      
       <h3>Responsabilités</h3>
       <ul>
         <li>Parlementaires :
@@ -70,10 +67,6 @@
       Téléphone : <a href="callto:0033549021575">05 49 02 15 75</a><br />
       Télécopie : 05 49 02 15 76
       </p>
-      
-      <h3>Place dans l'hémicycle</h3>
-      <p>Numéro de la place occupée : 385</p>
-      
       <p>Source : <a href="http://www.assembleenationale.fr/" onclick="window.open(this.href); return false;">Assemblée Nationale</a></p>
       </div>
     </div>
@@ -84,13 +77,9 @@
     <div class="b_d_h"><div class="b_d_hg"></div><div class="b_d_hd"></div></div>
     <div class="b_d_cont">
       <div class="b_d_infos">
-      <h2>Travaux Législatifs</h2>
-      <ul>
-        <li><a href="#">Propositions de loi et de résolution</a></li>
-        <li><a href="#">Rapports</a></li>
-        <li><a href="#">Séances publiques contenant au moins une intervention de <?php echo $parlementaire->nom; ?></a></li>
-        <li><a href="#">Réunions de commissions contenant au moins une intervention de <?php echo $parlementaire->nom; ?></a></li>
-      </ul>
+      <h2>Implication dans un projet de loi</h2>
+<?php echo include_component('section', 'parlementaire', array('parlementaire' => $parlementaire, 'limit'=>5)); ?>
+<?php echo link_to('suite', '@parlementaire_textes?slug='.$parlementaire->slug); ?>
       </div>
     </div>
     <div class="b_d_b"><div class="b_d_bg"></div><div class="b_d_bd"></div></div>
@@ -100,7 +89,7 @@
     <div class="b_d_h"><div class="b_d_hg"></div><div class="b_d_hd"></div></div>
     <div class="b_d_cont">
       <div class="b_d_infos">
-      <h2>Questions au gouvernement</h2>
+      <h2>Questions écrites/orales</h2>
       </div>
     </div>
     <div class="b_d_b"><div class="b_d_bg"></div><div class="b_d_bd"></div></div>
@@ -110,27 +99,13 @@
     <div class="b_d_h"><div class="b_d_hg"></div><div class="b_d_hd"></div></div>
     <div class="b_d_cont">
       <div class="b_d_infos">
-      <h2>Présence en hémicycle et commission</h2>
-      <h3><?php echo link_to("Présences",'@parlementaire_presences?slug='.$parlementaire->getSlug()); ?></h3>
-      <h3><?php echo link_to("Interventions",'@parlementaire_interventions?slug='.$parlementaire->getSlug()); ?></h3>
+      <h2>Interventions</h2>
+      <h3><?php echo link_to("Présences actives",'@parlementaire_presences?slug='.$parlementaire->getSlug()); ?></h3>
+      <h3><?php echo link_to("Toutes ses interventions",'@parlementaire_interventions?slug='.$parlementaire->getSlug()); ?></h3>
       <h3>Tags</h3>
 <div style="text-align: justify">
 <?php echo include_component('tag', 'tagcloud', array('tagquery' => $qtag, 'model' => 'Intervention', 'min_tag' => 2, 'route' => '@tag_parlementaire_interventions?parlementaire='.$parlementaire->slug.'&')); ?>
 </div>
-
-<h3>Textes</h3>
-<?php echo include_component('section', 'parlementaire', array('parlementaire' => $parlementaire, 'limit'=>5)); ?>
-<?php echo link_to('suite', '@parlementaire_textes?slug='.$parlementaire->slug); ?>
-      </div>
-    </div>
-    <div class="b_d_b"><div class="b_d_bg"></div><div class="b_d_bd"></div></div>
-  </div>
-  
-  <div class="boite_depute" id="b5">
-    <div class="b_d_h"><div class="b_d_hg"></div><div class="b_d_hd"></div></div>
-    <div class="b_d_cont">
-      <div class="b_d_infos">
-      <h2>Les votes en séance</h2>
       </div>
     </div>
     <div class="b_d_b"><div class="b_d_bg"></div><div class="b_d_bd"></div></div>
@@ -138,8 +113,9 @@
   
   <div class="bas_depute">
     <div class="bas_depute_g">
-      <h2>Top Karma</h2>
-      <div class="boite_citoyen">
+      <h2>Derniers commentaires</h2>  
+      <?php # var_dump( sfConfig::get('sf_escaping_strategy') ); ?>
+      <!-- <div class="boite_citoyen">
         <div class="b_c_h"><div class="b_c_hg"></div><div class="b_c_hd"></div></div>
         <div class="b_c_cont">
           <div class="b_c_photo">
@@ -152,37 +128,7 @@
           </div>
         </div>
         <div class="b_c_b"><div class="b_c_bg"></div><div class="b_c_bd"></div></div>
-      </div>
-      
-      <div class="boite_citoyen">
-        <div class="b_c_h"><div class="b_c_hg"></div><div class="b_c_hd"></div></div>
-        <div class="b_c_cont">
-          <div class="b_c_photo">
-          
-          </div>
-          <div class="b_c_text">
-            <h3>Amandine M. <span class="note"><?php echo image_tag('../css/'.$style.'/images/etoile.png', 'alt="***"'); ?></span></h3>
-            <p><a href="#">125 articles</a></p>
-            <p><a href="#">Voir la fiche perso</a></p>
-          </div>
-        </div>
-        <div class="b_c_b"><div class="b_c_bg"></div><div class="b_c_bd"></div></div>
-      </div>
-      
-      <div class="boite_citoyen">
-        <div class="b_c_h"><div class="b_c_hg"></div><div class="b_c_hd"></div></div>
-        <div class="b_c_cont">
-          <div class="b_c_photo">
-          
-          </div>
-          <div class="b_c_text">
-            <h3>Michael J. <span class="note"><?php echo image_tag('../css/'.$style.'/images/etoile.png', 'alt="***"'); ?></span></h3>
-            <p><a href="#">1 articles</a></p>
-            <p><a href="#">Voir la fiche perso</a></p>
-          </div>
-        </div>
-        <div class="b_c_b"><div class="b_c_bg"></div><div class="b_c_bd"></div></div>
-      </div>
+      </div> -->
     </div>
     <div class="bas_depute_d">
       <h2>Derniers articles des membres pour : <a href="#">#<?php echo $parlementaire->slug; ?></a> <span class="rss"><a href="#"><?php echo image_tag('../css/'.$style.'/images/rss.png', 'alt="Flux rss"'); ?></a></span></h2>
