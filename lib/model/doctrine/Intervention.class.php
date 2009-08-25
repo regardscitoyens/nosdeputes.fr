e0220d16ddb332517d26ac4d3210d64c6b265c24
Tangui Morlier <tangui@tangui.eu.org>
2009-08-25 16:44:32 +0000

    Correction pb amendements rectifiés
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/trunk/project@313 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

--- lib/model/doctrine/Intervention.class.php
+++ lib/model/doctrine/Intervention.class.php
@@ -123,8 +123,8 @@ class Intervention extends BaseIntervention
 	  if (preg_match_all('/\s*(\d[\d\s\à]*rectifiés?|\d[\d\s\à]*)(,\s*|\s*et\s*)*/', $match[3][$i], $amends)) {
 	    $replace = $match[3][$i];
 	    foreach($amends[1] as $amend) {
-	      $am = preg_replace('/[^\dà]+/', '',$amend);
-	      $am = preg_replace('/à+/', '-', $am);
+	      $am = preg_replace('/à+/', '-', $amend);
+	      $am = preg_replace('/[^\d\-]+/', '',$amend);
 	      $link = str_replace('LLL', urlencode($lois), $linko);
 	      $link = str_replace('AAA', urlencode($am), $link);
 	      $replace = preg_replace('/'.$amend.'/', ' <a name="amend_'.$am.'" href="'.$link.'">'.$amend.'</a> ', $replace);
