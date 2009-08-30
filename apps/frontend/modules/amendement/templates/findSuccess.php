16c86e7dafe6de0aba8105346b880490cf025730
Tangui Morlier <tangui@tangui.eu.org>
2009-08-30 01:22:08 +0000

    Maintenant ca fonctionne avec plusieurs amendements trouvés
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/trunk/project@354 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

--- apps/frontend/modules/amendement/templates/findSuccess.php
+++ apps/frontend/modules/amendement/templates/findSuccess.php
@@ -1,8 +1,11 @@
 <div class="temp">
-<p><?php echo count($amendements) ?> amendements</p>
+<?php if (!count($amendements)) { ?>
+<p>Nous n'avons pas trouvé d'amendmement correspondant à votre recherche.</p>
+<?php }else { ?>
+<p><?php echo count($amendements) ?> amendements :</p>
 <ul>
 <?php foreach($amendements as $a) :?>
-<li><?php echo link_for($a->numero, '@amendement?id='.$a->id); ?></li>
+<li><?php echo link_to('Amendement n°'.$a->numero.' portant sur "'.$a->sujet, '@amendement?id='.$a->id); ?></li>
 <?php endforeach; ?>
 </ul>
-</div>
+<?php } ?></div>
