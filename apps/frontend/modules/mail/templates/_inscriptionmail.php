a7359558a0b885468b4971a2d02a8bcbc8187982
bjperson <brice@ideeslibres.org>
2009-09-04 09:58:09 +0000

    utf8...
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/trunk/project@400 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

--- apps/frontend/modules/mail/templates/_inscriptionmail.php
+++ apps/frontend/modules/mail/templates/_inscriptionmail.php
@@ -1,10 +1,10 @@
-Bienvenue sur NosDéputés.fr !
+Bienvenue sur NosDÃ©putÃ©s.fr !
 
 Pour finaliser votre inscription, cliquez sur le lien suivant : 
 <?php  echo url_for('@activation_citoyen_mail?activation_id='.$activation_id, true); ?>
 
 
-Merci de l'intêret que vous nous portez.
+Merci de l'intÃªret que vous nous portez.
 
-L'équipe de
-NosDéputés.fr
+L'Ã©quipe de
+NosDÃ©putÃ©s.fr
