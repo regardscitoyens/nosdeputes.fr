26bcdb5640c2437f99214654af52c3111f40f457
Tangui Morlier <tangui@tangui.eu.org>
2009-09-05 14:43:37 +0000

    Oups pb de duplicated routes
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/trunk/project@403 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

--- apps/frontend/modules/parlementaire/templates/indexSuccess.php
+++ apps/frontend/modules/parlementaire/templates/indexSuccess.php
@@ -1,7 +1,7 @@
 <div class="temp">
 <ul>
 <li><?php echo link_to('tous les députés','@list_parlementaires')?></li>
-<li><?php echo link_to('les députés par tag','@parlementaire_tags')?></li>
+<li><?php echo link_to('les députés par tag','@parlementaires_tags')?></li>
 <li><?php echo link_to('tous les textes','@sections')?></li>
 <li><?php echo link_to('tous les amendements','@search_amendements')?></li>
 <li><?php echo link_to('top des interventions','@top_interventions')?></li>
