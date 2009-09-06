102dd4731c15b3c888aa3b0a437de5c1620a0bce
Tangui Morlier <tangui@tangui.eu.org>
2009-09-06 13:57:58 +0000

    Pas beaucoup mais titre et légende lisible (pour démo aux inrocks)
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/trunk/project@433 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

--- apps/frontend/modules/parlementaire/templates/showSuccess.php
+++ apps/frontend/modules/parlementaire/templates/showSuccess.php
@@ -1,13 +1,13 @@
 <?php $style = 'fixe'; # en attendant le style switcher ?>
 <div class="fiche_depute">
+<p><h1><?php echo $parlementaire->nom.', '.$parlementaire->getLongStatut(1); ?></h1></p>
   <div class="depute_gauche">
     <div class="photo_depute">
-<?php    echo '<img src="'.url_for('@resized_photo_parlementaire?height=150&slug='.$parlementaire->slug).'" class="photo_fiche" alt="Photo de '.$parlementaire->nom.'"/>'; ?>
+<?php    echo '<img src="'.url_for('@resized_photo_parlementaire?height=170&slug='.$parlementaire->slug).'" class="photo_fiche" alt="Photo de '.$parlementaire->nom.'"/>'; ?>
     </div>
   </div>
   <div class="graph_depute">
     <div class="info_depute">
-      <h1><?php echo $parlementaire->nom.', '.$parlementaire->getLongStatut(1); ?></h1>
     </div>
       <?php echo include_component('plot', 'parlementairePresence', array('parlementaire' => $parlementaire, 'options' => array('plot' => 'total', 'questions' => 'on', 'link' => 'on'))); ?>
   </div>
