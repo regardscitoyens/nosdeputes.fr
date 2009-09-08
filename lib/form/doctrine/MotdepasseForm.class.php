9de931dad40aa9af263ee5bda4d5800d1d7ccaa1
bjperson <brice@ideeslibres.org>
2009-09-08 11:06:59 +0000

    Oups! fatigué hier soir ;)
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/trunk/project@458 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

---
+++ lib/form/doctrine/MotdepasseForm.class.php
@@ -0,0 +1,53 @@
+<?php
+class MotdepasseForm extends CitoyenForm
+{
+  public function configure()
+  {
+    $this->widgetSchema->setNameFormat('citoyen[%s]');
+    
+    // Enleve les widgets qu'on ne veut pas montrer
+    unset(
+      $this['id'], 
+      $this['login'], 
+      $this['email'], 
+      $this['activite'], 
+      $this['url_site'], 
+      $this['naissance'], 
+      $this['sexe'], 
+      $this['employe_an'], 
+      $this['travail_pour'], 
+      $this['nom_circo'], 
+      $this['num_circo'], 
+      $this['photo'], 
+      $this['is_active'], 
+      $this['activation_id'], 
+      $this['role'], 
+      $this['last_login'], 
+      $this['created_at'], 
+      $this['updated_at'], 
+      $this['slug']
+    );
+    
+    // verif mot de passe avec confirmation
+    $this->widgetSchema['password'] = new sfWidgetFormInputPassword();
+    $this->widgetSchema['password_bis'] = new sfWidgetFormInputPassword();
+    
+    // Les labels
+    $this->widgetSchema->setLabels(array(
+      'password' => 'Mot de passe : ',
+      'password_bis' => 'Confirmation : '
+    ));
+    
+    $this->validatorSchema['password']     = new sfValidatorString(array('required' => true, 'min_length' => 6), array('min_length' => '"%value%" est trop court (%min_length% lettres minimum).', 'required' => 'Champ obligatoire.'));
+    $this->validatorSchema['password_bis'] = new sfValidatorString(array('required' => true, 'min_length' => 6), array('min_length' => '"%value%" est trop court (%min_length% lettres minimum).', 'required' => 'Champ obligatoire.'));
+    
+    $this->mergePostValidator(new sfValidatorSchemaCompare(
+      'password',
+      sfValidatorSchemaCompare::EQUAL,
+      'password_bis',
+      array(),
+      array('invalid' => 'Les champs doivent être identiques.')
+    ));
+  }
+}
+?>
