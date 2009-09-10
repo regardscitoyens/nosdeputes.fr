e43cc8569eaa8ec7579248c83e6d371085be6f1e
Tangui Morlier <tangui@tangui.eu.org>
2009-09-10 17:32:00 +0000

    Suppression des précédents tops et passage au top global
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/trunk/project@493 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

--- apps/frontend/modules/parlementaire/templates/indexSuccess.php
+++ apps/frontend/modules/parlementaire/templates/indexSuccess.php
@@ -5,8 +5,6 @@
 <li><?php echo link_to('les députés par tag','@parlementaires_tags')?></li>
 <li><?php echo link_to('tous les textes par interventions','@sections?order=plus')?></li>
 <li><?php echo link_to('tous les textes ordre chrono','@sections?order=date')?></li>
-<li><?php echo link_to('top des interventions','@top_interventions')?></li>
-<li><?php echo link_to('top des présences','@top_presences')?></li>
-<li><?php echo link_to('top des amendements','@top_amendements')?></li>
+<li><?php echo link_to('Synthèse globale','@top_global')?></li>
 </ul>
 </div>
