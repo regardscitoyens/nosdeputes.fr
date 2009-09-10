627902a19c4c9860b18156a5e2258487b4739e1d
Regards Citoyens <contact@regardscitoyens.org>
2009-09-10 12:50:56 +0000

    Debug affichage tags. Roux
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/trunk/project@489 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

--- apps/frontend/modules/intervention/templates/tagSuccess.php
+++ apps/frontend/modules/intervention/templates/tagSuccess.php
@@ -1,7 +1,10 @@
 <div class="temp">
-<?php
+<?php 
+$options = array('intervention_query' => $query, 'highlight' => $tags);
+if (isset($parlementaire)) {
 $titre = 'Interventions sur <em>"'.implode(', ', $tags).'"</em>';
 echo include_component('parlementaire', 'header', array('parlementaire' => $parlementaire, 'titre' => $titre));
-?>
-<?php echo include_component('intervention', 'pagerInterventions', array('intervention_query' => $query, 'highlight' => $tags, 'nophoto' => true)); ?>
+$options = array_merge($options, array('nophoto' => true)); 
+} ?>
+<?php echo include_component('intervention', 'pagerInterventions', $options); ?>
 </div>
