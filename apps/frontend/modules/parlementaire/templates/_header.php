a0522ab4408791fe1df129d9f16245d84456f2ac
RouxRC <b.ooghe@gmail.com>
2009-09-10 02:45:31 +0000

    correction de en d'
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/trunk/project@487 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

--- apps/frontend/modules/parlementaire/templates/_header.php
+++ apps/frontend/modules/parlementaire/templates/_header.php
@@ -8,7 +8,7 @@
 <h1><?php if (isset($surtitre)) { ?>
     <?php echo $surtitre; ?></h1><h2>
 <?php } ?>
-  <?php echo $titre.' de <a href="'.url_for($parlementaire->getPageLink()).'">'.$parlementaire->nom.'</a>';
+  <?php echo $titre.' '; if (preg_match('/^(A|E|É|I|O|U|Y)/', $parlementaire->nom)) echo "d'"; else echo 'de '; echo '<a href="'.url_for($parlementaire->getPageLink()).'">'.$parlementaire->nom.'</a>';
   if (isset($surtitre)) echo '</h1>'; else echo '</h2>'; ?>
   </div></div>
 </div>
