b674673fe927da658721c2bd3ffb08ef896c8f69
Gabriel Kerneis <gabriel@kerneis.info>
2009-09-11 08:50:48 +0000

    Amélioration Best/Worst (traduction en français, exposants typographiquement corrects).
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/trunk/project@505 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

--- apps/frontend/modules/parlementaire/templates/_top.php
+++ apps/frontend/modules/parlementaire/templates/_top.php
@@ -1,9 +1,19 @@
 <?php
+$title = array('semaine' => 'Semaines en commission',
+	       'commission_presences' => 'Présences en commission',
+	       'commission_interventions'=> 'Interventions en commission',
+	       'hemicycle_interventions'=>'Interventions longues en hémicycle',
+	       'hemicycle_invectives'=>'Interventions courtes en hémicycle',
+	       'amendements_signes' => 'Amendements signés',
+	       'amendements_adoptes'=>'Amendements adoptés',
+	       'amendements_rejetes' => 'Amendements rejetés',
+	       'questions_ecrites' => 'Questions écrites',
+	       'questions_orales' => 'Questions orales');
       $top = $parlementaire->getTop();
 if (!$top)
   return ;
 if (!$parlementaire->fin_mandat) {
-  echo '<h3>Best/Worst (sur 12 derniers mois)</h3>';
+  echo '<h3>Le meilleur et le pire (sur 12 derniers mois)</h3>';
   $rank = 1;
  } else {
   $rank = 0;
@@ -19,9 +29,19 @@ foreach(array_keys($top) as $k) {
   if ($rank && $top[$k]['rank'] >= $top[$k]['max_rank'] - 150) 
     echo 'color: red';
   echo '">';
-  echo $k.' : '.$top[$k]['value'];
-  if ($rank)
-    echo ' ('.$top[$k]['rank'].'ème/'.$top[$k]['max_rank'].')';
+  echo $title[$k].' : '.$top[$k]['value'];
+  if ($rank ){
+    echo ' ('.$top[$k]['rank'].'<sup>';
+    if ($top[$k]['rank'] == 1) {
+      if($parlementaire->sexe == 'H')
+        echo 'er';
+      else
+        echo 're';
+    }
+    else
+        echo 'e';
+    echo '</sup> sur '.$top[$k]['max_rank'].')';
+  }
   echo '</li>';
 }?></ul>
 
