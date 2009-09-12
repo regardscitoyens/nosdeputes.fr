389d833efe06d08b3c6df3e46aba987b9e0a4790
Gabriel Kerneis <gabriel@kerneis.info>
2009-09-12 16:08:04 +0000

    Message d'avertissement pour les commentaires non publics.
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/trunk/project@549 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

--- apps/frontend/modules/commentaire/templates/_show.php
+++ apps/frontend/modules/commentaire/templates/_show.php
@@ -6,6 +6,12 @@ if($c->is_public || $c->citoyen_id == $sf_user->getAttribute('user_id') ) :
 include_component('citoyen', 'shortCitoyen', array('citoyen_id'=>$c->citoyen_id));
 ?> a dit :</p>
 <p><?php echo $c->commentaire; ?></p>
+<?php
+if(!$c->is_public)
+	echo "<p><strong>Attention, ce commentaire est en attente de
+	validation par email. Les autres utilisateurs ne peuvent pas le
+	voir.</strong></p>";
+?>
 <p><a href="#commentaire_<?php echo $c->id;?>">Permalink</a></p>
 <p>Vous trouvez ce commentaire constructif : <?php include_component('rate', 'show', array('object' => $c)); ?> </p>
 </div>
