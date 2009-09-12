75181a69e8ac532cf5eb575c4a56faa1e586b1df
Gabriel Kerneis <gabriel@kerneis.info>
2009-09-12 15:57:03 +0000

    Affiche un commentaire seulement si il est public ou si c'est celui de l'utilisateur.
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/trunk/project@547 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

--- apps/frontend/modules/commentaire/templates/_show.php
+++ apps/frontend/modules/commentaire/templates/_show.php
@@ -1,13 +1,12 @@
-<?php foreach($commentaires as $c) : ?>
+<?php foreach($commentaires as $c) : 
+if($c->is_public || $c->citoyen_id == $sf_user->getAttribute('user_id') ) :
+?>
 <div id="commentaire_<?php echo $c->id; ?>" class="commentaire">
 <p>Le <?php echo myTools::displayDate($c->created_at); ?>, <?php 
 include_component('citoyen', 'shortCitoyen', array('citoyen_id'=>$c->citoyen_id));
 ?> a dit :</p>
-<p><?php 
-if($c->is_public)
-    echo $c->commentaire;
-?></p>
+<p><?php echo $c->commentaire; ?></p>
 <p><a href="#commentaire_<?php echo $c->id;?>">Permalink</a></p>
 <p>Vous trouvez ce commentaire constructif : <?php include_component('rate', 'show', array('object' => $c)); ?> </p>
 </div>
-<?php endforeach; ?>
+<?php endif; endforeach; ?>
