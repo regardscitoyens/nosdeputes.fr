081df2a717f62e712664fc9d655b024f5529875c
jbellieri <jbgabellieri@gmail.com>
2009-09-12 15:18:38 +0000

    Design - pourvu que je ne casse pas tout :)
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/trunk/project@543 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

--- apps/frontend/templates/layout.php
+++ apps/frontend/templates/layout.php
@@ -1,4 +1,4 @@
-<?php $style = 'fixe'; ?>
+<?php $style = 'xneth'; ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
 "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
@@ -27,114 +27,100 @@ if ($rss) {
     <![endif]-->
   </head>
   <body>
-  <div id="navigation"><a href="#menu">Aller au menu</a> <a href="#contenu">Aller au contenu</a> <a href="#raccourcis"  accesskey="0">Racourcis clavier</a> <a href="#"  accesskey="3">Plan du site</a></div>
-    <div class="haut">
-      <div class="conteneur_entete">
-        <div class="entete">
-          <div class="logo">  
-            <a href="<?php echo url_for('@homepage');?>"><?php echo image_tag($style.'/logo2.png', array('alt' => 'NosDéputés.fr')); ?></a>
-          </div>
-          <div class="haut_droite">
-            <div class="boite_util">
-              <div class="b_u_h"><div class="b_u_hg"></div><div class="b_u_hd"></div></div>
-              <div class="b_u_cont">
-                <?php if(!$sf_user->isAuthenticated()) { ?>
-                <form method="post" action="<?php echo url_for('@signin'); ?>">
-                  <input type="text" name="signin[login]" id="signin_login" size="10" class="signin" /> <input type="password" name="signin[password]" id="signin_password" size="10" class="signin" /> <input type="checkbox" name="signin[remember]" id="signin_remember" title="se rappeler de moi" />
-                  <input type="submit" value="login" class="bouton_ok" />
-                  <a href="<?php echo url_for('@inscription') ?>" class="bouton_ok">S'inscrire</a>
-                </form> <?php }
-                if($sf_user->isAuthenticated())
-                { 
-                  if($sf_user->getAttribute('is_active') == true) { ?>
-                  <a href="<?php echo url_for('@citoyen?slug='.$sf_user->getAttribute('slug')); ?>">Mon Profil</a> - 
-                  <?php
-                  }
-                  ?>
-                Connecté en tant que <?php echo $sf_user->getAttribute('login'); ?> - 
-                <a href="<?php echo url_for('@signout') ?>">Déconnexion</a>
-                <?php } ?>
-              </div>
-              <div class="b_u_b"><div class="b_u_bg"></div><div class="b_u_bd"></div></div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="conteneur_menu">
-        <div class="menu" id="menu">
-          <ul>
-            <li class="neuf"><a href="<?php echo url_for('@homepage'); ?>">Accueil</a></li>
-            <li class="douze"><a href="<?php echo url_for('@list_parlementaires'); ?>">Les Députés</a></li>
-            <li class="neuf"><a href="<?php echo url_for('@sections?order=date')?>">Les Lois</a></li>
-            <li class="treize"><a href="<?php echo url_for('@list_citoyens')?>">Les Citoyens</a></li>
-            <li class="douze"><a href="http://www.assemblee-nationale.fr/">L'Assemblée</a></li>
-          </ul>
-          <div class="search_box">
-            <form action="<?php echo url_for('@search'); ?>" method="get">
-            <p>
-              <input class="rechercher" name="search" type="text" size="15" value="<?php if (isset($_GET['search'])) echo $_GET['search']; ?>"/>
-              <?php
-                if (isset($_GET['search']) && preg_match('/parlementaire/', $_SERVER['REQUEST_URI'])) $selectdepute = ' selected="selected"';
-                else $selectdepute = "";
-                if (isset($_GET['search']) && preg_match('/circonscription/', $_SERVER['REQUEST_URI'])) $selectcirco = ' selected="selected"';
-                else $selectcirco = "";
-                if (isset($_GET['search']) && preg_match('/profession/', $_SERVER['REQUEST_URI'])) $selectprof = ' selected="selected"';
-                else $selectprof = "";
-                if (isset($_GET['search']) && preg_match('/intervention/', $_SERVER['REQUEST_URI'])) $selectinterv = ' selected="selected"';
-                else $selectinterv = "";
-                if (isset($_GET['search']) && preg_match('/amendement/', $_SERVER['REQUEST_URI'])) $selectamdmt = ' selected="selected"';
-                else $selectamdmt = "";
-              ?>
-              <select class="type_recherche" name="type">
-                <option value="depute"<?php echo $selectdepute; ?>>Députés</option>
-                <option value="departement"<?php echo $selectcirco; ?>>Départements</option>
-                <option value="profession"<?php echo $selectprof; ?>>Profession</option>
-                <option value="intervention"<?php echo $selectinterv; ?>>Interventions</option>
-                <option value="amendement"<?php echo $selectamdmt; ?>>Amendements</option>
-              </select>
-              <input class="bouton_ok" type="submit" value="ok"/>
-            </p>
-            </form>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="corps">
-      <div class="contenu" id="contenu">
-<?php if ($sf_user->hasFlash('notice')) :?>
-<p class='flash_notice'><?php echo $sf_user->getFlash('notice'); ?></p>
-<?php endif;?>
-<?php if ($sf_user->hasFlash('error')) :?>
-<p class='flash_error'><?php echo $sf_user->getFlash('error'); ?></p>
-<?php endif;?>
-        <?php echo $sf_content ?>
-      </div>
-    </div>
-    <div class="bas">
-      <div class="conteneur_menu_suite">
-        <div class="menu2">
-          <div class="up">
-            <a href="#" title="haut de page"><?php echo image_tag($style.'/mini_logo.png', 'alt=LGT'); ?></a>
-          </div>
-          <!-- <div class="style_switcher">
-            <form method="post" action="<?php echo  $_SERVER['PHP_SELF'] ?>" id="select_style">
-              <p>Style : 
-                <select name="style" onchange="javascript:this.form.submit()">
-                  <option value="defaut">par défaut</option>
-                  <option value="fixe" selected="selected">fixe</option>
-                </select> 
-              </p>
-              <noscript><p><input type="submit" value="ok" /></p></noscript>
-            </form>
-          </div> -->
-          <div class="menu_bas">
-            <ul>
-              <li class="dixhuit"><a href="#">Qui sommes nous ?</a></li>
-              <li class="treize"><a href="#">Plan du site</a></li>
-            </ul>
-          </div>
-        </div>
-      </div>
+  <div id="contenu">
+			<div id="top">
+				<div class="initiative">
+					<a href="http://www.regardscitoyens.org/" onclick="return(window.open(this.href)?false:true);"><?php echo image_tag($style.'/top_initiative.png', array('alt' => 'Une initiative de RegardsCitoyens.fr')); ?></a>
+				</div>
+				<div class="identification">
+				     <?php if(!$sf_user->isAuthenticated()) { ?>
+					<form method="post" action="<?php echo url_for('@signin'); ?>">
+					<p>
+					<a href="<?php echo url_for('@inscription') ?>"><button type="submit"  id="bt2"></button></a>
+					<button type="submit" value="login" id="bt1"></button>
+					<input type="text" name="signin[password]" value="&#149;&#149;&#149;&#149;&#149;&#149;&#149;&#149;" onfocus="if(this.value=='&#149;&#149;&#149;&#149;&#149;&#149;&#149;&#149;')this.value ='';" onblur="if(this.value=='')this.value ='&#149;&#149;&#149;&#149;&#149;&#149;&#149;&#149;';"/>
+					<input type="text" name="signin[login]" value="Identifiant" onfocus="if(this.value=='Identifiant')this.value ='';" onblur="if(this.value=='')this.value ='Identifiant';" />
+					<!-- <input type="checkbox" name="signin[remember]" id="signin_remember" title="se rappeler de moi" /> --> 
+					</p>
+					</form> <?php }
+					if($sf_user->isAuthenticated())
+					{ 
+					if($sf_user->getAttribute('is_active') == true) { ?>
+					<a href="<?php echo url_for('@citoyen?slug='.$sf_user->getAttribute('slug')); ?>">Mon Profil</a> - 
+					<?php
+					}
+					?>
+					Connecté en tant que <?php echo $sf_user->getAttribute('login'); ?> - 
+					<a href="<?php echo url_for('@signout') ?>">Déconnexion</a>
+					<?php } ?>
+				</div>
+			</div>
+			<div id="header">
+				<a href="<?php echo url_for('@homepage');?>"><?php echo image_tag($style.'/header_logo.png', array('alt' => 'NosDeput&eacute;s.fr')); ?></a>
+			</div>
+  			<div id="menu">
+				<div class="menu_navigation">
+					<div id="item1"><a href="<?php echo url_for('@homepage'); ?>"></a></div>
+					<div id="item2"><a href="<?php echo url_for('@list_parlementaires'); ?>"></a></div>
+					<div id="item3"><a href="<?php echo url_for('@sections?order=date')?>"></a></div>
+					<div id="item4"><a href="<?php echo url_for('@list_citoyens')?>"></a></div>
+					<div id="item5"><a href="#"></a></div>
+				</div>
+				<div class="menu_recherche">
+					<form action="<?php echo url_for('@search'); ?>" method="get">
+						<p>
+                            <select class="type_recherche" name="type">
+								<option value="depute"<?php echo $selectdepute; ?>>Députés</option>
+								<option value="departement"<?php echo $selectcirco; ?>>Départements</option>
+								<option value="profession"<?php echo $selectprof; ?>>Profession</option>
+								<option value="intervention"<?php echo $selectinterv; ?>>Interventions</option>
+								<option value="amendement"<?php echo $selectamdmt; ?>>Amendements</option>
+							</select>
+							<img src="images/recherche_fleche.png" alt="" title="" />
+							<input class="bouton_ok" value="" type="submit"/>
+							<input class="rechercher" name="search" type="text" size="15" value="<?php if (isset($_GET['search'])) echo $_GET['search']; ?>"/>
+							 <?php
+							if (isset($_GET['search']) && preg_match('/parlementaire/', $_SERVER['REQUEST_URI'])) $selectdepute = ' selected="selected"';
+							else $selectdepute = "";
+							if (isset($_GET['search']) && preg_match('/circonscription/', $_SERVER['REQUEST_URI'])) $selectcirco = ' selected="selected"';
+							else $selectcirco = "";
+							if (isset($_GET['search']) && preg_match('/profession/', $_SERVER['REQUEST_URI'])) $selectprof = ' selected="selected"';
+							else $selectprof = "";
+							if (isset($_GET['search']) && preg_match('/intervention/', $_SERVER['REQUEST_URI'])) $selectinterv = ' selected="selected"';
+							else $selectinterv = "";
+							if (isset($_GET['search']) && preg_match('/amendement/', $_SERVER['REQUEST_URI'])) $selectamdmt = ' selected="selected"';
+							else $selectamdmt = "";
+							?>
+						</p>
+					</form>
+				</div>
+			</div>
+			<div id="sous_menu">
+				<div class="elements_sous_menu"> <!-- A rendre dynamique en php -->
+					<ul>
+						<li><a href="#">El&eacute;ment 1</a> |</li>
+						<li><a href="#">El&eacute;ment 2</a> |</li>
+						<li><a href="#">El&eacute;ment 3</a> |</li>
+						<li><a href="#">El&eacute;ment 4</a> |</li>
+						<li><a href="#">El&eacute;ment 5</a></li>
+					</ul>
+				</div>
+			</div>
+			<div id="corps_page">
+				<div class="contenu_page">
+					<?php if ($sf_user->hasFlash('notice')) :?>
+					<p class='flash_notice'><?php echo $sf_user->getFlash('notice'); ?></p>
+					<?php endif;?>
+					<?php if ($sf_user->hasFlash('error')) :?>
+					<p class='flash_error'><?php echo $sf_user->getFlash('error'); ?></p>
+					<?php endif;?>
+					<?php echo $sf_content ?>
+				</div>
+			</div>
+			<div id="bottom">
+				<a href=""><?php echo image_tag($style.'/bottom_plansite.png', array('alt' => 'Plan du site')); ?></a>
+				<a href=""><?php echo image_tag($style.'/bottom_qui.png', array('alt' => 'Qui sommes-nous')); ?></a>
+			</div>
     </div>
   </body>
 </html>
