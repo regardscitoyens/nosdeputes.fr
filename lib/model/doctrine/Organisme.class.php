ec136466ffa0a213597e4e4f1b116efbfb1a44a1
RouxRC <b.ooghe@gmail.com>
2009-09-12 09:18:31 +0000

    Complément nécessaire au précédent commit oublié.
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/trunk/project@524 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

--- lib/model/doctrine/Organisme.class.php
+++ lib/model/doctrine/Organisme.class.php
@@ -16,6 +16,7 @@ class Organisme extends BaseOrganisme
         "Socialiste, radical, citoyen et divers gauche" => "SRC",
         "Gauche démocrate et républicaine" => "GDR",
         "Députés n'appartenant à aucun groupe" => "NI",
+        "Députés non-inscrits" => "NI",
         "Nouveau centre" => "NC"
     );
     return $hashmap[$this->getNom()];
@@ -28,6 +29,7 @@ class Organisme extends BaseOrganisme
          "src" => "Socialiste, radical, citoyen et divers gauche",
          "gdr" => "Gauche démocrate et républicaine",
          "ni" => "Députés n'appartenant à aucun groupe",
+         "ni" => "Députés non-inscrits",
          "nc" => "Nouveau centre" );
       return $hashmap["$acro"];
     } else {
