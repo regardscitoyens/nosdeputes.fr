3501102cec3c5e0bb53db6ef4631c292bbe5275d
Gabriel Kerneis <gabriel@kerneis.info>
2009-09-12 15:24:42 +0000

    Ne pas afficher les commentaires non publics par défaut.
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/trunk/project@544 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

--- apps/frontend/modules/commentaire/templates/_show.php
+++ apps/frontend/modules/commentaire/templates/_show.php
@@ -3,7 +3,10 @@
 <p>Le <?php echo myTools::displayDate($c->created_at); ?>, <?php 
 include_component('citoyen', 'shortCitoyen', array('citoyen_id'=>$c->citoyen_id));
 ?> a dit :</p>
-<p><?php echo $c->commentaire; ?></p>
+<p><?php 
+if($c->is_public)
+    echo $c->commentaire;
+?></p>
 <p><a href="#commentaire_<?php echo $c->id;?>">Permalink</a></p>
 <p>Vous trouvez ce commentaire constructif : <?php include_component('rate', 'show', array('object' => $c)); ?> </p>
 </div>
