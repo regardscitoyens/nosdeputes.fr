e92635592319d120306dd89956646dd30054c7c4
jbellieri <jbgabellieri@gmail.com>
2009-09-13 12:32:01 +0000

    Visuel nuage de mots-clés pour liste députés
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/trunk/project@614 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

--- apps/frontend/modules/parlementaire/templates/tagSuccess.php
+++ apps/frontend/modules/parlementaire/templates/tagSuccess.php
@@ -1,4 +1,6 @@
 <div class="temp">
+<div class="liste_deputes_tags">
+<p>Tous les députés par mots-clés prononcés</p>
 <?php
 if (!$tag) {
  echo include_component('tag', 'tagcloud', array('querytag'=>$tquery,'route'=>'@tag_result_parlementaires?', 'limit'=>500)); 
@@ -9,4 +11,5 @@ foreach($parlementaires as $inter) {
   echo '<li>'.link_to($inter['Parlementaire']['nom'], '@tag_parlementaire_interventions?parlementaire='.$inter['Parlementaire']['slug'].'&tags='.$tag).'('.$inter['nb'].')</li>';
 }
 ?>
+</div>
 </div>
