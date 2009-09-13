d96366179740823f29d95ca0f09df3a0c39b5181
RouxRC <b.ooghe@gmail.com>
2009-09-13 19:41:56 +0000

    cas aucun mot clé
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/trunk/project@642 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

--- apps/frontend/modules/tag/templates/_tagcloud.php
+++ apps/frontend/modules/tag/templates/_tagcloud.php
@@ -4,9 +4,13 @@
 .tag_level_2 {font-size: 1em;}
 .tag_level_3 {font-size: 1.5em;}
 </style>
-<?php foreach(array_keys($tags) as $tag) : 
-?><span class="tag_level_<?php echo $tags[$tag]['class']; ?>"><a href="<?php 
+<?php $keys = array_keys($tags);
+if (count($keys)) { foreach($keys as $tag) : ?>
+<span class="tag_level_<?php echo $tags[$tag]['class']; ?>"><a href="<?php 
 echo url_for($route.'tags='.$tags[$tag]['related']); ?>" title="<?php echo $tags[$tag]['count']; ?>"><?php 
 $nom = preg_replace('/\s+/', '&nbsp;', $tags[$tag]['tag']);
 echo $nom; ?></a> <?php
-?></span><?php endforeach; ?>
+?></span><?php endforeach;
+} else { ?>
+<span><em>Désolé, aucun mot-clé pertinent trouvé</em></span>
+<?php } ?>
