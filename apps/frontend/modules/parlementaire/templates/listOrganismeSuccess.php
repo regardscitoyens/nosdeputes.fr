63e5783eb708409514fa0a9ccbfbc56ea9a62ed7
Tangui Morlier <tangui@tangui.eu.org>
2009-09-13 23:20:38 +0000

    rebug
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/trunk/project@686 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

--- apps/frontend/modules/parlementaire/templates/listOrganismeSuccess.php
+++ apps/frontend/modules/parlementaire/templates/listOrganismeSuccess.php
@@ -13,8 +13,7 @@ echo link_to($parlementaire->nom, 'parlementaire/show?slug='.$parlementaire->slu
 echo $parlementaire->getStatut(1).", ".link_to($parlementaire->nom_circo, '@list_parlementaires_circo?search='.$parlementaire->nom_circo); ?>)</li>
 <?php endforeach ; ?>
 </ul>
-<?php include_partial('parlementaire/paginate', array('pager'=>$pager, 'link'=>'@list_parlementaires_organisme?slug='.$orga->getSlug().'&')); ?>
-<?php endif;
+<?php include_partial('parlementaire/paginate', array('pager'=>$pager, 'link'=>'@list_parlementaires_organisme?slug='.$orga->getSlug().'&'));
 if (count($seances)) { ?>
 <div><h3>Les dernières réunions de la <?php if (preg_match('/commission/i', $orga->getNom())) echo 'Comm'; else echo 'M'; ?>ission</h3>
 <ul>
