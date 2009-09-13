00e20ca9e541f9b91fe19e6c4ec2d1b2bd4bfb3c
jbellieri <jbgabellieri@gmail.com>
2009-09-13 23:07:18 +0000

    accueil
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/trunk/project@675 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

--- apps/frontend/modules/parlementaire/templates/indexSuccess.php
+++ apps/frontend/modules/parlementaire/templates/indexSuccess.php
@@ -1,19 +1,31 @@
 <div class="temp">
-<ul>
-<li><?php echo link_to('tous les députés','@list_parlementaires')?></li>
-<li><?php echo link_to('un député au hasard','@parlementaire_random')?></li>
-<li><?php echo link_to('les députés par tag','@parlementaires_tags')?></li>
-<li><?php echo link_to('tous les textes par interventions','@sections?order=plus')?></li>
-<li><?php echo link_to('tous les textes ordre chrono','@sections?order=date')?></li>
-<li><?php echo link_to('Synthèse globale','@top_global')?></li>
-</ul>
-<div>
-<?php echo include_component('parlementaire', 'duJour'); ?>
+<div class="texte_presentation_gauche">
 </div>
-<div>
-<?php echo include_component('plot', 'groupes', array('plot' => 'total')); ?>
+
+<div class="menu_accueil_droite">
+	<div class="accueil_liste_liens">
+	<h2>Navigation :</h2>
+	<ul>
+	<li id="couleur1"><?php echo link_to('Tous les députés','@list_parlementaires')?></li>
+	<li id="couleur2"><?php echo link_to('Un député au hasard','@parlementaire_random')?></li>
+	<li id="couleur1"><?php echo link_to('Les députés par tag','@parlementaires_tags')?></li>
+	<li id="couleur2"><?php echo link_to('Tous les textes par interventions','@sections?order=plus')?></li>
+	<li id="couleur1"><?php echo link_to('Tous les textes ordre chrono','@sections?order=date')?></li>
+	<li id="couleur2"><?php echo link_to('Synthèse globale','@top_global')?></li>
+	</ul>
+	</div>
+	<div class="accueil_deputes_jour">
+	<?php echo include_component('parlementaire', 'duJour'); ?>
+	</div>
 </div>
-<div>
-<?php echo include_component('tag', 'globalActivite'); ?>
+
+<div class="accueil_plot">
+	<div id="center">
+	<?php echo include_component('plot', 'groupes', array('plot' => 'total')); ?>
+	</div>
+</div>
+
+<div class="accueil_nuage_tags">
+	<?php echo include_component('tag', 'globalActivite'); ?>
 </div>
 </div>
