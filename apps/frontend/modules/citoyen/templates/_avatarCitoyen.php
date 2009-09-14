757d7c0313494d2526c9f31532e1e66478110ee9
RouxRC <b.ooghe@gmail.com>
2009-09-14 04:18:18 +0000

    photo_avatar
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/trunk/project@726 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

--- apps/frontend/modules/citoyen/templates/_avatarCitoyen.php
+++ apps/frontend/modules/citoyen/templates/_avatarCitoyen.php
@@ -1,9 +1,10 @@
 <?php
 if (!$user or !$user->photo)
 {
-  echo image_tag($style.'/avatar_citoyen.png', array('alt' => 'Avatar par défaut'));
+  echo image_tag('xneth/avatar_citoyen.png', array('alt' => 'Avatar par d"faut'));
 }
 else
 {
   echo '<a href="'.url_for('@citoyen?slug='.$user->slug).'"><img src="'.url_for('@photo_citoyen?slug='.$user->slug).'" alt="avatar" /></a>';
 }
+?>
