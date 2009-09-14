cfcdb21e40721e6f424eb7f2be86c724a2ff8801
RouxRC <b.ooghe@gmail.com>
2009-09-14 03:28:30 +0000

    titre tag
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/trunk/project@722 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

--- apps/frontend/modules/intervention/templates/tagSuccess.php
+++ apps/frontend/modules/intervention/templates/tagSuccess.php
@@ -1,9 +1,13 @@
 <?php 
 $options = array('intervention_query' => $query, 'highlight' => $tags);
-if (isset($parlementaire)) {
 $titre = 'Interventions sur <em>"'.implode(', ', $tags).'"</em>';
+if (isset($parlementaire)) {
+$titre .= ' de '.link_to($parlementaire->nom, 'parlementaire/'.$parlementaire->slug);
 echo include_component('parlementaire', 'header', array('parlementaire' => $parlementaire, 'titre' => $titre));
 $options = array_merge($options, array('nophoto' => true)); 
-} ?>
+} else {
+  $sf_response->setTitle(strip_tags($titre)); ?>
+<h1><?php echo $titre; ?></h1>
+<?php } ?>
 <?php echo include_component('intervention', 'pagerInterventions', $options); ?>
 
