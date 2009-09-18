48ada1169182d7d907df091754109c4e7c11a2b1
Tangui Morlier <tangui@tangui.eu.org>
2009-09-18 11:04:23 +0000

    Meilleur présentation des commentaires trunqués
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/trunk/project@770 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

--- apps/frontend/modules/commentaire/templates/_showTruncCommentaire.php
+++ apps/frontend/modules/commentaire/templates/_showTruncCommentaire.php
@@ -4,6 +4,6 @@
 include_component('citoyen', 'shortCitoyen', array('citoyen_id'=>$c->citoyen_id));
 ?> a dit le <?php echo myTools::displayDate($c->created_at); ?>:</a></span></p>
 <div class="commentaire_avatar"><?php include_component('citoyen','avatarCitoyen', array('citoyen_id'=>$c->citoyen_id)); ?></div>
-<p><?php echo truncate_text($c->commentaire, 200); ?></p>
+<p><?php echo truncate_text(strip_tags($c->commentaire), 200); ?></p>
 <p><a href="<?php echo url_for($c->lien); ?>#commentaire_<?php echo $c->id; ?>">Lire dans le contexte</a></p>
 </div>
