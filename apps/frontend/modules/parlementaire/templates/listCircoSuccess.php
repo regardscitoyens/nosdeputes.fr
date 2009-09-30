7f127e9a21a87d32e62ccd281bd41ce4e516a121
Gabriel Kerneis <gabriel@kerneis.info>
2009-09-30 16:54:22 +0000

    Intégration des cartes sur les pages des circonscriptions.
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/trunk/project@821 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

--- apps/frontend/modules/parlementaire/templates/listCircoSuccess.php
+++ apps/frontend/modules/parlementaire/templates/listCircoSuccess.php
@@ -9,10 +9,11 @@
 <?php endforeach; ?>
 </ul></td></tr>
 </table>
+<p><?php include 'circonscriptions/france.html' ?></p>
 <?php else : ?>
 <?php $nResults = count($parlementaires);
  if ($num != 0 && $nResults != 0) : ?>
-  <img class="carte_departement" src="http://www.assemblee-nationale.fr/13/qui/circonscriptions/<?php if (preg_match('/^(\d+)(\w+)$/', $num, $match)) printf ('%02d%s-%02d%s', $match[1], $match[2], $match[1], $match[2]); else printf('%03d-%03d', $num, $num); ?>-1.gif" alt="$circo"/>
+ <?php include 'circonscriptions/'.sprintf("%03d",$num).'.html'; ?>
 <?php endif; ?>
 <h1>Les députés par circonscriptions</h1>
 <?php
@@ -20,11 +21,31 @@ $sf_response->setTitle('Les députés par circonscriptions');
   if ($nResults == 0) : ?>
 <p>Aucune circonscription trouvée pour <em>"<?php if ($circo != '') echo $circo; else echo $num; ?>"</em></p>
   <?php else : ?>
-<p><?php echo $circo; ?><?php if ($num != 0) echo ' ('.sprintf("%02d",$num).')'; ?>&nbsp;: <?php echo $nResults; ?> député<?php if ($nResults > 1) echo 's'; if ($num != 0) echo ' pour '.$n_circo.' circonscriptions'; ?></p>
+<p>
+<?php echo $circo; ?>
+<?php if ($num != 0) echo ' ('.sprintf("%02d",$num).')'; 
+if ($num_circo > 0) echo  ', '.$parlementaires[0]->getNumCircoString(1).':';
+else echo '&nbsp;:';
+?>
+
+<?php echo $nResults; ?> député<?php if ($nResults > 1) echo 's';
+if ($num != 0 && $num_circo == 0) echo ' pour '.$n_circo.' circonscriptions';
+?>
+</p>
 <?php endif; ?>
 <ul>
 <?php foreach($parlementaires as $parlementaire) : ?>
-<li><?php if ($num == 0) {echo $parlementaire->getNomNumCirco()." - "; } echo $parlementaire->getNumCircoString(1); ?>&nbsp;: <?php echo link_to($parlementaire->nom, 'parlementaire/show?slug='.$parlementaire->slug); ?> (<?php echo $parlementaire->getStatut(1); ?>)</li>
+<li><?php
+if ($num == 0) {echo $parlementaire->getNomNumCirco()." - "; } 
+echo link_to($parlementaire->getNumCircoString(1),
+        '@list_parlementaires_circo?search='.
+        $parlementaire->getNumDepartement().'-'.$parlementaire->num_circo) ;
+?>
+&nbsp;:
+<?php
+echo link_to($parlementaire->nom, 'parlementaire/show?slug='.$parlementaire->slug); 
+?>
+(<?php echo $parlementaire->getStatut(1); ?>)</li>
 <?php endforeach ; ?>
 </ul>
 <?php endif; ?>
