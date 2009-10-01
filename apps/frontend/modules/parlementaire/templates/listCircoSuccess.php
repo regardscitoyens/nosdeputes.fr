ec34c6eb7b2cf9d04ddd089180bd69ebe881e4eb
RouxRC <b.ooghe@gmail.com>
2009-10-01 18:57:37 +0000

    chiffres
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/trunk/project@827 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

--- apps/frontend/modules/parlementaire/templates/listCircoSuccess.php
+++ apps/frontend/modules/parlementaire/templates/listCircoSuccess.php
@@ -13,7 +13,8 @@
 <?php else : ?>
 <?php $nResults = count($parlementaires);
  if ($num != 0 && $nResults != 0) : ?>
- <?php if (preg_match('/\d[a-z]/i', $num)) $fixednum = '0'.$num; else $fixednum = sprintf('%03d',$num);
+ <?php  if (preg_match('/^\d$/', $num)) $num = sprintf("%02d",$num);
+  if (preg_match('/\d[a-z]/i', $num)) $fixednum = '0'.$num; else $fixednum = sprintf('%03d',$num);
   include 'circonscriptions/'.$fixednum.'.html'; ?>
 <?php endif; ?>
 <h1>Les députés par circonscriptions</h1>
@@ -24,7 +25,7 @@ $sf_response->setTitle('Les députés par circonscriptions');
   <?php else : ?>
 <p>
 <?php echo $circo; ?>
-<?php if ($num != 0) echo ' ('.strtoupper($fixednum).')'; 
+<?php if ($num != 0) echo ' ('.strtoupper($num).')'; 
 if ($num_circo > 0) echo  ', '.$parlementaires[0]->getNumCircoString(1).':';
 else echo '&nbsp;:';
 ?>
