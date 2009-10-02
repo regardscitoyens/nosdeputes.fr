7b1a005c7c4864880e6cdbfc2046078fa7092580
Gabriel Kerneis <gabriel@kerneis.info>
2009-10-02 08:25:13 +0000

    batch/circonscriptions/install_maps.sh utilise url_for
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/trunk/project@830 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

--- batch/circonscriptions/install_maps.sh
+++ batch/circonscriptions/install_maps.sh
@@ -1,6 +1,7 @@
 #!/bin/sh -ev
 rm -rf ../../web/images/circonscriptions \
-../../apps/frontend/modules/parlementaire/templates/circonscriptions
+../../apps/frontend/modules/parlementaire/templates/circonscriptions php
 cp -r png ../../web/images/circonscriptions
-cp -r html ../../apps/frontend/modules/parlementaire/templates/circonscriptions
-
+cp -r html php
+sed -i 's/circonscription?search=\(......\)/<?php echo url_for("@list_parlementaires_circo?search=\1");?>/' php/*
+cp -r php ../../apps/frontend/modules/parlementaire/templates/circonscriptions
