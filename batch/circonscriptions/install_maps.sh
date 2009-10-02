f19f0356d331bcf95426309505ca7c244a2af5a1
Regards Citoyens <contact@regardscitoyens.org>
2009-10-02 09:25:13 +0000

    fichiers dans web plutot que dans apps
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/trunk/project@832 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

--- batch/circonscriptions/install_maps.sh
+++ batch/circonscriptions/install_maps.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -ev
 rm -rf ../../web/images/circonscriptions \
-../../apps/frontend/modules/parlementaire/templates/circonscriptions php
+../../web/circonscriptions php
 cp -r png ../../web/images/circonscriptions
 cp -r html php
 sed -i 's/circonscription?search=\(......\)/<?php echo url_for("@list_parlementaires_circo?search=\1");?>/' php/*
-cp -r php ../../apps/frontend/modules/parlementaire/templates/circonscriptions
+cp -r php ../../web/circonscriptions
