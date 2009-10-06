205e434bfb8992051b34eec0393be931dd47405e
Regards Citoyens <contact@regardscitoyens.org>
2009-10-06 16:10:56 +0000

    Affiche l'activité des commentateurs
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/trunk/project@862 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

--- apps/frontend/modules/citoyen/templates/_shortCitoyen.php
+++ apps/frontend/modules/citoyen/templates/_shortCitoyen.php
@@ -3,4 +3,6 @@ if (!$user) {
   echo 'Anonyme';
   return ;
  }
-echo '<a href="'.url_for('@citoyen?slug='.$user->slug).'">'.$user->login.'</a>';
+echo '<a href="'.url_for('@citoyen?slug='.$user->slug).'">'.$user->login;
+if (isset($user->activite) && $user->activite != "") echo ' ('.$user->activite.')';
+echo '</a>';
