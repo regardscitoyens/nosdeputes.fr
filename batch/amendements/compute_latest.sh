89957398a6b25d3cc4ec90755a2da522830e71cc
Regards Citoyens <contact@regardscitoyens.org>
2009-10-07 13:37:21 +0000

    Centralisation de la conf db
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/trunk/project@873 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

--- batch/amendements/compute_latest.sh
+++ batch/amendements/compute_latest.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-MYSQLID="-u cpc -pM_O_T__D_E__P_A_S_S_E"
-DBNAME="cpc"
+. ../../bin/db.inc
+
 cat liste_sort_indefini.sql | mysql $MYSQLID $DBNAME | grep -v source > liste_sort_indefini.txt
+
 rm -f html/*
+
 perl download_amendements.pl > /tmp/download_amendements.log
+
 for file in html/*; do 
 	fileout=$(echo $file | sed 's/html/json/' | sed 's/\.asp/\.xml/')
 	perl cut_amdmt.pl $file > $fileout
--- batch/questions/compute_latest.sh
+++ batch/questions/compute_latest.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-MYSQLID="-u cpc -pM_O_T__D_E__P_A_S_S_E"
-DBNAME="cpc"
+. ../../bin/db.inc
+
 cat dernier_numero.sql | mysql $MYSQLID $DBNAME | grep -v numero > dernier_numero.txt
 cat liste_sans_reponse.sql | mysql $MYSQLID $DBNAME | grep -v source > liste_sans_reponse.txt
+
 rm -f html/*
+
 #log cette partie très verbeuse
 perl download_questions.pl > /tmp/download_questions.log
 
