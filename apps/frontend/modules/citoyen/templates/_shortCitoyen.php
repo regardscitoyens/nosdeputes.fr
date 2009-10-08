b7cb28bfe5eacce310be8e792f3bb8c9cac0e96d
bjperson <brice@ideeslibres.org>
2009-10-08 11:49:24 +0000

    supression pour pb d'encodage
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/trunk/project@882 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

--- apps/frontend/modules/citoyen/templates/_shortCitoyen.php
+++
@@ -1,9 +0,0 @@
-<?php
-if (!$user) {
-  echo 'Anonyme';
-  return ;
- }
-echo '<a href="'.url_for('@citoyen?slug='.$user->slug).'">'.$user->login;
-if (isset($user->activite) && $user->activite != "") echo ' ('.$user->activite.')';
-echo '</a>';
- 
