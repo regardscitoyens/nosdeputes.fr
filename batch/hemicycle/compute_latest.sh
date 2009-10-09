1b7a72b362f31acdc7a27b76488c228fb035a40e
Tangui Morlier <tangui@tangui.eu.org>
2009-10-09 19:54:24 +0000

    La page des cri est plus réactive que cette des recherches
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/trunk/project@888 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

--- batch/hemicycle/compute_latest.sh
+++ batch/hemicycle/compute_latest.sh
@@ -1,4 +1,9 @@
-for file in $(perl download_via_recherche.pl); do
+#!/bin/bash
+
+#SCRIPT=download_via_recherche.pl
+SCRIPT=download_hemicycle.pl
+
+for file in $(perl $SCRIPT); do
 	perl parse_hemicycle.pl html/$file > out/$file ;
 	echo out/$file done;
 done
--- batch/hemicycle/download_hemicycle.pl
+++ batch/hemicycle/download_hemicycle.pl
@@ -4,17 +4,16 @@ use WWW::Mechanize;
 use HTML::TokeParser;
 
 @url = (
-    "http://www.assemblee-nationale.fr/13/cri/2008-2009/",
+    "http://www.assemblee-nationale.fr/13/cri/2009-2010/",
 );
 
 $a = WWW::Mechanize->new();
 
 foreach $url (@url) {
-    print "url: $url\n";
     $a->get($url);
     $content = $a->content;
     $p = HTML::TokeParser->new(\$content);
-    
+    $cpt = 0; 
     while ($t = $p->get_tag('a')) {
 	$txt = $p->get_text('/a');
 	if ($txt =~ /\d+[\Serm]+\s+\S+ance/i) {
@@ -22,7 +21,15 @@ foreach $url (@url) {
 	    $file = $a->uri();
 	    $file =~ s/\//_/gi;
 	    $file =~ s/\#.*//;
-	    print "seance: $file\n";
+	    #on ne peut pas quitter dès le premier, seulement au bout de 
+	    #trois fois on est sur qu'il n'y a pas de nouveaux fichiers
+	    if (-s "html/$file") {
+		$cpt++;
+		exit if ($cpt > 3);
+		break;
+	    }
+	    $cpt = 0;
+	    print "$file\n";
 	    open FILE, ">:utf8", "html/$file";
 	    print FILE $a->content;
 	    close FILE;
