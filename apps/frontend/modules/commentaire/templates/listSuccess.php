97d786e0703b223675006b6baf582047fff324f0
Tangui Morlier <tangui@tangui.eu.org>
2009-10-14 17:12:55 +0000

    Ajout d'une icone rss sur les commentaires
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/trunk/project@899 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

--- apps/frontend/modules/commentaire/templates/listSuccess.php
+++ apps/frontend/modules/commentaire/templates/listSuccess.php
@@ -1,4 +1,4 @@
-<h1>Les derniers commentaires</h1>
+<h1>Les derniers commentaires<span class="rss"><a href="<?php echo url_for('@commentaires_rss'); ?>"><img src="/images/xneth/rss.png" alt="Flux rss"/></a></span></h1>
 <div class="last_commentaires">
 <?php echo include_component('commentaire', 'pager', array('query_commentaires' => $comments, 'partial' => 'Trunc'));  ?>
 </div>
