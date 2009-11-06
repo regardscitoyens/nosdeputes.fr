c968508ef89b80a0830d656d444b55c6c1391b2b
Gabriel Kerneis <gabriel@kerneis.info>
2009-11-06 07:16:41 +0000

    Removing deprecated batch/circonscriptions
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/trunk/project@938 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

--- batch/circonscriptions/install_maps.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh -ev
-rm -rf ../../web/images/circonscriptions \
-../../web/circonscriptions php
-cp -r png ../../web/images/circonscriptions
-cp -r html php
-sed -i 's/circonscription?search=\(......\)/<?php echo url_for("@redirect_parlementaires_circo?code=\1");?>/' php/*
-cp -r php ../../web/circonscriptions
