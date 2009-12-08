58db361ad5e208c04a2d9780c77a5dc53fa08823
Regards Citoyens <contact@regardscitoyens.org>
2009-12-08 12:14:48 +0000

    Gestion des ordonnances
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/trunk/project@986 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

--- apps/frontend/modules/loi/templates/_alinea.php
+++ apps/frontend/modules/loi/templates/_alinea.php
@@ -8,9 +8,12 @@ else
 echo '" id="alinea_'.$a->texteloi_id.'-'.$slug_article.'-'.$a->numero.'"><td class="alineanumero"><p>'.$a->numero.'.</p></td><td class="alineatexte">';
 $s = $a->texte;
 $s = preg_replace('/(articles? (L?)\.? ?([0-9.\-a-z]+)) (de la loi (n° *[\d\-]+ du \d+e?r? \S+ \d{4}))/i', '<a href="/redirect/loi/\\5/\\2\\3">\\1</a> \\4', $s);
+$s = preg_replace('/(articles? (L?)\.? ?([0-9.\-a-z]+)( <i>([^<]*)<\/i>)?) de (l[^ ]*(ordonnance n° *[\d\-]+ du \d+e?r? \S+ \d{4}))/i', '<a href="/redirect/loi/\\7/\\2\\3\\5">\\1</a> \\6', $s);
 $s = preg_replace('/(articles? (L?)\.? ?([0-9.\-a-z]+ ?[A-Z]?)) (du code ([^,.»]+))([,.»]| est| et au| sont)/i', '<a href="/redirect/loi/\\5/\\2\\3">\\1</a> \\4\\6', $s);
+if ($a->ref_loi)
 $s = preg_replace('/(articles? (L?)\.? ?([0-9.\-]+))/i', '<a href="/redirect/loi/'.preg_replace('/^(code|livre) */', '', $a->ref_loi).'/\\2\\3">\\1</a>', $s);
 $s = preg_replace('/(loi (n° *[\d\-]+ du \d+e?r? \S+ \d{4}))([^"\/])/', '<a href="/redirect/loi/\\2">\\1</a>\\3', $s);
+$s = preg_replace('/(l[^ \/]*)(ordonnance (n° *[\d\-]+ du \d+e?r? \S+ \d{4}))/i', '\\1<a href="/redirect/loi/\\2">\\2</a>', $s);
 if ($a->ref_loi) 
 $s = preg_replace('/('.$a->ref_loi.')([^"\/])/', '<a href="/redirect/loi/\\1">\\1</a>\\2', $s);
 $s = preg_replace('/\s+(:|;|!|\?|»|\-)/', '&nbsp;\1', $s);
