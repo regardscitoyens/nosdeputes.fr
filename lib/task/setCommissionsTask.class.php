34ad553cc2ddfe168e515f9841dd178150bae96b
Regards Citoyens <contact@regardscitoyens.org>
2010-01-04 22:04:43 +0000

    evitons que ca cause des problemes si quelqu'un joue avec
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/trunk/project@1009 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

--- lib/task/setCommissionsTask.class.php
+++ lib/task/setCommissionsTask.class.php
@@ -13,26 +13,11 @@ class setCommissionsTask extends sfBaseTask {
     $manager = new sfDatabaseManager($this->configuration);
     $option = Doctrine::getTable('VariableGlobale')->findOneByChamp('commissions');
 
-    $commissions = array(
-
-"comité d'évaluation et de contrôle, des politiques publiques" => "comité d’évaluation et de contrôle des politiques publiques",
-"commission chargée des affaires européennes" => "commission des affaires européennes",
-"commission des affaires culturelles et de l’éducation" => "commission des affaires culturelles et de l'éducation",
-"commission des affaires économiques" => "commission des affaires économiques, de l’environnement et du territoire",
-"commission des affaires culturelles, familiales et sociales" => "commission des affaires culturelles et de l'éducation",
-"commission des affairesétrangères" => "commission des affaires étrangères",
-"commission des finances, de l’économie générale et du contrôle budgétaire" => "commission des finances, du contrôle budgétaire et des comptes économiques de la nation",
-"commission des finances, de l’économie générale et du plan" => "commission des finances, du contrôle budgétaire et des comptes économiques de la nation",
-"commission des lois constitutionnelles, de la législation et de l’administration générale de la république" => "commission des lois constitutionnelles, de législation, du suffrage universel, du règlement et d’administration générale",
-"commission spéciale chargée de vérifier et d’apurer les comptes" => "commission spéciale chargée de vérifier et d’apurer les comptes de la nation",
-"commission spéciale chargée d’examiner le projet de loi organique relatif à la nomination des présidents des sociétés de l’audiovisuel public et le projet de loi sur le service public de la télévision" => "commission spéciale chargée d’examiner le projet de loi organique relatif à la nomination des présidents des sociétés france télévisions, radio france et de la société en charge de l’audiovisuel extérieur de la france et le projet de loi",
-"délégation aux droits des femmes et à l’égalité des chances entre les hommes et les femmes" => "délégation aux droits des femmes et l’égalité des chances entre les hommes et les femmes"
-
-    );
+ //   $commissions = array();
 
 
-    $option->setValue(serialize($commissions));
-    $option->save();
+//    $option->setValue(serialize($commissions));
+//    $option->save();
     print_r(unserialize($option->getValue()));
   }
 }
