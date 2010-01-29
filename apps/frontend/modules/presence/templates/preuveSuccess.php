89dc06bd572565fca3074dd94929448a9319cdfb
Regards Citoyens <contact@regardscitoyens.org>
2010-01-29 15:15:32 +0000

    lien jo pour preuves
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/trunk/project@1044 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

--- apps/frontend/modules/presence/templates/preuveSuccess.php
+++ apps/frontend/modules/presence/templates/preuveSuccess.php
@@ -3,5 +3,6 @@ $sf_response->setTitle($titre); ?>
 <h1><?php echo $titre; ?></h1>
 <ul><?php foreach($preuves as $preuve) { ?>
   <li><?php if (preg_match('/http/', $preuve->source)) echo link_to('Compte-rendu de séance', $preuve->source);
+       else if ($preuve->type == 'jo' && preg_match('/(\d{2}\/\d{2}\/\d{4})$/', $preuve->source, $match)) echo '<a href="http://www.journal-officiel.gouv.fr/users.php?date_jo='.$match[1].'">'.$preuve->source.'</a>';
        else echo $preuve->source; ?></li>
 <?php } ?></ul>
