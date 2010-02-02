7126821d86157decae801a243c0b887dac89615a
Tangui Morlier <tangui@tangui.eu.org>
2010-02-02 17:19:22 +0000

    Warnings pas très beaux virés
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/trunk/project@1061 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

--- apps/frontend/modules/intervention/templates/tagSuccess.php
+++ apps/frontend/modules/intervention/templates/tagSuccess.php
@@ -1,10 +1,10 @@
 <?php 
 $options = array('intervention_query' => $query, 'highlight' => $tags);
 $titre = 'Interventions sur <em>"'.implode(', ', $tags).'"</em>';
-$sf_response->setTitle(strip_tags($titre).' de '.$parlementaire->nom);
 if (isset($parlementaire)) {
-echo include_component('parlementaire', 'header', array('parlementaire' => $parlementaire, 'titre' => $titre));
-$options = array_merge($options, array('nophoto' => true)); 
+  $sf_response->setTitle(strip_tags($titre).' de '.$parlementaire->nom);
+  echo include_component('parlementaire', 'header', array('parlementaire' => $parlementaire, 'titre' => $titre));
+  $options = array_merge($options, array('nophoto' => true)); 
 } else {
   $sf_response->setTitle(strip_tags($titre)); ?>
 <h1><?php echo $titre; ?></h1>
