5c24d5e2aa852d50d1942005025916f0734645dd
Regards Citoyens <contact@regardscitoyens.org>
2010-02-08 16:40:02 +0000

    s
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/trunk/project@1091 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

--- apps/frontend/modules/amendement/templates/findSuccess.php
+++ apps/frontend/modules/amendement/templates/findSuccess.php
@@ -3,7 +3,7 @@
 <?php $sf_response->setTitle($titre);
   if (!isset($amendements)) {
     if ($sf_request->getParameter('numero') == 'all' || $sf_request->getParameter('numero') == 'new') {
-	echo "<p>Trié par : ";
+	echo "<p>Triés par : ";
 	if ($sf_request->getParameter('numero') == 'new') {
 	echo link_to("numéro d'attribution", "@find_amendements_by_loi_and_numero?loi=".implode(',', $lois)."&numero=all");
 	echo " - <b>date de dépôt</b>";
@@ -12,6 +12,7 @@
 	echo " - ";
 	echo link_to("date de dépôt", "@find_amendements_by_loi_and_numero?loi=".implode(',', $lois)."&numero=new");
 	}
+	echo "</p>
     }
     echo include_component('amendement', 'pagerAmendements', array('amendement_query' => $amendements_query, 'lois' => $lois));
   }
