6fba50874b02516ede2b35b3d6c978794f64dc05
Regards Citoyens <contact@regardscitoyens.org>
2010-02-08 16:36:29 +0000

    On indiq la possiblité de tri das le template
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/trunk/project@1090 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

--- apps/frontend/modules/amendement/templates/findSuccess.php
+++ apps/frontend/modules/amendement/templates/findSuccess.php
@@ -1,8 +1,20 @@
 <?php $titre = 'Recherche d\'amendements'; ?>
 <h1><?php echo $titre; ?></h1>
 <?php $sf_response->setTitle($titre);
-  if (!isset($amendements))
+  if (!isset($amendements)) {
+    if ($sf_request->getParameter('numero') == 'all' || $sf_request->getParameter('numero') == 'new') {
+	echo "<p>Trié par : ";
+	if ($sf_request->getParameter('numero') == 'new') {
+	echo link_to("numéro d'attribution", "@find_amendements_by_loi_and_numero?loi=".implode(',', $lois)."&numero=all");
+	echo " - <b>date de dépôt</b>";
+	}else{
+	echo "<b>numéro d'attribution</b>";
+	echo " - ";
+	echo link_to("date de dépôt", "@find_amendements_by_loi_and_numero?loi=".implode(',', $lois)."&numero=new");
+	}
+    }
     echo include_component('amendement', 'pagerAmendements', array('amendement_query' => $amendements_query, 'lois' => $lois));
+  }
   else { ?>
 <?php if (!count($amendements)) { ?>
 <p>Nous n'avons pas trouvé d'amendement correspondant à votre recherche.</p>
