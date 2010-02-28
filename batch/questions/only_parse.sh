524bfcc67e34942327a397efb305f9399aadc75f
Regards Citoyens <contact@regardscitoyens.org>
2010-02-28 01:04:56 +0000

    parse all questions
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/trunk/project@1133 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

--- batch/questions/only_parse.sh
+++ batch/questions/only_parse.sh
@@ -3,5 +3,4 @@
 for file in wget/* ; do
 #   perl cut_quest.pl $file 1 > $(echo $file | sed 's/html/input/'  | sed 's/wget/input/'| sed 's/htm/xml/')
     python parse.py $file > $(echo $file | sed 's/wget/input/'| sed 's/htm/xml/')
-    break;
 done
