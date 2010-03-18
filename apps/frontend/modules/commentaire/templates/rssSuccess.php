e1843282a9d03a9f3b66ca30ee6f82660bd2b85b
Regards Citoyens <contact@regardscitoyens.org>
2010-03-18 11:14:01 +0000

    meilleure présentation rss comments
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/trunk/project@1149 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

--- apps/frontend/modules/commentaire/templates/rssSuccess.php
+++ apps/frontend/modules/commentaire/templates/rssSuccess.php
@@ -6,12 +6,13 @@ else $feed->setTitle($titre);
 $feed->setLink('http://'.$_SERVER['HTTP_HOST'].url_for(preg_replace('/_rss/', '', $linkrss)));
 foreach($comments as $c)
 {
+  $auteur = $c->getCitoyen()->login;
   $item = new sfFeedItem();
   if (isset($presentation))
-    $item->setTitle($c->getPresentation($presentation));
+    $item->setTitle($c->getPresentation($presentation).', '.$auteur.' a dit');
   else $item->setTitle($c->getPresentation());
-  $item->setLink('http://'.$_SERVER['HTTP_HOST'].url_for($c->getLien()));
-  $item->setAuthorName($c->getCitoyen()->login);
+  $item->setLink('http://'.$_SERVER['HTTP_HOST'].url_for($c->getLien()).'#commentaire_'.$c->id);
+  $item->setAuthorName($auteur);
   $item->setPubdate(strtotime($c->created_at));
   $item->setUniqueId('Commentaire'.$c->id);
   $item->setDescription(utf8_encode(utf8_decode(strip_tags($c))));
