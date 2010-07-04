6676d0bd3b72b3b3efd840fa4616adaa22583fb9
Tangui Morlier <tangui@tangui.eu.org>
2010-07-04 11:38:59 +0000

    Les présents issus du cri
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/trunk/project@1225 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

---
+++ batch/commission/parse_presents.pl
@@ -0,0 +1,142 @@
+#!/usr/bin/perl
+
+$file = $url = shift;
+#use HTML::TokeParser;
+$url =~ s/^[^\/]+\///;
+$url =~ s/_/\//g;
+$source = $url;
+
+if ($url =~ /\/(\d+)-(\d+)\//) {
+    $session = '20'.$1.'20'.$2;
+}
+
+open(FILE, $file) ;
+@string = <FILE>;
+$string = "@string";
+close FILE;
+
+$string =~ s/<\/?b>/|/g;
+$string =~ s/<\/?i>/\//g;
+$string =~ s/\r//g;
+
+$mois{'janvier'} = '01';
+$mois{'février'} = '02';
+$mois{'mars'} = '03';
+$mois{'avril'} = '04';
+$mois{'mai'} = '05';
+$mois{'juin'} = '06';
+$mois{'juillet'} = '07';
+$mois{'août'} = '08';
+$mois{'septembre'} = '09';
+$mois{'octobre'} = '10';
+$mois{'novembre'} = '11';
+$mois{'décembre'} = '12';
+
+
+#utf8::decode($string);
+#
+#$p = HTML::TokeParser->new(\$string);
+#
+#while ($t = $p->get_tag('p', 'h1', 'h5')) {
+#    print "--".$p->get_text('/'.$t->[0])."\n";
+#}
+#
+#exit;
+$cpt = 0;
+sub checkout {
+    foreach $depute (@presents) {
+	$depute =~ s/^\s+//;
+	$depute =~ s/\s+$//;
+	print '{"reunion": "'.$date.'", "session": "'.$heure.'", "commission": "'.$commission.'", "depute": "'.$depute.'", "source": "'.$source.'"}'."\n";
+    }
+    return ;
+    $out =  '{"commission": "'.$commission.'", "intervention": "'.$intervention.'", "timestamp": "'.$cpt.'", "date": "'.$date.'", "source": "'.$source.'", "heure":"'.$heure.'", "session": "'.$session.'", ';
+    if ($intervenant) {
+	if ($intervenant =~ s/ et M[mes\.]* (.*)//) {
+	    print $out.'"intervenant": "'.$1."\"}\n";
+	}
+	print $out.'"intervenant": "'.$intervenant.'", "fonction": "'.$inter2fonction{$intervenant}."\"}\n";
+    }elsif($intervention) {
+	print $out.'"intervenant":"'."\"}\n";
+    }else {
+	return ;
+    }
+    $commentaire = "";
+    $intervenant = "";
+    $intervention = "";
+}
+
+$string =~ s/\r//g;
+$string =~ s/&nbsp;/ /g;
+$string =~ s/&#8217;/'/g;
+$string =~ s/&#339;|œ+/oe/g;
+$string =~ s/\|(\W+)\|/$1/g;
+$string =~ s/ission d\W+information/ission d'information/gi;
+$string =~ s/à l\W+aménagement /à l'aménagement /gi;
+$majIntervenant = 0;
+$body = 0;
+$present = 0;
+$string =~ s/<br>\n//gi;
+
+# Le cas de <ul> qui peut faire confondre une nomination à une intervention : 
+#on vire les paragraphes contenus et on didascalise
+
+
+$string =~ s/<\/?ul>//gi;
+
+#print $string; exit;
+
+foreach $line (split /\n/, $string)
+{
+    if ($line =~ /<body[^>]*>/) {
+	$body = 1;
+    }
+    next unless ($body);
+    if ($line =~ /\<[a]/i) {
+	if ($line =~ /<a name=["']([^"']+)["']/) {
+	    $source = $url."#$1";
+	}elsif($line =~ /<a[^>]+>([^<]+)</) {
+	    $test = $1;
+	    if (!$commission && $test =~ /Commission|mission/) {
+		$test =~ s/ Les comptes rendus de la m/M/;
+		$test =~ s/^ +//;
+		$commission = $test;
+	    }
+	}
+    }
+    if ($line =~ /<h[1-9]+/i) {
+	if (!$date && $line =~ /SOMdate|\"seance\"|h2/) {
+	    if ($line =~ /SOMdate|Lundi|Mardi|Mercredi|Jeudi|Vendredi|Samedi|Dimanche/i) {
+	      if ($line =~ /\w+\s+(\d+)[erme]*\s+([^\s\d]+)\s+(\d+)/i) {
+		$date = sprintf("%04d-%02d-%02d", $3, $mois{lc($2)}, $1);
+	      }
+	    }
+	}elsif ($line =~ /SOMseance|"souligne_cra"/i) {
+	    if ($line =~ /(\d+)\s*(h|heures?)\s*(\d*)/i) {
+		$heure = sprintf("%02d:%02d", $1, $3 || "00");
+	    }
+	}elsif(!$commission && $line =~ /groupe|commission|mission|délégation|office|comité/i) {
+	    if ($line =~ /[\>\|]\s*((Groupe|Com|Miss|Délé|Offic)[^\>\|]+)[\<\|]/) {
+		$commission = $1;
+		$commission =~ s/\s*$//;
+	    }
+	}elsif($line =~ /SOMnumcr/i) {
+	    if ($line =~ /\s0*(\d+)/ && $1 > 1) {
+		$cpt = $1*1000000;
+	    }
+	}
+    }
+    if ($present) {
+	$line =~ s/<[^>]+>//gi;
+	$line =~ s/&nbsp;/ /gi;
+	$line =~ s/M[me\.]+ //gi;
+	if ($line =~ s/\/?(Présents|Assistaient également à la réunion)\W+//) {
+	    push @presents, split /, /, $line; #/
+            print join('/', @presents);
+	}
+    }
+    if ($line =~ /Membres présents ou excusés/) {
+	$present = 1;
+    }
+}
+checkout();
