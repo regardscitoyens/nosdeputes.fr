ee50df5647e466090c6d792980bcb6b47b00199d
Regards Citoyens <contact@regardscitoyens.org>
2010-07-25 11:29:24 +0000

    oups
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/trunk/project@1251 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

--- apps/frontend/modules/presence/templates/preuveSuccess.php
+++ apps/frontend/modules/presence/templates/preuveSuccess.php
@@ -4,6 +4,6 @@ $sf_response->setTitle($titre); ?>
 <ul><?php foreach($preuves as $preuve) { ?>
   <li><?php if ($preuve->type === 'compte-rendu') echo 'Liste des présences au bas du ';
        if ($preuve->type === 'compte-rendu' || $preuve->type === 'intervention') echo link_to('Compte-rendu de séance', $preuve['source']);
-       else if ($preuve->type === 'jo' && (preg_match('/(\d{2}\/\d{2}\/\d{4})$/', $preuve->source, $match)) echo '<a href="http://www.journal-officiel.gouv.fr/users.php?date_jo='.$match[1].'">'.$preuve->source.'</a>';
+       else if ($preuve->type === 'jo' && (preg_match('/(\d{2}\/\d{2}\/\d{4})$/', $preuve->source, $match))) echo '<a href="http://www.journal-officiel.gouv.fr/users.php?date_jo='.$match[1].'">'.$preuve->source.'</a>';
        else echo $preuve->source; ?></li>
 <?php } ?></ul>
