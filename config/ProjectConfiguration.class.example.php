ada39b616a84d1e7d7447e4aadf4843bdf52fe3b
Regards Citoyens <contact@regardscitoyens.org>
2010-08-22 15:58:03 +0000

    fichiers d'install pour symfon1.4
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/trunk/project@1270 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

--- config/ProjectConfiguration.class.example.php
+++ config/ProjectConfiguration.class.example.php
@@ -8,6 +8,6 @@ class ProjectConfiguration extends sfProjectConfiguration
   public function setup()
   {
     // for compatibility / remove and enable only the plugins you want
-    $this->enableAllPluginsExcept(array('sfPropelPlugin', 'sfCompat10Plugin'));
+    $this->enableAllPluginsExcept(array('sfPropelPlugin'));
   }
 }
