2e38cec9291ef2893787d823a0b5de56716b1d49
Regards Citoyens <contact@regardscitoyens.org>
2010-08-26 10:34:13 +0000

    titre simplifions
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/trunk/project@1295 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

--- apps/frontend/modules/section/templates/_simplifions.php
+++ apps/frontend/modules/section/templates/_simplifions.php
@@ -1,5 +1,5 @@
 <div>
-<h2>Les dossiers en discussion sur NosDéputés.fr</h2>
+<h2>Les dossiers en discussion sur "Simplifions la loi 2.0"</h2>
 <ul>
 <?php foreach($lois as $l) : ?>
 <li><?php echo link_to(strip_tags($l['titre']), "@loi?loi=".$l['texteloi_id']); if ($l['nb_commentaires'] > 0) { echo ' ('.$l['nb_commentaires'].' commentaire'; if ($l['nb_commentaires'] > 1) echo 's'; echo ')'; } ?></li>
