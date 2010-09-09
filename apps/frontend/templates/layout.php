25b284707deed2852751a9dd6d1a16298fad8ef2
bjperson <brice@ideeslibres.org>
2010-09-09 10:17:26 +0000

    oups
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/trunk/project@1339 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

--- apps/frontend/templates/layout.php
+++ apps/frontend/templates/layout.php
@@ -141,17 +141,5 @@ $menu_citoyen = $selectcitoyen;
         <a href="http://www.regardscitoyens.org"><?php echo image_tag($style.'/bottom_qui.png', array('alt' => 'Qui sommes-nous')); ?></a>
       </div>
     </div>
-<script type="text/javascript">
-
-</script>
-<script type="text/javascript">
-var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
-</script>
-<script type="text/javascript">
-try {
-var pageTracker = _gat._getTracker("UA-10423931-2");
-pageTracker._trackPageview();
-} catch(err) {}</script>
   </body>
 </html>
