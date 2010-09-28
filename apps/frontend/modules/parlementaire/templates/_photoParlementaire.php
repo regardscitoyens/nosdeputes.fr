10901b71935ee32aaf91b6906b217b988bee2f72
Tangui Morlier <tangui@tangui.eu.org>
2010-09-28 22:30:22 +0000

    Proction contre les faux parlementaires
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/trunk/project@1486 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

--- apps/frontend/modules/parlementaire/templates/_photoParlementaire.php
+++ apps/frontend/modules/parlementaire/templates/_photoParlementaire.php
@@ -1,3 +1,3 @@
 <?php 
-
+if ($parlementaire->slug)
 echo '<img title="'.$parlementaire->nom.'<br/>(Groupe parlementaire : '.$parlementaire->groupe_acronyme.')" src="'.url_for('@resized_photo_parlementaire?height='.$height.'&slug='.$parlementaire->slug).'" class="jstitle photo_fiche" alt="Photo de '.$parlementaire->nom.'"/>';
