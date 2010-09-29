617ae1de3765439f3f44ecfaea7733ff7ebfbf6b
RouxRC <b.ooghe@gmail.com>
2010-09-29 01:50:10 +0000

    syntaxe plus propre pour routing et images, correction sizee timer solr pour nojs
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/trunk/project@1499 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

--- apps/frontend/modules/parlementaire/templates/showSuccess.php
+++ apps/frontend/modules/parlementaire/templates/showSuccess.php
@@ -73,17 +73,17 @@
       <div class="b_d_infos">
       <h2>Travaux législatifs</h2>
       <h3>Ses derniers dossiers</h3>
-      <?php echo include_component('section', 'parlementaire', array('parlementaire' => $parlementaire, 'limit' => 5, 'order' => 'date')); ?>
+      <?php echo include_component('section', 'parlementaire', array('parlementaire' => $parlementaire, 'limit' => 4, 'order' => 'date')); ?>
       <p class="suivant"><?php echo link_to('Tous ses dossiers', '@parlementaire_textes?slug='.$parlementaire->slug); ?></p>
       <h3><?php echo link_to('Travaux en commissions','@parlementaire_interventions?slug='.$parlementaire->getSlug().'&type=commission'); ?></h3>
       <h3><?php echo link_to('Travaux en hémicycle','@parlementaire_interventions?slug='.$parlementaire->getSlug().'&type=loi'); ?></h3>
       <h3><?php echo link_to('Toutes ses interventions','@parlementaire_interventions?slug='.$parlementaire->getSlug().'&type=all'); ?></h3>
       <h2>Questions au gouvernement</h2>
       <h3>Ses dernières questions orales</h3>
-       <?php echo include_component('intervention', 'parlementaireQuestion', array('parlementaire' => $parlementaire, 'limit' => 6)); ?>
+       <?php echo include_component('intervention', 'parlementaireQuestion', array('parlementaire' => $parlementaire, 'limit' => 4)); ?>
       <p class="suivant"><?php echo link_to('Toutes ses questions orales','@parlementaire_interventions?slug='.$parlementaire->getSlug().'&type=question'); ?></p>
       <h3>Ses dernières questions écrites</h3>
-       <?php echo include_component('questions', 'parlementaire', array('parlementaire' => $parlementaire, 'limit' => 6)); ?>
+       <?php echo include_component('questions', 'parlementaire', array('parlementaire' => $parlementaire, 'limit' => 4)); ?>
       <p class="suivant"><?php echo link_to('Toutes ses questions écrites','@parlementaire_questions?slug='.$parlementaire->getSlug()); ?></p>
       </div>
     </div>
@@ -96,8 +96,8 @@
       <div class="b_d_infos">
     <h2>Suivre l'activité du député</h2>
 <table width=100% style="text-align: center"><tr>
-       <td><a href="<?php echo url_for('@alerte_parlementaire?slug='.$parlementaire->slug); ?>"><img src="/images/xneth/email.png" alt="Email"><br/>par email</a></td>
-       <td><a href="<?php echo url_for('@parlementaire_rss?slug='.$parlementaire->slug); ?>"><img src="/images/xneth/rss_obliq.png" alt="Flux rss"><br/>par RSS</a></td>
+       <td><a href="<?php echo url_for('@alerte_parlementaire?slug='.$parlementaire->slug); ?>"><?php echo image_tag('xneth/email.png', 'alt="Email"'); ?><br/>par email</a></td>
+       <td><a href="<?php echo url_for('@parlementaire_rss?slug='.$parlementaire->slug); ?>"><?php echo image_tag('xneth/rss_obliq.png', 'alt="Flux rss"'); ?><br/>par RSS</a></td>
 </tr></table>
       <h2>Champ lexical</h2>
       <div style="text-align: justify">
