ab151d2f14f73a71e87953c1a9b2fb48144a6362
RouxRC <b.ooghe@gmail.com>
2010-09-30 16:20:18 +0000

    refacto double hilight
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/trunk/project@1514 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

--- apps/frontend/modules/circonscription/templates/showSuccess.php
+++ apps/frontend/modules/circonscription/templates/showSuccess.php
@@ -6,23 +6,3 @@
 <div class="list_circo">
   <?php include_partial('parlementaire/table', array('deputes' => $parlementaires, 'circo' => $departement_num)); ?>
 </div>
-<script type="text/javascript">
-/* survol du txt */
-$(".list_dep").live("mouseover", function() {
-  dep = $(this).attr("id").substring(3);
-  $("#map"+dep).mouseover();
-})
-$(".list_dep").live("mouseout", function() {
-  dep = $(this).attr("id").substring(3);
-  $("#map"+dep).mouseout();
-})
-/* survol de la map */
-$("area").live("mouseover", function() {
-  dep = $(this).attr("id").substring(3);
-  $("#dep"+dep).css("background-color", "#D1EA74");
-})
-$("area").live("mouseout", function() {
-  dep = $(this).attr("id").substring(3);
-  $("#dep"+dep).css("background-color", "#fff");
-})
-</script>
