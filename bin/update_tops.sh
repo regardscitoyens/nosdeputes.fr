e1d80354a615c30bb44ad118a83e8f5433266feb
RouxRC <b.ooghe@gmail.com>
2010-10-02 21:19:36 +0000

    script d'update de l'API pour cron bimensuellee
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/trunk/project@1537 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

--- bin/update_tops.sh
+++ bin/update_tops.sh
@@ -4,8 +4,14 @@ php symfony top:Deputes
 for month in 06 07 08 09 10 11 12; do
   php symfony top:Deputes 2007-$month-01
 done
-for year in 2008 2009 2010; do
+for year in 2008 2009 2010 2011 2012; do
+  if test $year -gt `date +%Y`; then
+    break
+  fi
   for month in 01 02 03 04 05 06 07 08 09 10 11 12; do
+    if test $month -gt `date +%m`; then
+      break
+    fi
     php symfony top:Deputes $year-$month-01
   done
 done
