caa0c7b26f5e59619bf6eebc987cee21963a62e4
Tangui Morlier <tangui@tangui.eu.org>
2010-10-02 18:47:30 +0000

    Meilleure gestion des sémaphores
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/trunk/project@1528 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

--- lib/model/solr/SolrCommands.class.php
+++ lib/model/solr/SolrCommands.class.php
@@ -7,6 +7,8 @@ class SolrCommands
     if (!file_exists(sfConfig::get('sf_log_dir').'/solr/')) {
       mkdir (sfConfig::get('sf_log_dir').'/solr/');
     }
+    if (!file_exists(sfConfig::get('sf_log_dir').'/solr/commands.log'))
+      touch(sfConfig::get('sf_log_dir').'/solr/commands.log');
     return sfConfig::get('sf_log_dir').'/solr/commands.log';
   }
 
@@ -14,7 +16,6 @@ class SolrCommands
   protected $file = null; 
 
   protected static $instance = null;
-  protected static $semaphore_id = "99999910823498202340982340982340981678";
   public static function getInstance() {
     if (!self::$instance) {
       self::$instance = new SolrCommands();
@@ -22,23 +23,33 @@ class SolrCommands
     return self::$instance;
   }
 
-  public function __construct() {
-    $this->semaphore = sem_get(self::$semaphore_id);
+  public function __destruct() {
+    if ($this->semaphore)
+      $this->unprotect();
   }
 
-  public function __destruct() {
-    sem_remove($this->semaphore); //semble supprimer non seulement notre instance mais le semaphore global :(
+  private function protect() {
+    if (! $this->semaphore) {
+      $id = ftok(self::getFileCommands(), 's');
+      $this->semaphore = sem_get($id);
+    }
+    sem_acquire($this->semaphore);
+  }
+
+  private function unprotect() {
+    sem_release($this->semaphore);
+    sem_remove($this->semaphore);
     $this->semaphore = null;
   }
 
   public function addCommand($status, $json) {
-    sem_acquire($this->semaphore);
+    $this->protect();
     if (! $this->file) {
       $this->file = fopen($this->getFileCommands(), 'a+');
     }
     $str = $status.' : '.json_encode($json)."\n";
     fwrite($this->file, $str, strlen($str));
-    sem_release($this->semaphore);
+    $this->unprotect();
   }
 
   public  function getCommandContent() {
@@ -46,7 +57,7 @@ class SolrCommands
     if (file_exists($lockfile)) {
       return $lockfile;
     }
-    sem_acquire($this->semaphore);
+    $this->protect();
     if ($this->file) {
       fclose($this->file);
       $this->file = null;
@@ -54,13 +65,13 @@ class SolrCommands
     if (!file_exists($this->getFileCommands()))
       touch($this->getFileCommands());
     rename($this->getFileCommands(), $lockfile);
-    sem_release($this->semaphore);
+    $this->unprotect();
     return $lockfile;
   }
   public  function releaseCommandContent() {
-    sem_acquire($this->semaphore);
+    $this->protect();
     unlink($this->getFileCommands().'.lock');
-    sem_release($this->semaphore);
+    $this->unprotect();
   }
 }
 
