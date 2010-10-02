12472289133b83fa19899de401b4050e03e88700
RouxRC <b.ooghe@gmail.com>
2010-10-02 23:40:18 +0000

    correction indexation circos
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/trunk/project@1543 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

---
+++ lib/task/printCircosSolrTask.class.php
@@ -0,0 +1,24 @@
+<?php
+
+class printCircosSolrTask extends sfBaseTask {
+  protected function configure() {
+    $this->namespace = 'print';
+    $this->name = 'CircosSolr';
+    $this->briefDescription = 'print url objets circonscriptions pour Solr';
+    $this->addOption('env', null, sfCommandOption::PARAMETER_OPTIONAL, 'Changes the environment this task is run in', 'test');
+    $this->addOption('app', null, sfCommandOption::PARAMETER_OPTIONAL, 'Changes the environment this task is run in', 'frontend');
+ }
+
+  protected function execute($arguments = array(), $options = array()) {
+    $manager = new sfDatabaseManager($this->configuration);
+    $ct = 1;
+    foreach (Parlementaire::$dptmt_pref as $dep => $pref) {
+      echo "url$ct:\n";
+      echo "  url: \"/circonscription/departement/".str_replace(' ', '_', $dep)."\"\n";
+      echo "  title: \"Les députés ".$pref.(!preg_match("/'/", $pref) ? ' ' : '').$dep."\"\n";
+      echo "  weight: 10\n";
+      $ct++;
+    }
+  }
+}
+
