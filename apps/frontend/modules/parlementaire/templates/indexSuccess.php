46ce1f6a9943fa107eeddc0005c2c3f7e1ff6262
Tangui Morlier <tangui@tangui.eu.org>
2010-10-03 09:57:29 +0000

    Hauteur du graph et tags cloud dynamique
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/trunk/project@1557 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

--- apps/frontend/modules/parlementaire/templates/indexSuccess.php
+++ apps/frontend/modules/parlementaire/templates/indexSuccess.php
@@ -176,15 +176,18 @@
     <p style="text-align: right;"><a href="http://www.nosdeputes.fr/confiseurs/index.php">Lire l'étude</a></p>
     </div>
   </div>
-  <div class="box_repartition aligncenter">
+<div class="box_container">
+  <div class="box_repartition aligncenter"><div style="margin: auto;">
   <h2><span style="margin-right: 5px;"><img alt="activite" src="<?php echo $sf_request->getRelativeUrlRoot(); ?>/images/xneth/ico_graph.png"></span><a href="<?php echo url_for('@top_global'); ?>">Activité parlementaire des 12 derniers mois</a></h2>
   <?php echo include_component('plot', 'newGroupes', array('type' => 'home')); ?>
   <?php // echo include_component('plot', 'groupes', array('plot' => 'total')); ?>
-  </div>
+  </div></div>
+  <div class="table_sep">&nbsp;</div>
   <div class="box_tags">
   <h2><span style="margin-right: 5px;"><img alt="tags" src="<?php echo $sf_request->getRelativeUrlRoot(); ?>/images/xneth/assemblee-nationale.png"></span><?php echo link_to('En ce moment à l\'Assemblée Nationale', '@parlementaires_tags'); ?></h2>
   <?php echo include_component('tag', 'globalActivite'); ?>
   </div>
+</div>
   <div class="clear"></div>
   <div class="box_widget">
     <h2><span style="margin-right: 5px;"><img alt="comments" src="<?php echo $sf_request->getRelativeUrlRoot(); ?>/images/xneth/ico_comment.png"></span><a href="<?php echo url_for('@commentaires'); ?>">Les derniers commentaires</a></h2>
