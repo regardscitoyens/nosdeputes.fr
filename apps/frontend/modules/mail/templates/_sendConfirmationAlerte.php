fb90ec1db2b95f30b0bbb5fcc0544e6b26047332
Regards Citoyens <contact@regardscitoyens.org>
2010-10-04 15:07:42 +0000

    correction message alert
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/trunk/project@1660 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

--- apps/frontend/modules/mail/templates/_sendConfirmationAlerte.php
+++ apps/frontend/modules/mail/templates/_sendConfirmationAlerte.php
@@ -1,9 +1,9 @@
-Nous avons recu une demande d'abonnement pour l'alerte email suivante : « <?php echo $alerte->titre; ?> »
+Nous avons reçu une demande d'abonnement pour l'alerte email suivante : « <?php echo $alerte->titre; ?> »
 
-Si vous êtes bien à l'origine de cette demande, merci de la confirmer en cliquant sur le lien suivant :
+Si vous êtes bien à l'origine de cette demande, merci de confirmer en cliquant sur le lien suivant :
 
 <?php echo sfConfig::get('app_base_url').preg_replace('/symfony\/?/', '', url_for('alerte/confirmation?verif='.$alerte->getVerif())); ?>
 
-Si ce n'est pas le cas, merci d'ignorer ce mail et de nous excuser de vous avoir importuné.
+Si ce n'est pas le cas, vous pouvez ignorer cet mail et nous vous prions de nous excuser pour la gêne occasionnée.
 
-L'équipe de Regards Citoyens, l'association en charge de NosDeputes.fr
+L'équipe de Regards Citoyens, l'association à l'initiative de NosDeputes.fr
