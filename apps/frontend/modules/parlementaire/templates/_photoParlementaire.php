7b726dbfa1f1902121b4f807786b58dfdeb295ac
Tangui Morlier <tangui@tangui.eu.org>
2010-10-04 04:06:12 +0000

    Ajout du flip
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/trunk/project@1651 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

--- apps/frontend/modules/parlementaire/templates/_photoParlementaire.php
+++ apps/frontend/modules/parlementaire/templates/_photoParlementaire.php
@@ -1,3 +1,5 @@
 <?php 
+if (isset($flip) && $flip)
+  $add = '&flip='.$flip;
 if ($parlementaire->slug)
-echo '<img title="'.$parlementaire->nom.' -- (Groupe parlementaire : '.$parlementaire->groupe_acronyme.')" src="'.url_for('@resized_photo_parlementaire?height='.$height.'&slug='.$parlementaire->slug).'" class="jstitle photo_fiche" alt="Photo de '.$parlementaire->nom.'"/>';
+echo '<img title="'.$parlementaire->nom.' -- (Groupe parlementaire : '.$parlementaire->groupe_acronyme.')" src="'.url_for('@resized_photo_parlementaire?height='.$height.'&slug='.$parlementaire->slug.$add).'" class="jstitle photo_fiche" alt="Photo de '.$parlementaire->nom.'"/>';
