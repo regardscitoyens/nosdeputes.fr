d40e7b966dcc267ac805c47df9fb12752ae5e385
Regards Citoyens <contact@regardscitoyens.org>
2010-10-04 22:54:35 +0000

    correction analytics
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/trunk/project@1665 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

--- apps/frontend/templates/layout.php
+++ apps/frontend/templates/layout.php
@@ -139,13 +139,15 @@ $menu_citoyen = $selectcitoyen;
       </div>
     </div>
   </body>
-<script>
+<script type="text/javascript">
 // Google
-var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
-try {
-  var pageTracker = _gat._getTracker("UA-10423931-2");
-  pageTracker._trackPageview();
-} catch(err) {}
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-10423931-2']);
+  _gaq.push(['_trackPageview']);
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
 </script>
 </html>
