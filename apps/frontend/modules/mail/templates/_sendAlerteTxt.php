d6effbb352462ee26796351ac34c3bec7c3c021b
Regards Citoyens <contact@regardscitoyens.org>
2010-10-07 19:48:17 +0000

    Tentative de template
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/trunk/project@1706 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

--- apps/frontend/modules/mail/templates/_sendAlerteTxt.php
+++ apps/frontend/modules/mail/templates/_sendAlerteTxt.php
@@ -1,27 +1,32 @@
 ============ Alerte NosDeputes.fr ============
 
-Voici les dernières alertes de votre abonnement : « <?php echo $alerte->titre; ?> »
+Voici les dernières alertes de votre abonnement : <?php echo $alerte->titre; ?> 
 
 <?php
 foreach ($results['docs'] as $res) 
 {
-  echo "- ".$res['object_name'].' de '.$res['object']->getPersonne()."\n";
+  echo $res['object_name'].' : '.preg_replace('/^ *'.$res['object_name'].'/', '', $res['object']->getTitre())."\n";
+  echo "------------------------------------------------\n";
   $printable = array();
   foreach($res['text'] as $text) {
     if (!preg_match('/=/', $text))
       array_push($printable , $text);
   }
-  echo "  ".implode('...', $printable)."\n";
-  echo sfConfig::get('app_base_url').preg_replace('/symfony\/?/', '', $res['object']->getLink())."\n\n";
+  $text = preg_replace('/ *\n+ */', ' ', implode('...', $printable));
+  if (strlen($text) > 700) {
+	$text = substr($text, 0, 700).'...';
+  }
+  echo "$text\n";
+  echo sfConfig::get('app_base_url').'/'.preg_replace('/symfony\/?/', '', $res['object']->getLink())."\n\n";
 }
 
 ?>
 ===============================================
 Pour éditer cette alerte :
-<?php echo sfConfig::get('app_base_url').preg_replace('/symfony\/?/', '', url_for('alerte/edit?verif='.$alerte->getVerif())); ?>
+<?php echo sfConfig::get('app_base_url').'/'.preg_replace('/symfony\/?/', '', url_for('alerte/edit?verif='.$alerte->getVerif())); ?>
 
 Pour supprimer cette alerte :
-<?php echo sfConfig::get('app_base_url').preg_replace('/symfony\/?/', '', url_for('alerte/delete?verif='.$alerte->getVerif())); ?>
+<?php echo sfConfig::get('app_base_url').'/'.preg_replace('/symfony\/?/', '', url_for('alerte/delete?verif='.$alerte->getVerif())); ?>
 
 L'interface vous permettant de gérer vos alertes :
-<?php echo sfConfig::get('app_base_url').preg_replace('/symfony\/?/', '', url_for('alerte/list')); ?>
+<?php echo sfConfig::get('app_base_url').'/'.preg_replace('/symfony\/?/', '', url_for('alerte/list')); ?>
