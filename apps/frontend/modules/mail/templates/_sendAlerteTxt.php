8f443040a76f593f5bc38da30f0ddd9ab0332b22
RouxRC <b.ooghe@gmail.com>
2010-10-10 20:57:46 +0000

    présentation alertes
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/trunk/project@1715 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

--- apps/frontend/modules/mail/templates/_sendAlerteTxt.php
+++ apps/frontend/modules/mail/templates/_sendAlerteTxt.php
@@ -5,7 +5,14 @@ Voici les dernières alertes de votre abonnement : <?php echo $alerte->titre; ?>
 <?php
 foreach ($results['docs'] as $res) 
 {
-  echo $res['object_name'].' : '.preg_replace('/^ *'.$res['object_name'].'/', '', $res['object']->getTitre())."\n";
+  $titre = $res['object']->getTitre();
+  if ($res['object_name'] === "Commentaire")
+    echo "Commentaire : ";
+  else if ($res['object_name'] === "Section")
+    echo "Dossier : ";
+  else if ($res['object_name'] === "QuestionEcrite")
+    $titre = str_replace('Question', 'Question écrite', $titre);
+  echo $titre."\n";
   echo "------------------------------------------------\n";
   $printable = array();
   foreach($res['text'] as $text) {
@@ -16,6 +23,7 @@ foreach ($results['docs'] as $res)
   if (strlen($text) > 700) {
 	$text = substr($text, 0, 700).'...';
   }
+  $text = str_replace($titre, '', $text);
   echo "$text\n";
   echo sfConfig::get('app_base_url').'/'.preg_replace('/symfony\/?/', '', $res['object']->getLink())."\n\n";
 }
