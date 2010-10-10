5ada45664b09804660dc4a79f8c438c818cc6b4a
RouxRC <b.ooghe@gmail.com>
2010-10-10 19:29:04 +0000

    bulle des députés sans groupe
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/trunk/project@1713 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

--- apps/frontend/modules/parlementaire/templates/_photoParlementaire.php
+++ apps/frontend/modules/parlementaire/templates/_photoParlementaire.php
@@ -2,5 +2,8 @@
 $add='';
 if (isset($flip) && $flip)
   $add = '&flip='.$flip;
+if ($parlementaire->fin_mandat != null)
+  $groupe = "ancien".($parlementaire->sexe == "F" ? "ne" : "")." député".($parlementaire->sexe == "F" ? "e" : "");
+else $groupe = "Groupe parlementaire : ".$parlementaire->groupe_acronyme;
 if ($parlementaire->slug)
-echo '<img title="'.$parlementaire->nom.' -- (Groupe parlementaire : '.$parlementaire->groupe_acronyme.')" src="'.url_for('@resized_photo_parlementaire?height='.$height.'&slug='.$parlementaire->slug.$add).'" class="jstitle photo_fiche" alt="Photo de '.$parlementaire->nom.'"/>';
+echo '<img title="'.$parlementaire->nom.' -- ('.$groupe.')" src="'.url_for('@resized_photo_parlementaire?height='.$height.'&slug='.$parlementaire->slug.$add).'" class="jstitle photo_fiche" alt="Photo de '.$parlementaire->nom.'"/>';
