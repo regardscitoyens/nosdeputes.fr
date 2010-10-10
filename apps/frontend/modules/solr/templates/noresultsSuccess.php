276a3fe088433f87bed635f78edd02f5f943e66d
RouxRC <b.ooghe@gmail.com>
2010-10-10 19:30:35 +0000

    refacto boite alertes pour noresults
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/trunk/project@1714 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

--- apps/frontend/modules/solr/templates/noresultsSuccess.php
+++ apps/frontend/modules/solr/templates/noresultsSuccess.php
@@ -1,7 +1,6 @@
-<div>
+<div class="solr">
+<?php include_partial('solr/follow', array('query' => $query, 'selected' => array(), 'norss' => true, 'zero' => true)); ?>
 <?php include_partial('solr/searchbox'); ?>
 <p>Désolé, nous n'avons pas trouvé de résultat pour votre recherche.</p>
 <?php include_component('parlementaire', 'search', array('query' => $query, 'msg'=>"Peut être recherchiez vous l'un de ces députés :")); ?>
-<p><a href="<?php echo url_for('alerte/create?query='.urlencode($query)); ?>"><?php echo image_tag('xneth/email.png', 'alt="Email :"'); ?>
-Etre alerté par email lorsque cette recherche comportera un résultat</a></p>
 </div>
--- apps/frontend/modules/solr/templates/searchSuccess.php
+++ apps/frontend/modules/solr/templates/searchSuccess.php
@@ -213,21 +213,7 @@ if($graph) {
 ///////////////////// FIN SANS AJAX /////////////////////
 ?>
 </div>
-<div class="options">
-  <div class="mail">
-  <h3 class="aligncenter">S'abonner aux résultats<br/>de cette recherche</h3>
-<?php $args = '';
-  foreach(array_keys($selected) as $k) {
-    if (!is_array($selected[$k]) || $k == 'date')
-      continue;
-    if ($args)
-      $args .= '&';
-    $args.= "$k=".implode(',', array_keys($selected[$k]));
-  } ?>
-<table width=100% style="text-align: center"><tr>
-       <td><a href="<?php echo url_for('alerte/create?filter='.urlencode($args).'&query='.urlencode($query)); ?>"><?php echo image_tag('xneth/email.png', 'alt="Email"'); ?></a><br/><a href="<?php echo url_for('alerte/create?filter='.urlencode($args).'&query='.urlencode($query)); ?>">par email</a></td>
-       <td><a href="<?php $newargs_rss = $selected; $newargs_rss['format']['rss'] = 'rss'; if (isset($newargs_rss['date'])) unset($newargs_rss['date']); if (isset($newargs_rss['sort'])) unset($newargs_rss['sort']); echo url_for(url_search($query, $newargs_rss)); ?>"><?php echo image_tag('xneth/rss_obliq.png', 'alt="Flux rss"'); ?></a><br/><a href="<?php echo url_for(url_search($query, $newargs_rss)); ?>">par RSS</a></td>
-</tr></table></div></div>
+<?php include_partial('solr/follow', array('query' => $query, 'selected' => $selected)); ?>
 <?php endif;
 global $facetName2HumanName;
 $facetName2HumanName = array(
