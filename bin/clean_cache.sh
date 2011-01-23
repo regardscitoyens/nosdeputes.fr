b13584ed2f59a32928163c3230bb3fde2f1d5ff7
Tangui Morlier <tangui@tangui.eu.org>
2011-01-23 14:21:17 +0000

    Refactorisation de la gestion des script bash pour homogénéiser les environnements et leurs configurations
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/trunk/project@1766 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

--- bin/clean_cache.sh
+++ bin/clean_cache.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-cd /home/nosdeputes/project
+#Récupère le fichier de configuration pour notre environnement (qui se trouve dans le même que ce script)
+. $(echo $0 | sed 's/[^\/]*$//')db.inc
+
+cd $PATH_APP
 
 cd cache
 mv frontend frontend-old
