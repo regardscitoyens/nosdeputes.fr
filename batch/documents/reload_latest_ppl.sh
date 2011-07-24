1291a61847be9bf9f0a4fbb9354bca9159b66f00
Regards Citoyens <contact@regardscitoyens.org>
2011-07-24 15:48:47 +0000

    appel du script de reload des ppl à chaque load old_questions hebdo
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/trunk/project@2017 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

--- batch/documents/reload_latest_ppl.sh
+++ batch/documents/reload_latest_ppl.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+. ../../bin/db.inc
+
 for url in `echo "SELECT source FROM texteloi WHERE date > DATE_SUB(CURDATE(), INTERVAL 90 DAY) AND type = 'Proposition de loi' ORDER BY numero"  | mysql $MYSQLID $DBNAME | grep -v source`; do
   file=`echo $url | sed 's/\//_/g'`
   perl download_one.pl $url
