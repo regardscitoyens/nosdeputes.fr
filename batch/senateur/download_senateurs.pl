95ab9fe7d62cf917212e4d71e30305e4cf73a9f2
Tangui Morlier <tangui@tangui.eu.org>
2011-08-11 15:28:33 +0000

    Député du parsing des sénateurs
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/branches/senat/project@2022 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

---
+++ batch/senateur/download_senateurs.pl
@@ -0,0 +1,44 @@
+#!/usr/bin/perl
+
+use WWW::Mechanize;
+use HTML::TokeParser;
+use URI::Escape;
+use Encode;
+
+$verbose = shift || 0;
+%done = ();
+sub download_fiche {
+	$uri = shift;
+	return if ($done{$uri});
+	$done{$uri} = 1;
+	$a->get($uri);
+	$file = uri_escape($a->uri());
+	return if ($done{$file});
+	$done{$file} = 1;
+        print "saving $file : " if ($verbose);
+	mkdir html unless -e "html/" ;
+	open FILE, ">:utf8", "html/$file";
+	$thecontent = $a->content;
+	$thecontent =~ s/iso-8859-1/utf8/g;
+	print FILE $thecontent if ($verbose);
+	close FILE;
+print "DONE\n";
+	return $file;
+}
+$a = WWW::Mechanize->new();
+
+sub find_senateurs {
+	$url = shift;
+	print "looking for senateurs in $url\n" if ($verbose);
+	$a->get($url);
+	$content = $a->content;
+	$p = HTML::TokeParser->new(\$content);
+	while ($t = $p->get_tag('a')) {
+	    if ($t->[1]{href} =~ /\/(senateur|senfic)\//) {
+		download_fiche($t->[1]{href});
+	    }
+	}
+}
+
+find_senateurs("http://www.senat.fr/senateurs/senatl.html");
+find_senateurs("http://www.senat.fr/senateurs/news.html");
---
+++ batch/senateur/parse_senateur.pl
@@ -0,0 +1,148 @@
+#!/usr/bin/perl
+
+use HTML::TokeParser;
+use URI::Escape;
+use utf8;
+
+$file = shift;
+$xml = shift || 0;
+
+open $fh, $file ;
+@content = <$fh>;
+$content = "@content";
+$content =~ s/\n/ /g;
+$content =~ s/  / /g;
+@content = ();
+seek($fh, 0, 0);
+$p = HTML::TokeParser->new($fh);
+
+sub datize {
+	$date = shift;
+	%mois = ('janvier'=>'01', 'fvrier'=>'02', 'mars'=>'03', 'avril'=>'04', 'mai'=>'05', 'juin'=>'06', 'juillet'=>'07','aot'=>'08', 'septembre'=>'09', 'octobre'=>'10', 'novembre'=>'11', 'dcembre'=>'12');
+	$date =~ /([0-9]*) (\S*) ([0-9]*)/;
+	$jour = $1;
+	$mois = $2;
+	$annee = $3;
+	$mois =~ s/[^a-z].//;
+	return sprintf("%02d/%02d/%04d", $jour,$mois{$mois},$annee);
+}
+
+my %senateur;
+
+if ($file =~ /%2F([^%]+).html/) {
+    $senateur{'id_senat'} = $1;
+    $senateur{'url_senat'} = uri_unescape($file);
+    $senateur{'url_senat'} =~ s/html\///;
+}
+$p->get_tag('h1');
+$senateur{'Nom'} = $p->get_text('/h1');
+$senateur{'Nom'} =~ s/^\n?(.+) ([A-ZÉ][^A-ZÉ].*)$/$2 $1/;
+$senateur{'Nom_de_famille'} = $1;
+$p->get_tag('h2');
+$senateur{'Circonscription'} = $p->get_text('/h2');
+$senateur{'Circonscription'} =~  s/\n/ /g;
+$senateur{'Circonscription'} =~ s/.*(ancienn?e? |)s..nat\S+ //i;
+$senateur{'Circonscription'} =~ s/^d[eus' ]*(l[a']|) *//;
+$senateur{'Circonscription'} =~ s/repr..sentant les //;
+$senateur{'Circonscription'} =~  s/ +\(.*//;
+
+if ($content =~ /place-hemicycle_([0-9]+)/) {
+	$senateur{'Place_hemicycle'} = $1;
+}
+
+if ($content =~/<dt>Profession<\/dt>[^<]*<dd>([^<]+)<\/dd/) {
+	$senateur{'Profession'} = $1;
+	$senateur{'Profession'} =~ s/\n/ /;
+}
+
+if ($content =~ /N..e? le ([0-9]* \S* [0-9]*)/) {
+	$senateur{'Naissance'} = datize($1);
+}
+
+%mails = ();
+while($content =~ />([^>\s]+\@[^<\s]+)</g) {
+	$senateur{'Mails'}{$1} = 1;
+}
+delete $senateur{'Mails'}{'notices-senateurs@senat.fr'};
+delete $senateur{'Mails'}{'e-bure@u'};
+
+$senateur{'sexe'} = ($content =~ /sentation de M\. /) ? 'H' : 'F';
+if ($content =~ /Elue? le ([0-9]* \S* [0-9]{4})/) {
+	$senateur{'debut_mandat'} = datize($1);
+}
+if ($content =~ /Fin de mandat le ([0-9]* \S* [0-9]{4})/) {
+	$senateur{'fin_mandat'} = datize($1);
+}
+if ($content =~ /src="([^"]+)"[^>]+Photo de M/) {
+	$senateur{'photo'} = 'http://www.senat.fr'.$1;
+}
+if ($xml) {
+print "<Senateur>\n";
+
+foreach $k (keys %senateur) {
+    print '<'.lc($k).'>';
+    if (ref($senateur{$k}) eq 'HASH' ) {
+	if (ref(${$senateur{$k}}[0]) eq 'HASH') {
+	    foreach $h (keys %{$senateur{$k}}) {
+		print "\n<hash>";
+		foreach $cle (keys (%{$h})) {
+		    next unless ($h->{$cle});
+		    print "<item key=\"$cle\">";
+		    print $h->{$cle};
+		    print "</item>";
+		}
+		print "</hash>";
+	    }
+	}else{
+	    print "\n<item>";
+	    print join("</item>\n<item>", @{$senateur{$k}});
+	    print "</item>";
+	}
+    }else {
+	print $senateur{$k};
+    }
+    print '</'.lc($k).'>';
+    print "\n";
+}
+print "</Senateur>\n";
+exit;
+} 
+
+
+if ($yml) {
+    
+    print "  senateur_".$senateur{'id_an'}.":\n"; 
+    foreach $k (keys %senateur) { 
+	next if ($k =~ /suppléant/i); 
+	if (ref($senateur{$k}) =~ /HASH/) { 
+	    print "    ".lc($k).":\n"; 
+	    foreach $i (keys %{$senateur{$k}}) { 
+		print "      - $i\n"; 
+	    } 
+	}else { 
+	    if ($k !~ /supplÃ©ant/i) {
+		print "    ".lc($k).": ".$senateur{$k}."\n"; 
+	    }
+	} 
+    } 
+    print "    type: senateur\n"; 
+    
+    exit;
+}
+
+print "{ ";
+foreach $k (keys %senateur) {
+    next if ($k =~ /suppléant/i);
+    if (ref($senateur{$k}) =~ /HASH/) {
+	print '"'.lc($k).'" : [';
+	foreach $i (keys %{$senateur{$k}}) {
+	    $i =~ s/"//g;
+	    print '"'.$i.'",';
+	}
+	print '"" ], ';
+    }else {
+	$senateur{$k} =~ s/"//g;
+	print '"'.lc($k).'" : "'.$senateur{$k}.'", ';
+    }
+}
+print "\"type\" : \"senateur\" }\n";
