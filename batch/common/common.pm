a03ce957c8337966906202e873e5131c6474780e
Tangui Morlier <tangui@tangui.eu.org>
2011-08-31 13:02:51 +0000

    Abstraction gestion du repérage des dates et des heures
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/branches/senat/project@2045 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

---
+++ batch/common/common.pm
@@ -0,0 +1,48 @@
+%mois = ('janvier'=>'01', 'fvrier'=>'02', 'mars'=>'03', 'avril'=>'04', 'mai'=>'05', 'juin'=>'06', 'juillet'=>'07','aot'=>'08', 'septembre'=>'09', 'octobre'=>'10', 'novembre'=>'11', 'dcembre'=>'12');
+
+
+sub datize {
+        my $date = shift;
+        $date =~ /([0-9]+)e?r? (\S*) ([0-9]+)/;
+        my $jour = sprintf "%02d", $1;
+        my $mois = $2;
+        my $annee = $3;
+	$mois =~ s/&[^;]+;//g;
+        $mois =~ s/[^a-z]\W?//g;
+        return ($annee,$mois{$mois},$jour);
+}
+
+$heure{'neuf'} = '09';
+$heure{'dix'} = '10';
+$heure{'onze'} = '11';
+$heure{'douze'} = '12';
+$heure{'treize'} = '13';
+$heure{'quatorze'} = '14';
+$heure{'quinze'} = '15';
+$heure{'seize'} = '16';
+$heure{'dix-sept'} = '17';
+$heure{'dix-huit'} = '18';
+$heure{'dix-neuf'} = '19';
+$heure{'vingt'} = '20';
+$heure{'vingt et une'} = '21';
+$heure{'vingt-deux'} = '22';
+$heure{'quarante'} = '45';
+$heure{'quarante-cinq'} = '45';
+$heure{'trente'} = '30';
+$heure{'trente-cinq'} = '35';
+$heure{'quinze'} = '15';
+$heure{'zéro'} = '00';
+$heure{'cinq'} = '00';
+$heure{''} = '00';
+
+sub heurize {
+	shift;
+	/(\S+) heures\s*(\S*)/;
+	return sprintf("%02d:%02d", $heure{$1}, $heure{$2});
+}
+
+sub sessionize {
+        return ($_[1] <= 8) ? ($_[0]-1).'-'.$_[0] :  $_[0].'-'.($_[0]+1);
+}
+
+1;
