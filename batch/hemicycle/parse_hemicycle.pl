2fde900234186bd673555935a5bf0b2ffd4ee3f2
Tangui Morlier <tangui@tangui.eu.org>
2011-08-31 13:04:05 +0000

    Gestion des amendements et des num de pj utilisation de common
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/branches/senat/project@2046 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

--- batch/hemicycle/parse_hemicycle.pl
+++ batch/hemicycle/parse_hemicycle.pl
@@ -1,50 +1,24 @@
 #!/usr/bin/perl
 
-open(FILE, shift);
+use URI::Escape;
+require "../common/common.pm";
+
+$file = shift;
+open(FILE, $file);
 @doc = <FILE>;
 $doc = "@doc";
 @doc = ();
 close FILE;
 
-$mois{'janvier'} = '01';
-$mois{'février'} = '02';
-$mois{'mars'} = '03';
-$mois{'avril'} = '04';
-$mois{'mai'} = '05';
-$mois{'juin'} = '06';
-$mois{'juillet'} = '07';
-$mois{'août'} = '08';
-$mois{'septembre'} = '09';
-$mois{'octobre'} = '10';
-$mois{'novembre'} = '11';
-$mois{'décembre'} = '12';
+$url_source = uri_unescape($file);
+$url_source =~ s/.*http/http/;
 
-$heure{'neuf'} = '09';
-$heure{'dix'} = '10';
-$heure{'onze'} = '11';
-$heure{'douze'} = '12';
-$heure{'treize'} = '13';
-$heure{'quatorze'} = '14';
-$heure{'quinze'} = '15';
-$heure{'seize'} = '16';
-$heure{'dix-sept'} = '17';
-$heure{'dix-huit'} = '18';
-$heure{'dix-neuf'} = '19';
-$heure{'vingt'} = '20';
-$heure{'vingt et une'} = '21';
-$heure{'vingt-deux'} = '22';
-$heure{'quarante'} = '45';
-$heure{'quarante-cinq'} = '45';
-$heure{'trente'} = '30';
-$heure{'trente-cinq'} = '35';
-$heure{'quinze'} = '15';
-$heure{'zéro'} = '00';
-$heure{'cinq'} = '00';
-$heure{''} = '00';
+$session = '';
 
-
-if ($doc =~ /ance du (\d+)e?r? (\S+) (\d+)/i) {
-	$date = sprintf("%04d-%s-%02d", $3, $mois{$2}, $1);
+if ($doc =~ /ance du (\d+e?r? \S+ \d+)/i) {
+	@date = datize($1);
+	$date = join '-', @date;
+	$session = sessionize(@date);
 }
 
 $doc =~ s/\n/ /g;
@@ -63,32 +37,58 @@ $doc =~ s/<\/p>/<\/p>\n/g;
 $intervention = '';
 $timestamp = 0;
 sub print_inter {
-	if ($heure && $intervention) {
+	if ($heure && $intervention && $intervention ne "<p></p>") {
 		$timestamp += 20;
 		$context = $bigcontext;
 		$context .= ' > '.$subcontext if ($subcontext);
-	print '{"contexte": "'.$context.'", "intervention": "'.$intervention.'", "timestamp": "'.$timestamp.'", "date": "'.$date.'", "source": "'.$source.'", "heure":"'.$heure.'", "intervenant": "'.$inter.'", "fonction": "'.$fonction.'", "intervenant_url": "'.$url_inter.'"}'."\n";
+                if ($intervention =~ /\((projet n[^<]+)/) {
+                          $docs = $1;
+                          $docs =~ s/&[^;]*;//g;
+                          $numeros_loi = '';
+                          while ($docs =~ /(\d+)(, (\d{4}-\d{4}|))/g) {
+                                 if ($3) {
+                                          $numeros_loi .= "$1/$3,";
+                                 }else{
+                                          $numeros_loi .= "$1/".$session.",";
+                                 }
+                          }
+                          chop($numeros_loi);
+                }
+                if ($intervention =~ /amendements? n([^<]+)/) {
+                                        $amdt = $1;
+                                        $amdt =~ s/&[^;]*;//g;
+                                        print "docs: $amdt\n";
+                                        $amendements = '';
+                                        while ($amdt =~ /(\d+)( ?rectifi|)\D/g) {
+                                                $amendements .= "$1";
+                                                $amendements .= " rectifié" if ($2);
+                                                $amendements .= ",";
+				}
+				chop($amendements);
+			}
+	$intervention =~ s/<p> +/<p>/g;
+	print '{"contexte": "'.$context.'", "intervention": "'.$intervention.'", "timestamp": "'.$timestamp.'", "date": "'.$date.'", "source": "'.$url_source.$source.'", "heure":"'.$heure.'", "intervenant": "'.$inter.'", "fonction": "'.$fonction.'", "intervenant_url": "'.$url_inter.'", "session":"'.$session.'"';
+	print ', "numeros_loi":"'.$numeros_loi.'"' if ($numeros_loi);
+	print ', "amendements":"'.$amendements.'"' if ($amendements);
+	print "}\n";
 	}
 	$intervention = '';
 	$inter = '';
 	$fonction = '';
 	$url_inter = '';
+	$amendements = '';
 }
 
 foreach (split /\n/, $doc) {
 	if (/name="([^"]+)"/) {
 		$source = "#$1";
 	}
-        if (s/<span class="info_entre_parentheses">\((.*)\)\S*<\/span>//) {
+        if (/<span class="info_entre_parentheses">\((.*)<\/span>([\.\s\)]*)/) {
                 $didasc = $1;
-                $didasc =~ s/<[^>]*>//gi;
-		print_inter() if ($heure || $intervenant);
-                if ($didasc =~ /(ouverte|reprise) &#224; (\S+) heures\s*(\S*)\W/) {
-                        $heure = sprintf("%02d:%02d", $heure{$2}, $heure{$3});
+                if ($didasc =~ /(ouverte|reprise) (&#224;|à) (\S+ heures\s*\S*)\W/) {
+                        $heure = heurize($3);
 			$timestamp = 0;
                 }
-		$intervention .= '<p>'.$didasc.'</p>';
-		
         }
 	if (/Pr\&\#233\;sidence de (M[^<]*)/) {
 		$president = $1;
@@ -120,6 +120,22 @@ foreach (split /\n/, $doc) {
 			$fonction = $tmpfonction;
 		}
 	}
+        if (s/([^>]*)<span class="info_entre_parentheses">\((.*)<\/span>([\.\s\)]*)//) {
+                $intervention .= "<p>$1</p>";
+                $didasc = $2;
+                $didasc =~ s/<[^>]*>//gi;
+                $didasc =~ s/\)//g;
+                $predida_inter = $inter;
+                $predida_urlinter = $url_inter;
+                $predida_fonction = $fonction;
+                print_inter();
+                $intervention .= '<p>'.$didasc.'</p>';
+                print_inter();
+                $inter = $predida_inter;
+                $url_inter = $predida_urlinter;
+                $fonction = $predida_fonction;
+
+        }
 	if (/class="titre_/) {
 		if ($inter) {
                         print_inter();
@@ -140,8 +156,9 @@ foreach (split /\n/, $doc) {
 				$subcontext = '';
 			}else{
 				$subcontext = $_;
+				$subcontext =~ s/<[^>]+>//g;
 			}
-			}else{
+			}elsif(/[a-z]/i){
 				$intervention .= "<p>$_</p>";
 			}
 		}
