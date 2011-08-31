be9b05b1a6a1e056e2e45861474ca0f242c1bd18
Tangui Morlier <tangui@tangui.eu.org>
2011-08-31 13:04:45 +0000

    Download des hémicycles
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/branches/senat/project@2047 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

--- batch/hemicycle/download_hemicycle.pl
+++ batch/hemicycle/download_hemicycle.pl
@@ -2,44 +2,48 @@
 
 use WWW::Mechanize;
 use HTML::TokeParser;
+use URI::Escape;
+use Encode;
 
-@url = (
-    "http://www.assemblee-nationale.fr/13/cri/2010-2011/",
-    "http://www.assemblee-nationale.fr/13/cri/2009-2010-extra2/",
-    "http://www.assemblee-nationale.fr/13/cri/2009-2010-extra/",
-    "http://www.assemblee-nationale.fr/13/cri/2009-2010/",
-);
+@files = <./html/*>;
+$lastfile = pop(@files);
+@files = ();
+
+$dannee = "2005"; $dmois = "10";
+if ($lastfile =~ /s(\d{4})(\d{2})\d{2}_mono.html/) {
+	$dannee = $1;
+	$dmois = $2;
+}
+
+my ($sec,$min,$hour,$mday,$mon,$year) = localtime(time);
 
 $a = WWW::Mechanize->new();
 
-foreach $url (@url) {
+print "$dannee : $year\n";
+print "$dmois ; $mon\n";
+
+for($annee = $dannee ; $annee <= $year +1900 ; $annee++) {
+$lastmonth = 12;
+$lastmonth = $mon if ($year + 1900 == $annee);
+for($mois = $dmois ; $mois <= $lastmonth ; $mois++) { 
+
+    $url = "http://www.senat.fr/seances/s$annee$mois/s$annee$mois.html";
+
+    print STDERR "$url\n";
+
     $a->get($url);
     $content = $a->content;
     $p = HTML::TokeParser->new(\$content);
     $cpt = 0;
     while ($t = $p->get_tag('a')) {
-	$txt = $p->get_text('/a');
-	if ($txt =~ /(\d+[\Serm]+\s+\S+ance|S\S+ance uniq)/i && $t->[1]{href} !~ /provisoire/) {
+	  next if ($t->[1]{href} !~ /_mono.html/);
           $a->get($t->[1]{href});
-          $file = $a->uri();
-          if ($file !~ /provisoire/) {
-	    $file =~ s/\//_/gi;
-	    $file =~ s/\#.*//;
-	    #on ne peut pas quitter dès le premier, seulement au bout de 
-	    #trois fois on est sur qu'il n'y a pas de nouveaux fichiers
-	    $size = -s "html/$file";
-	    if ($size) {
-		$cpt++;
-		exit if ($cpt > 3);
-		next;
-	    }
-	    $cpt = 0;
-	    print "$file\n";
-	    open FILE, ">:utf8", "html/$file";
-	    print FILE $a->content;
-	    close FILE;
-          }
+          $file = uri_escape($a->uri());
+	  print "$file\n";
+	  open FILE, ">:utf8", "html/$file";
+	  print FILE $a->content;
+	  close FILE;
 	  $a->back();
-	}
     }
-}
+
+}}
