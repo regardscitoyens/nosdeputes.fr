84d028be78e71c0f4e7749b5c2ffb64b6839e71c
RouxRC <b.ooghe@gmail.com>
2011-09-13 20:25:19 +0000

    download commun a amendements rapports et lois
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/branches/senat/project@2147 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

---
+++ batch/download_elements_dossiers.pl
@@ -0,0 +1,134 @@
+#!/usr/bin/perl
+
+use WWW::Mechanize;
+use HTML::TokeParser;
+use URI::Escape;
+use Encode;
+use File::Path qw(make_path);
+
+$lastyear = localtime(time);
+my @month = `date +%m`;
+$lastyear =~ s/^.*\s(\d{4})$/$1/;
+$lastyear-- if ($month[0] < 10);
+
+$year = shift;
+$year = $lastyear if (!$year);
+if (! $year =~ /^\d{4}$/) {
+  print STDERR "Please input a 4-digit year\n";
+  exit;
+}
+
+$verbose = shift || 0;
+
+%done = ();
+%donedl = ();
+$a = WWW::Mechanize->new();
+
+sub download_one {
+  $uri = shift;
+  $dir = shift;
+  make_path($dir) unless -e $dir;
+  return if ($donedl{$uri});
+  $donedl{$uri} = 1;
+  eval {$a->get($uri);};
+  if ($a->status() == 404) {
+    $a->back();
+    if ($dir =~ /rap/i && $uri =~ /_mono/i) {
+      $uri =~ s/_mono//i;
+      download_one($uri, $dir);
+    } else {
+      print STDERR "ERREUR 404 sur $uri\n";
+    }
+    return;
+  }
+  $htmfile = uri_escape($a->uri);
+  if ($donedl{$htmfile}) {
+    $a->back();
+    return;
+  }
+  $donedl{$htmfile} = 1;
+  print "    $dir\t\t->\t\t$htmfile\n";
+  open FILE, ">:utf8", "$dir/$htmfile";
+  $thecontent = $a->content;
+  if ($thecontent =~ s/iso-8859-1/utf-8/gi) {
+    $thecontent = decode("windows-1252", $thecontent);
+  }
+  print FILE $thecontent;
+  close FILE;
+  $a->back();
+}
+
+sub examine_url {
+  my $lk = lc(shift);
+  $lk =~ s/^.*<a([^>]+)?\s+href\s?=\s?['"]\s?([^'"#]+)['"#].*$/$2/i;
+  $lk =~ s/^([^h\/])/\/$1/i;
+  return "" if ($lk !~ /senat\.fr/ && $lk !~ /^\//);
+  $lk =~ s/^.*senat\.fr//i;
+  $lk =~ s/^/http:\/\/www.senat.fr/;
+  return if ($done{$lk});
+  $done{$lk} = 1;
+  my $urls = "";
+  my $outdirs = "";
+  if ($lk =~ /\/(motion)?(tas|p[jp][lr])(\d\d)-(\d{3})/) {
+    $urls = "http://www.senat.fr/leg/$1$2$3-$4.html";
+    $outdirs = "documents/$2";
+  } elsif ($lk =~ /\/([arl]\d\d-\d{3}\d*(-\d+)*)/) {
+    $urls = "http://www.senat.fr/rap/$1/$1_mono.html";
+    $outdirs = "documents/rap";
+  }
+  download_one($urls, $outdirs) if $urls;
+  return $lk;
+}
+
+sub find_elements {
+  my $urlp = shift;
+  my $urla = "";
+  my $outdir = "";
+  $a->get($urlp);
+  $done{$a->uri} = 1;
+  print STDERR " examine dossier $urlp\n" if ($verbose);
+  my $contentleg = $a->content;
+  if ($contentleg =~ s/iso-8859-1/utf-8/gi) {
+    $contentleg = decode("windows-1252", $contentleg);
+  }
+  $contentleg =~ s/<a/\n<a/g;
+  foreach $line (split /\n/, $contentleg) {
+    $line = examine_url($line);
+    $urla = "";
+    if ($line =~ /(\/amendements.*\/(\d{4})-.+\/)accueil\.html/) {
+      if ($2 < 2004) {
+        $a->back();
+        next;
+      }
+      $urla = "http://www.senat.fr$1jeu_complet.html";
+      $outdir = "amendements/html";
+    } elsif ($line =~ /(\/amendements[^'"]+\.pdf)/){
+      $urla = $line;
+      $outdir = "amendements/pdfs";
+    }
+    download_one($urla, $outdir) if ($urla);
+  }
+  $a->back();
+}
+
+# urls en compte-rendu-commissions a checker si already dl?
+
+while ($year <= $lastyear) {
+  $baseurl = "http://www.senat.fr/dossiers-legislatifs/depots/depots-$year.html";
+  print STDERR "Download documents et amendements de la session $year : $baseurl ...\n" if ($verbose);
+  $a->get($baseurl);
+  $content = $a->content;
+  if ($content =~ s/iso-8859-1/utf-8/gi) {
+    $content = decode("windows-1252", $content);
+  }
+  $content =~ s/<a/\n<a/g;
+  foreach $link (split /\n/, $content) {
+    $link = examine_url($link);
+    if ($link =~ /dossier(leg|-legislatif)/) {
+      find_elements($link);
+    }
+  }
+  $a->back();
+  $year++;
+}
+
