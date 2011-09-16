56dedc18f9de78469df36b0eb2bfc6a2fca19bb8
Tangui Morlier <tangui@tangui.eu.org>
2011-09-16 18:19:12 +0000

    Recherche empirique de pages d'indexe de commission/mission/...
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/branches/senat/project@2179 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

---
+++ batch/commission/predownload_commission.pl
@@ -0,0 +1,76 @@
+#!/usr/bin/perl
+
+
+use WWW::Mechanize;
+use HTML::TokeParser;
+use  URI::Escape;
+use Encode;
+
+$with_archive = shift;
+
+@archive = ("http://www.senat.fr/compte-rendu-commissions/commission-mixte-paritaire_archives.html", "http://www.senat.fr/compte-rendu-commissions/affaires-etrangeres_archives.html", "http://www.senat.fr/compte-rendu-commissions/controle-de-la-securite-sociale_archives.html", "http://www.senat.fr/compte-rendu-commissions/affaires-sociales_archives.html", "http://www.senat.fr/compte-rendu-commissions/culture_archives.html","http://www.senat.fr/compte-rendu-commissions/economie_archives.html",  "http://www.senat.fr/compte-rendu-commissions/finances_archives.html", "http://www.senat.fr/compte-rendu-commissions/lois_archives.html");
+
+@indexes = ("http://www.senat.fr/offices_deleg_observatoire/index.html", "http://www.senat.fr/commission/spec/index.html", "http://www.senat.fr/commission/missions/index.html", "http://www.senat.fr/commission/enquete/index.html");
+
+@more = ("http://www.senat.fr/compte-rendu-commissions/office-parlementaire-d-evaluation-des-choix-scient.-tech..html");
+
+$a = WWW::Mechanize->new();
+$start = shift || '0';
+$count = 50;
+$ok = 1;
+
+foreach $index (@indexes) {
+    print "INDEX : $index\n";
+    $a->get($index);
+    $content = $a->content;
+    $p = HTML::TokeParser->new(\$content);
+    while ($t = $p->get_tag('a')) {
+	if ($t->[1]{href} =~ /\/commission\/([^\/]+)\/(.*)/) {
+	    $type = $1 ; $suite = $2;
+	    next if ($suite =~ /^index.html/ && $type =~ /\/(spec|enquete)\//);
+	    $url = $t->[1]{href};
+	    $url =~ s/\#.*//;
+	    $url =~ s/\/\.\.\/commission\//\/commission\//;
+	    next if ($done{$url});
+	    $done{$url} = 1;
+	    print "MISSION : $url\n";
+	    $a->get($url);
+	    $content_page = $a->content;
+	    $pp = HTML::TokeParser->new(\$content_page);
+	    while ($tp = $pp->get_tag('a')) {
+		$text = $pp->get_text('/a');
+		$href = $tp->[1]{href};
+#		print "LOG: ".$href." $text\n";
+		$cr = '';
+		$cr = $href if (($text =~ /Comptes[\s\-]+rendus$/i || $href =~ /compte-rendu-commissions/ || $href =~ /\/travaux.html/) && $href !~ /\d{6}/ && $href !~ /somsea/ && $href !~ /prog.html/) ;
+		$cr = $url if ($href =~ /\d{6}.html/ && $href !~ /^\/presse\//);
+		next unless ($cr);
+		$cr =~ s/^\/..\//\//;
+		if ($cr !~ /^http/ && $cr !~ /^\//) {
+		    $a->get($cr);
+		    $cr = $a->uri();
+		    $a->back();
+		}
+		$cr = "http://www.senat.fr$cr" if ($cr =~ /^\//);
+		$cr =~ s/\#.*//;
+		next if ($cr{$cr});
+		print "CR FOUND: $cr\n";
+		$cr{$cr} = 1;
+	    }
+	    $a->back();
+	}
+    }
+}
+
+if ($with_archive) {
+    @more = (@more, @archive);
+}
+mkdir('conf') if (! -e 'conf');
+open FILE, "> conf/cr.list";
+foreach $url (@more) {
+    print FILE "$url\n";
+}
+foreach $url (keys %cr) {
+    print FILE "$url\n";
+}
+close FILE;
