ed04b3d4def2e1858524cee2dee593ffae051d10
Tangui Morlier <tangui@tangui.eu.org>
2011-09-16 18:22:10 +0000

    Utilisation du fichier predownload + meilleure gestion des 404
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/branches/senat/project@2182 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

--- batch/commission/download_commission.pl
+++ batch/commission/download_commission.pl
@@ -5,24 +5,45 @@ use HTML::TokeParser;
 use  URI::Escape;
 use Encode;
 
-@indexes = ("http://www.senat.fr/compte-rendu-commissions/affaires-etrangeres.html", "http://www.senat.fr/compte-rendu-commissions/affaires-etrangeres_archives.html", "http://www.senat.fr/compte-rendu-commissions/controle-de-la-securite-sociale.html", "http://www.senat.fr/compte-rendu-commissions/controle-de-la-securite-sociale_archives.html", "http://www.senat.fr/compte-rendu-commissions/affaires-sociales.html", "http://www.senat.fr/compte-rendu-commissions/affaires-sociales_archives.html", "http://www.senat.fr/compte-rendu-commissions/culture.html", "http://www.senat.fr/compte-rendu-commissions/culture_archives.html", "http://www.senat.fr/compte-rendu-commissions/economie.html", "http://www.senat.fr/compte-rendu-commissions/economie_archives.html", "http://www.senat.fr/compte-rendu-commissions/finances.html", "http://www.senat.fr/compte-rendu-commissions/finances_archives.html", "http://www.senat.fr/compte-rendu-commissions/lois.html", "http://www.senat.fr/compte-rendu-commissions/lois_archives.html");
 
 $a = WWW::Mechanize->new();
 $start = shift || '0';
 $count = 50;
 $ok = 1;
+
+open FILE, "conf/cr.list";
+@indexes = <FILE>;
+close FILE;
+
 foreach $index (@indexes) {
-    $a->get($index);
+    chomp($index);
+    eval {$a->get($index);};
+    if ($a->status() == 404) {
+	print "ERR: bad file in index : 404 on $index\n";
+	next;
+    }
     $content = $a->content;
     $p = HTML::TokeParser->new(\$content);
     while ($t = $p->get_tag('a')) {
-	if ($t->[1]{href} =~ /compte-rendu-commissions/) {
+	if ($t->[1]{href} =~ /compte-rendu-commissions/ || $t->[1]{href} =~ /\d{6}\d*.html/) {
 	    $curl =  $t->[1]{href};
+	    next if ($curl =~ /\/presse\// || curl =~ /prog.html/);
 	    next if ($url{$curl});
-            $url{$curl} = 1;
-	    $a->get($t->[1]{href});
-	    $file =  uri_escape($a->uri());
- 	    next if -e "html/$file";
+	    $uriurl = $curl;
+	    $uriurl =~ s/^\//http:\/\/www.senat.fr\//;
+ 	    next if -e "html/".uri_escape($uriurl);
+	    eval {$a->get($curl);};
+	    if ($a->status() == 404) {
+		print "ERR: 404 ".$a->uri()." (from $index)\n ";
+		$a->back();
+		next;
+	    }
+	    $uri = $a->uri();
+	    if ($url{$uri}) { $a->back(); next; }
+            $url{$curl} = 1;	    
+            $url{$uri} = 1;	    
+	    $file =  uri_escape($uri);
+ 	    if (-e "html/$file") { $a->back(); next; }
 	    print "$file\n";
 	    open FILE, ">:utf8", "html/$file.tmp";
 	    print FILE $a->content;
