2dd31ecc87bb40d2069370c3aa57da7bb909f62f
Tangui Morlier <tangui@tangui.eu.org>
2011-09-17 00:14:20 +0000

    Encore des problèmes de date
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/branches/senat/project@2190 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

--- batch/commission/parse_commission.pl
+++ batch/commission/parse_commission.pl
@@ -19,7 +19,7 @@ $content =~ s/\n//g;
 $content =~ s/(<td[^>]*>)(\s*<\/?(a|strong|p|em)[^>]*>)+/$1/gi;
 $content =~ s/<\/?(a|strong|p|em)[^>]*>\s*<\/td>/<\/td>/gi;
 
-$content =~ s/<\/(p|h[1234]|ul|div)>/<\/$1>--\n/gi;
+$content =~ s/<\/(p|h[1234]|ul|div)>/<\/$1>\n/gi;
 
 $content =~ s/(<h\d[^>]*>)\s*<b>/$1/gi;
 $content =~ s/<\/b>\s*(<\/h\d[^>]*>)/$1/gi;
@@ -86,7 +86,7 @@ foreach (split /\n/, $content) {
 #print STDERR "title: $1\n" if (/<title>([^<]*)</);
 	$commission = $1 if (/TITLE>.*((Commission|Mission|Office)[^\&:]*)/i);
 #	print ;	print "\n";
-	if (/<(h[123]|strong)[^>]*>(\s*<[^>]*>)*([^<\(]+\d{4})\W*<\/(h[123]|strong)>/i) {
+	if (/<(h[123]|strong)[^>]*>(\s*<[^>]*>)*([^<\(]+\d[^<\(]+\d{4})\W*<\/(h[123]|strong)>/i) {
 #print STDERR "date: $3 $url_year\n";
 		@date = datize($3, $url_year);
 		print_inter();
