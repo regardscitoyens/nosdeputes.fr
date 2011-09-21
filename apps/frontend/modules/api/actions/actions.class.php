ef1926c47381cbafe04e7b0e90c6809ab5082873
Regards Citoyens <contact@regardscitoyens.org>
2011-09-21 08:44:44 +0000

    oups
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/trunk/project@2294 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

--- apps/frontend/modules/api/actions/actions.class.php
+++ apps/frontend/modules/api/actions/actions.class.php
@@ -135,7 +135,9 @@ class apiActions extends sfActions
     $this->res['depute']['mandat_debut'] = $depute->debut_mandat;
     if ($depute->fin_mandat)
       $this->res['depute']['mandat_fin'] = $depute->fin_mandat;
-    $this->res['depute']['groupe'] = $depute->getGroupe()->__toString();
+    $groupe = $depute->getGroupe();
+    if (is_object($groupe))
+      $this->res['depute']['groupe'] = $groupe->__toString();
     $this->res['depute']['groupe_sigle'] = $depute->groupe_acronyme;
     $this->res['depute']['responsabilites'] = $this->array2hash($depute->getResponsabilites(), 'responsabilite');
     $this->res['depute']['responsabilites_extra_parlementaires'] = $this->array2hash($depute->getExtras(), 'responsabilite');
