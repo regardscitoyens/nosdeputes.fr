595438d57695284fc4b4027837b9caa8175789c0
Regards Citoyens <contact@regardscitoyens.org>
2011-09-22 01:50:11 +0000

    section nomination de rapporteur a ne pas lawtagger
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/branches/senat/project@2331 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

--- lib/model/doctrine/Intervention.class.php
+++ lib/model/doctrine/Intervention.class.php
@@ -187,7 +187,7 @@ class Intervention extends BaseIntervention
   public function tagSectionLois($lois) {
      if ($this->section_id != 1) {
         $titre = $this->Section->Section->getTitre();
-        if (!(preg_match('/(conf.*rence des pr.*sidents|^(d.*p.*t|transmission) d(e |.une? |.)(documents?|rapport|proposition|avis|projet)|cloture|ouverture|question|ordre du jour|calendrier|élection.*nouveau|démission|reprise|examen simplifié|cessation.*mandat|proclamation|souhaits|application de l.article|renvoi pour avis|nomination (de|d.une?) membres.|rappel au règlement|^communication|^candidature)/i', $titre))) {
+        if (!(preg_match('/(conf.*rence des pr.*sidents|^(d.*p.*t|transmission) d(e |.une? |.)(documents?|rapport|proposition|avis|projet)|cloture|ouverture|question|ordre du jour|calendrier|élection.*nouveau|démission|reprise|examen simplifié|cessation.*mandat|proclamation|souhaits|application de l.article|renvoi pour avis|nomination (de|d.une?) (membre|rapporteur)s?|rappel au règlement|^communication|^candidature)/i', $titre))) {
           foreach($lois as $loi) {
             $tag = 'loi:numero='.$loi;
             $this->Section->addTag($tag);
