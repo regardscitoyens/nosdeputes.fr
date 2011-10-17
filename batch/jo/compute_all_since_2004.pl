4a32298b980b4a3be13fc5a30eb6b1b9e88f337e
Regards Citoyens <contact@regardscitoyens.org>
2011-10-17 16:17:20 +0000

    parse all jo since 2004
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/branches/senat/project@2402 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

---
+++ batch/jo/compute_all_since_2004.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/perl
+
+for ($cpt = 0 ; 1 ; $cpt++) {
+	@time = localtime(time()-60*60*24*$cpt);
+	$year = $time[5];
+	$mon = $time[4] + 1;
+	$mday = $time[3];
+	$year += 1900;
+	$file = sprintf('pdf/%04d%02d%02d.pdf', $year, $mon, $mday);
+	print "$file\n";
+#	if (-e $file) {
+#		exit;
+#	}
+	if ($year <= 2004 && $mon <= 9 && $mday <= 31) {
+		print "done\n";
+		exit;
+	}
+	$ret = system('sh compute_jo.sh '.$mday.' '.$mon.' '.$year);
+	if ($ret) {
+		exit;
+	}
+}
+
