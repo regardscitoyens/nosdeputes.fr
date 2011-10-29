8ebec83780d3155b3a0a61e9798b5ae09b6118d8
Regards Citoyens <contact@regardscitoyens.org>
2011-10-29 20:50:36 +0000

    fix tags
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/branches/senat/project@2427 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

--- lib/task/tagSeanceTask.class.php
+++ lib/task/tagSeanceTask.class.php
@@ -13,13 +13,17 @@ class tagSeanceTask extends sfBaseTask
  
   protected function count($array, $excludeS = 0) {
     foreach($array as $i) {
-      $i = preg_replace('/\([^\)]+\)/', '', $i['intervention']);
-      $i = strip_tags($i);
+      $i = strip_tags($i['intervention']);
+      $i = preg_replace('/<[^>]+>/', '', $i);
+      $i = html_entity_decode(str_replace('&nbsp;', ' ', htmlentities($i, ENT_COMPAT, 'UTF-8')), ENT_COMPAT, 'UTF-8');
+      $i = preg_replace('/\([^\)]+\)/', '', $i);
       $i = preg_replace('/&#339;/', 'oe', $i);
-      foreach(preg_split('/[\s\,\;\.\:\_\(\)\&\#\<\>\']+/i', $i) as $w) {
+      $i = str_replace(array(',',';','.',':','_','(',')','&','#','<','>','\'','"','«','»',' -','- ','?','!'), ' ', $i);
+      $i = preg_replace('/\s+/', ' ', $i);
+      foreach(explode(" ", $i) as $w) {
 	if (!preg_match('/^[A-Z]+$/', $w))
 	  $w = strtolower($w);
-	if (strlen($w)>1 && preg_match('/[a-z]/i', $w)) {
+	if (strlen($w)>2 && preg_match('/[a-z]/i', $w)) {
 	  //	  $s = soundex($w);
 	  $s = $w;
 	  if (!isset($words[$s]))
@@ -59,9 +63,9 @@ class tagSeanceTask extends sfBaseTask
     $cpt = 0;
     $tot = count($words);
 
-    $exclude = array('lecture'=>1, 'séance'=>1, 'alinéa'=>1, 'résolution'=>1, 'adoption'=>1, 'collègue'=>1, 'cher'=>1, 'collègues'=>1, 'chers'=>1,'bis'=>1, '1er'=>1, 'rectifié'=>1, 'question'=>1, 'rédactionnel'=>1, 'scrutin'=>1, 'exposer'=>1, 'identiques'=>1, 'identique'=>1, 'commission'=>1, 'adopte'=>1, 'rejette' => 1, 'additionnel' => 1, 'tendant' => 1, 'examiné' => 1, 'examine' => 1, 'rejeté'=> 1, 'avis' => 1, 'suivant'=>1, 'estimé'=>1, 'déclaré'=>1);
+    $exclude = array('vice-président' => 1, 'restant' => 1, 'mixte' => 1, 'paritaire' => 1, 'rapporteure' => 1, 'rapporteur' => 1, 'députée' => 1, 'député' => 1, 'sénateur' => 1, 'sénatrice' => 1, 'présidente' => 1, 'président' => 1, 'rédaction' => 1, 'issue' => 1, 'spéciale' => 1, 'adopté' => 1, 'lecture'=>1, 'séance'=>1, 'alinéa'=>1, 'résolution'=>1, 'adoption'=>1, 'collègue'=>1, 'cher'=>1, 'collègues'=>1, 'chers'=>1,'bis'=>1, '1er'=>1, 'rectifié'=>1, 'question'=>1, 'rédactionnel'=>1, 'scrutin'=>1, 'exposer'=>1, 'identiques'=>1, 'identique'=>1, 'commission'=>1, 'adopte'=>1, 'rejette' => 1, 'additionnel' => 1, 'tendant' => 1, 'examiné' => 1, 'examine' => 1, 'rejeté'=> 1, 'avis' => 1, 'suivant'=>1, 'estimé'=>1, 'déclaré'=>1);
     $include = array('télévision' => 1, 'dimanche'=>1, 'internet'=>1, 'outre-mer'=>1, 'logement'=>1, 'militaire'=>1, 'taxe'=>1, 'médecin'=>1, 'hôpital'=>1);
-    $exclude_sentences = array('garde des sceaux'=>1, 'haut-commissaire' => 1, 'monsieur' => 1, 'madame'=>1);
+    $exclude_sentences = array('commission spéciale' => 1, 'garde des sceaux'=>1, 'haut-commissaire' => 1, 'monsieur' => 1, 'madame'=>1, 'mixte paritaire' => 1, 'commission mixte' => 1, 'commission mixte paritaire' => 1);
 
     foreach(array_keys($words) as $k) {
       if (!isset($include[$k]))
@@ -81,13 +85,14 @@ class tagSeanceTask extends sfBaseTask
     unset($words);
 
     $qs = Doctrine::getTable('Seance')->createQuery()->select('id')->where('tagged IS NULL')->orderBy('date DESC');
+//$qs = Doctrine::getTable('Seance')->createQuery()->select('id')->where('id = ?', '1859');
 
     foreach($qs->fetchArray() as $s) {
       echo "Seance ".$s['id']." ..";
       
       //Recherche toutes les interventions pour cette séance
       $q = Doctrine_Query::create();
-      $q->select('intervention, id, parlementaire_id')->from('Intervention i')->where('seance_id = ?', $s['id'])->andWhere('( i.parlementaire_id IS NOT NULL OR i.personnalite_id IS NOT NULL )');//->andWhere('(i.fonction IS NULL OR i.fonction NOT LIKE ? )', 'président%');
+      $q->select('intervention, id, parlementaire_id')->from('Intervention i')->where('seance_id = ?', $s['id'])->andWhere('( i.parlementaire_id IS NOT NULL OR i.personnalite_id IS NOT NULL )')->andWhere('(i.fonction IS NULL OR i.fonction NOT LIKE ? )', 'président%');
 
       $array = $q->fetchArray();
       if (!count($array)) {
@@ -100,6 +105,7 @@ class tagSeanceTask extends sfBaseTask
       $tags = array();
       //Pour les mots le plus populaires non exclus on les gardes
       foreach(array_keys($words) as $k) {
+        $k = trim($k);
         if (!isset($exclude[$k])) {
           $cpt++;
           $pc = $words[$k]*100/$tot;
@@ -114,17 +120,20 @@ class tagSeanceTask extends sfBaseTask
       //On cherche des groupes de mots commums à partir des tags trouvés
       foreach ($array as $inter) {
         $i = null;
+        $inter['intervention'] = html_entity_decode(str_replace('&nbsp;', ' ', htmlentities($inter['intervention'], ENT_COMPAT, 'UTF-8')), ENT_COMPAT, 'UTF-8');
+
         foreach (array_keys($tags) as $tag) {
-          if (preg_match('/([^\s\,\.\:\>\;\(\)]*[^\,\.\:\>\;\(\)]{6}'.$tag.'[^\s\,\.\:\<\&\(\)]*)/i', $inter['intervention'], $match)) {
-            $sent = strtolower($match[1]);
-	    if (!isset($sentences[$sent]))
+          $srctag = preg_replace('/\//', '\/', $tag);
+          if (preg_match('/([^\s,\.:>;\(\)«»]*[^,\.:>;\(\)«»]{7}'.$srctag.'[^\s,\.:<\(\)«»]*)/i', $inter['intervention'], $match)) {
+            $sent = trim(strtolower($match[1]));
+            if (!isset($sentences[$sent]))
 	      $sentences[$sent] = 1;
 	    else
 	      $sentences[$sent]++;
             $sent2word[$sent] = $tag;
           }
-          if (preg_match('/([^\s\,\.\:\>\;\)\)]*'.$tag.'[^\,\.\:\<\&\(\)]{6}[^\s\,\.\:\<\&\(\)]*)/i', $inter['intervention'], $match)) {
-            $sent = strtolower($match[1]);
+          if (preg_match('/([^\s,\.:>;\(\)«»]*'.$srctag.'[^,\.:<\(\)«»]{7}[^\s,\.:<\(\)«»]*)/i', $inter['intervention'], $match)) {
+            $sent = trim(strtolower($match[1]));
 	    if (!isset($sentences[$sent]))
 	      $sentences[$sent] = 1;
 	    else
@@ -140,7 +149,6 @@ class tagSeanceTask extends sfBaseTask
       $debut_bani = 'à|de|la|ainsi|ensuite';
       if (count($sentences)) {
         foreach (array_keys($sentences) as $sent) {
-	  
           if  (preg_match("/^($debut_bani)/i", $sent) || preg_match("/($debut_bani)$/i", $sent) || preg_match('/\d|amendement|rapporteur|commision|collègue/i', $sent) )
             continue;
 	  
@@ -156,7 +164,7 @@ class tagSeanceTask extends sfBaseTask
           if (($sentences[$sent]*100/$tot > 0.8 || $sentences[$sent]*100/$words[$sent2word[$sent]] > 70)&& $words[$sent2word[$sent]] > 5) {
 	    $ok = 1;
 	    foreach($exclude_sentences as $excl_sent) {
-	      if (preg_match('/'.$excl_sent.'/', $sent)) {
+	      if (preg_match('/'.$excl_sent.'/i', $sent)) {
 		$ok = 0;
 		break;
 	      }
@@ -172,6 +180,7 @@ class tagSeanceTask extends sfBaseTask
       unset($sentences);
       unset($sent2word);
 
+foreach ($tags as $t => $n) print htmlentities($t, ENT_COMPAT, 'UTF-8')."\n";
       print_r($tags);
 
       //On cherche maintenant les tags dans les interventions pour les associer
@@ -183,7 +192,8 @@ class tagSeanceTask extends sfBaseTask
 
         $i = null;
         foreach (array_keys($tags) as $tag) {
-    	  if (preg_match('/'.$tag.'/i', $inter['intervention'])) {
+          $tag = trim($tag);
+    	  if (preg_match('/'.preg_replace('/\//', '\/', $tag).'/i', $inter['intervention'])) {
             if (!$i)
               $i = Doctrine::getTable('Intervention')->find($inter['id']);
             $i->addTag($tag);
