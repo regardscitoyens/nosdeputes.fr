5c2e3ec6372fd27aea1aa2c3e544e5af9c4a6e6b
RouxRC <b.ooghe@gmail.com>
2011-10-29 15:19:15 +0000

    oups
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/trunk/project@2420 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

--- lib/model/doctrine/Intervention.class.php
+++ lib/model/doctrine/Intervention.class.php
@@ -137,7 +137,7 @@ class Intervention extends BaseIntervention
   public function setContexte($contexte, $date = null, $timestamp = null, $tlois = null, $debug = 0) {
 
     if ($date && preg_match("/^(\d{4}-\d\d-\d\d)/", $date, $annee)) {
-      if (!preg_match("/^".$annee[1]."\d\d:\d\d$/")
+      if (!preg_match("/^".$annee[1]."\d\d:\d\d$/"))
         $date = $annee[1]."00:00";
     } else print "WARNING : Intervention $this->id has incorrect date : $date";
 
