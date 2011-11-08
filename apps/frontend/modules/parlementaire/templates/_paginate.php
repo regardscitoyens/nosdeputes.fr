a3570e6d32f25583241ea284aeae631994156592
Regards Citoyens <contact@regardscitoyens.org>
2011-11-08 13:47:42 +0000

    suggestion titles from mail
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/branches/senat/project@2478 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

--- apps/frontend/modules/parlementaire/templates/_paginate.php
+++ apps/frontend/modules/parlementaire/templates/_paginate.php
@@ -1,15 +1,15 @@
 <?php if ($pager->haveToPaginate()) : ?>
 <div class="pagination">
-   <?php if ($pager->getPage() != 1) echo link_to('<img alt="first" src="'.$sf_request->getRelativeUrlRoot().'/images/xneth/min2.png"/>&nbsp; ', $link.'page=1'); ?>
-    <?php if ($pager->getPage() != 1)  echo link_to('<img alt="left" src="'.$sf_request->getRelativeUrlRoot().'/images/xneth/left.png"/>&nbsp; ', $link.'page='.$pager->getPreviousPage()); ?>
+   <?php if ($pager->getPage() != 1) echo link_to('<img alt="first" title="première page" src="'.$sf_request->getRelativeUrlRoot().'/images/xneth/min2.png"/>&nbsp; ', $link.'page=1'); ?>
+    <?php if ($pager->getPage() != 1)  echo link_to('<img alt="left" title="page précédente" src="'.$sf_request->getRelativeUrlRoot().'/images/xneth/left.png"/>&nbsp; ', $link.'page='.$pager->getPreviousPage()); ?>
     <?php foreach ($pager->getLinks() as $page): ?>
       <?php if ($page == $pager->getPage()): ?>
         <span><b><?php echo $page ?></b></span>
       <?php else: ?>
-        <span><?php echo link_to($page, $link.'page='.$page); ?></span>
+        <span><a title="page <?php echo $page; ?>" href="<?php echo $link.'page='.$page; ?>"><?php echo $page; ?></a></span>
       <?php endif; ?>
     <?php endforeach; ?>
-    <?php if ($pager->getPage() != $pager->getLastPage()) echo link_to('<img alt="right" src="'.$sf_request->getRelativeUrlRoot().'/images/xneth/right.png"/>', $link.'page='.$pager->getNextPage()); ?>
-    <?php if ($pager->getPage() != $pager->getLastPage()) echo link_to('<img alt="last" src="'.$sf_request->getRelativeUrlRoot().'/images/xneth/max2.png"/> ', $link.'page='.$pager->getLastPage()); ?>
+    <?php if ($pager->getPage() != $pager->getLastPage()) echo link_to('<img alt="right" title="page suivante" src="'.$sf_request->getRelativeUrlRoot().'/images/xneth/right.png"/>', $link.'page='.$pager->getNextPage()); ?>
+    <?php if ($pager->getPage() != $pager->getLastPage()) echo link_to('<img alt="last" title="dernière page" src="'.$sf_request->getRelativeUrlRoot().'/images/xneth/max2.png"/> ', $link.'page='.$pager->getLastPage()); ?>
 </div>
 <?php endif; ?>
