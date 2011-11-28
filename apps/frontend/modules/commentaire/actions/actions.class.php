0b5f903be1fe266de4e32fb3a532edb4303c1dc5
Regards Citoyens <contact@regardscitoyens.org>
2011-11-28 18:42:12 +0000

    oups oubli
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/trunk/project@2510 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

--- apps/frontend/modules/commentaire/actions/actions.class.php
+++ apps/frontend/modules/commentaire/actions/actions.class.php
@@ -267,18 +267,6 @@ class commentaireActions extends sfActions
   }
 
   public function executeList(sfWebRequest $request) {
-    if ($this->type != 'all') {
-      $commentsids = array();
-      foreach (Doctrine_Query::create()
-        ->select('c.id')
-        ->from('Commentaire c')
-        ->where('c.id = (SELECT c2.id FROM commentaire c2 WHERE c.citoyen_id = c2.citoyen_id ORDER BY c2.created_at DESC LIMIT 1)')
-        ->andWhere('c.is_public = 1')
-        ->orderBy('c.created_at DESC')
-        ->fetchArray() as $com)
-          $commentsids[] = $com['id'];
-      $this->commentaires->andWhere('id IN ('.implode(',', array_values($commentsids)).')');
-    } else $this->commentaires->andWhere('is_public = 1');
     $this->titre = 'Les derniers commentaires';
     $this->url_link = '';
     if ($slug = $request->getParameter('slug')) {
