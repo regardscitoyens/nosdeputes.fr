99ea41f63cc767ff1e28b1c5d70b0cc5d4b04a99
Tangui Morlier <tangui@tangui.eu.org>
2011-12-03 18:46:52 +0000

    Optimisation pour mise à jour automatique (modified since)
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/branches/senat/project@2528 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

--- batch/commission/download_commission.pl
+++ batch/commission/download_commission.pl
@@ -5,9 +5,14 @@ use HTML::TokeParser;
 use URI::Escape;
 use Encode;
 
+#Prend en compte uniquement les pages de commission qui ont changé depuis moins de x heures (-1 pour désactiver)
+$since_hour = shift || 24;
+
 mkdir "html" unless -e "html";
 
 $a = WWW::Mechanize->new();
+$aif = WWW::Mechanize->new();
+$aif->add_header('If-Modified-Since' =>  scalar(localtime(time()-3600*$since_hour))) if ($since_hour > 0);
 $start = shift || '0';
 $count = 50;
 $ok = 1;
@@ -18,12 +23,16 @@ close FILE;
 
 foreach $index (@indexes) {
     chomp($index);
-    eval {$a->get($index);};
-    if ($a->status() == 404) {
+    eval {$aif->get($index);};
+    if ($aif->status() == 404) {
 	print "ERR: bad file in index : 404 on $index\n";
 	next;
     }
-    $content = $a->content;
+    $content = $aif->content;
+    if (!$content) {
+	next;
+    }
+    $a->get($index);
     $p = HTML::TokeParser->new(\$content);
     while ($t = $p->get_tag('a')) {
 	if ($t->[1]{href} =~ /compte-rendu-commissions/ || $t->[1]{href} =~ /\d{6}\d*.html/) {
@@ -45,15 +54,17 @@ foreach $index (@indexes) {
             $url{$uri} = 1;	    
 	    $file =  uri_escape($uri);
  	    if (-e "html/$file") { $a->back(); next; }
-	    print "$file\n";
 	    open FILE, ">:utf8", "html/$file.tmp";
 	    $thecontent = $a->content;
-            if ($thecontent =~ s/iso-8859-1/utf-8/gi) {
-              $thecontent = decode("windows-1252", $thecontent);
-            }
-	    print FILE $thecontent;
-	    close FILE;
-	    rename "html/$file.tmp", "html/$file"; 
+	    if ($thecontent) {
+		if ($thecontent =~ s/iso-8859-1/utf-8/gi) {
+		    $thecontent = decode("windows-1252", $thecontent);
+		}
+		print FILE $thecontent;
+		close FILE;
+		rename "html/$file.tmp", "html/$file";
+		print "$file\n";
+	    }
 	    $a->back();
 	}
     }
