a8d5c6f91aed119ecedceba08f4138cca1c48f57
Regards Citoyens <contact@regardscitoyens.org>
2012-01-01 05:37:20 +0000

    add numéro département + prenom à API
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/trunk/project@2550 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

--- apps/frontend/modules/api/actions/actions.class.php
+++ apps/frontend/modules/api/actions/actions.class.php
@@ -129,9 +129,10 @@ class apiActions extends sfActions
     $this->res['depute'] = array();
     $this->res['depute']['id'] = $depute->id * 1;
     $this->res['depute']['nom'] = $depute->nom;
-    $this->res['depute']['nom_de_famille'] = $depute->nom_de_famille;
+    $this->res['depute']['nom_de_famille'] = $depute->getNomFamilleCorrect();
     $this->res['depute']['prenom'] = $depute->getPrenom();
     $this->res['depute']['slug'] = $depute->getSlug();
+    $this->res['depute']['num_deptmt'] = $depute->getNumDepartement();
     $this->res['depute']['nom_circo'] = $depute->nom_circo;
     $this->res['depute']['num_circo'] = $depute->num_circo * 1;
     $this->res['depute']['mandat_debut'] = $depute->debut_mandat;
