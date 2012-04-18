40a0e9db78b3050ac1efcab87a11c9f96b4ce826
Regards Citoyens <contact@regardscitoyens.org>
2012-04-18 17:07:41 +0000

    plus propre
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/branches/senat/project@2657 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

--- bin/update_tops.sh
+++ bin/update_tops.sh
@@ -6,10 +6,7 @@ php symfony top:Senateurs
 for month in 10 11 12; do
   php symfony top:Senateurs 2004-$month-01
 done
-for year in 2005 2006 2007 2008 2009 2010 2011 2012; do
-  if test $year -gt `date +%Y`; then
-    break
-  fi
+for year in `seq 2005 $(date +%Y)`; do
   for month in 01 02 03 04 05 06 07 08 09 10 11 12; do
     if test $year -eq `date +%Y`; then
      if test $month -gt `date +%m`; then
