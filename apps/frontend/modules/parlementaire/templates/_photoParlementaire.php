8c505132b474f32131691de8695a3ef2fd8e5396
Tangui Morlier <tangui@tangui.eu.org>
2012-05-12 18:40:26 +0000

    Réserve l'espace pour les photos
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/trunk/project@2675 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

--- apps/frontend/modules/parlementaire/templates/_photoParlementaire.php
+++ apps/frontend/modules/parlementaire/templates/_photoParlementaire.php
@@ -6,4 +6,4 @@ if ($parlementaire->fin_mandat != null && $parlementaire->fin_mandat >= $parleme
   $groupe = "ancien".($parlementaire->sexe == "F" ? "ne" : "")." député".($parlementaire->sexe == "F" ? "e" : "");
 else $groupe = "Groupe parlementaire : ".$parlementaire->groupe_acronyme;
 if ($parlementaire->slug)
-echo '<img title="'.$parlementaire->nom.' -- ('.$groupe.')" src="'.url_for('@resized_photo_parlementaire?height='.$height.'&slug='.$parlementaire->slug.$add).'" class="jstitle photo_fiche" height="'.$height.'" alt="Photo de '.$parlementaire->nom.'"/>';
+  echo '<img title="'.$parlementaire->nom.' -- ('.$groupe.')" src="'.url_for('@resized_photo_parlementaire?height='.$height.'&slug='.$parlementaire->slug.$add).'" class="jstitle photo_fiche" width="'.round($height*156/200).'" height="'.$height.'" alt="Photo de '.$parlementaire->nom.'"/>';
