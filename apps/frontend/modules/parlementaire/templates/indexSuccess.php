3eb20947115f6ef52e7ed5ab5c1b89c7ed5976b6
Tangui Morlier <tangui@tangui.eu.org>
2012-05-13 19:44:28 +0000

    Législature au lieu de 12 mois qd fin leg
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/trunk/project@2693 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

--- apps/frontend/modules/parlementaire/templates/indexSuccess.php
+++ apps/frontend/modules/parlementaire/templates/indexSuccess.php
@@ -196,7 +196,12 @@ echo link_to($titretags, '@parlementaires_tags'); ?></h2>
   <?php echo include_component('tag', 'globalActivite'); ?>
   </div>
   <div class="box_repartition aligncenter"><div style="margin: auto;">
-  <h2><span style="margin-right: 5px;"><img alt="activite" src="<?php echo $sf_request->getRelativeUrlRoot(); ?>/images/xneth/ico_graph.png" /></span><a href="<?php echo url_for('@top_global'); ?>">Activité parlementaire des 12 derniers mois</a></h2>
+  <h2><span style="margin-right: 5px;"><img alt="activite" src="<?php echo $sf_request->getRelativeUrlRoot(); ?>/images/xneth/ico_graph.png" /></span><a href="<?php echo url_for('@top_global'); ?>">Activité parlementaire <?php
+if (myTools::isFinLegislature()) {
+echo 'de la législature';
+}else{
+echo 'des 12 derniers mois';
+}?></a></h2>
   <?php echo include_component('plot', 'newGroupes', array('type' => 'home')); ?>
   <?php // echo include_component('plot', 'groupes', array('plot' => 'total')); ?>
   </div></div>
