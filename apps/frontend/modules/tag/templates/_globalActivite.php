edfee1044665d062c0c7e2794b29be79caf13b39
Tangui Morlier <tangui@tangui.eu.org>
2012-05-13 20:57:10 +0000

    Ajout d'un lien vers tous les mots clés sur la home
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/trunk/project@2702 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

--- apps/frontend/modules/tag/templates/_globalActivite.php
+++ apps/frontend/modules/tag/templates/_globalActivite.php
@@ -1,1 +1,7 @@
-<?php echo include_component('tag', 'tagcloud', array('tagquery' => $itag, 'model'=>'Intervention', 'min_tag'=>2, 'limit'=>90, 'route'=>'@tag_interventions?')); ?>
+<?php echo include_component('tag', 'tagcloud', array('tagquery' => $itag, 'model'=>'Intervention', 'min_tag'=>2, 'limit'=>100, 'route'=>'@tag_interventions?')); ?>
+<p class="suivant" style="padding-right: 10px">
+<?php 
+    echo link_to('Voir tous les mots clés', 'parlementaire/tag'); 
+
+?>
+</p>
