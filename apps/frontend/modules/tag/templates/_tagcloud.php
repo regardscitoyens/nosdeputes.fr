53f00818af149ab2411650d773ef57b8e9423d40
Regards Citoyens <contact@regardscitoyens.org>
2012-05-19 19:52:01 +0000

    remove espaces insecables pour widget
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/trunk/project@2736 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

--- apps/frontend/modules/tag/templates/_tagcloud.php
+++ apps/frontend/modules/tag/templates/_tagcloud.php
@@ -16,7 +16,6 @@ if ($tot > 0) { foreach($keys as $tag) : ?>
 <span class="tag_level_<?php echo $tags[$tag]['class']; ?>"><?php if (isset($route)) {
 echo '<a href="'; 
 $rel = $tags[$tag]['related'];
-//$rel = preg_replace('/\s+/', '&nbsp;', $rel);
 $rel = preg_replace('/^aZ/', 'â', $rel);
 $rel = preg_replace('/^eZ/', 'é', $rel);
 $rel = preg_replace('/^EZ/', 'É', $rel);
@@ -24,12 +23,13 @@ $rel = preg_replace('/^iZ/', 'î', $rel);
 $rel = preg_replace('/^IZ/', 'Î', $rel);
 echo url_for($route.'tags='.$rel, $abs); ?>" title="<?php echo $tags[$tag]['count']; ?>"><?php
 } else if ($ct % 2 == 0) echo '<br/>';
-$nom = preg_replace('/\s+/', '&nbsp;', $tags[$tag]['tag']);
-$nom = preg_replace('/^aZ/', 'â', $nom);
+$nom = preg_replace('/^aZ/', 'â', $tags[$tag]['tag']);
 $nom = preg_replace('/^eZ/', 'é', $nom);
 $nom = preg_replace('/^EZ/', 'É', $nom);
 $nom = preg_replace('/^iZ/', 'î', $nom);
 $nom = preg_replace('/^IZ/', 'Î', $nom);
+if (!isset($absolute) || !$absolute)
+  $nom = preg_replace('/\s+/', '&nbsp;', $nom);
 echo $nom;
 if (isset($route)) { ?></a> <?php } 
 		 ?></span> <?php $ct++; endforeach; 
