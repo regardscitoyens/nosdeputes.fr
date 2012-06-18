d3631f14943b4e57e1d5c0d820d829a71397c0c8
Regards Citoyens <contact@regardscitoyens.org>
2012-06-18 19:17:18 +0000

    Mise en cohérence avec le reste des scripts d'update et de load
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/trunk/project@2789 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

--- bin/updateDB6.sh
+++ bin/updateDB6.sh
@@ -1,6 +1,5 @@
-. db.inc
+. bin/db.inc
 
 echo 'ALTER TABLE parlementaire ADD url_ancien_cpc TEXT NOT NULL , ADD url_nouveau_cpc TEXT NOT NULL'  | mysql $MYSQLID $DBNAME
 
-cd ../
 php symfony doctrine:build-model
