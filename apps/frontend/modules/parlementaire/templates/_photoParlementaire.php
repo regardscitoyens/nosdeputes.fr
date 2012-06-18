0d44de74fbd505a0d38f815c0ae1c933c304f3b5
Regards Citoyens <contact@regardscitoyens.org>
2012-06-18 23:02:05 +0000

    fix bulles infos manquantes + sources photos dans alt
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/trunk/project@2808 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

--- apps/frontend/modules/parlementaire/templates/_photoParlementaire.php
+++ apps/frontend/modules/parlementaire/templates/_photoParlementaire.php
@@ -1,12 +1,13 @@
 <?php 
 $add='';
+$groupe = "";
 if (isset($flip) && $flip)
   $add = '&flip='.$flip;
 if ($parlementaire->fin_mandat != null && $parlementaire->fin_mandat >= $parlementaire->debut_mandat)
-  $groupe = "ancien".($parlementaire->sexe == "F" ? "ne" : "")." député".($parlementaire->sexe == "F" ? "e" : "");
-else $groupe = "Groupe parlementaire : ".$parlementaire->groupe_acronyme;
+  $groupe = " -- (ancien".($parlementaire->sexe == "F" ? "ne" : "")." député".($parlementaire->sexe == "F" ? "e" : "").')';
+else if ($parlementaire->groupe_acronyme) $groupe = " -- (Groupe parlementaire : ".$parlementaire->groupe_acronyme.')';
 $abs = '';
 if (isset($absolute) && $absolute)
   $abs = 'absolute=true';
 if ($parlementaire->slug)
-  echo '<img title="'.$parlementaire->nom.' -- ('.$groupe.')" src="'.url_for('@resized_photo_parlementaire?height='.$height.'&slug='.$parlementaire->slug.$add, $abs).'" class="jstitle photo_fiche" style="width:'.round($height*156/200).'px; height:'.$height.'px;" alt="Photo de '.$parlementaire->nom.'"/>';
+  echo '<img alt="Photo issue du site de l\'Assemblée nationale ou de Wikipedia" title="'.$parlementaire->nom.$groupe.'" src="'.url_for('@resized_photo_parlementaire?height='.$height.'&slug='.$parlementaire->slug.$add, $abs).'" class="jstitle photo_fiche" style="width:'.round($height*156/200).'px; height:'.$height.'px;" alt="Photo de '.$parlementaire->nom.'"/>';
