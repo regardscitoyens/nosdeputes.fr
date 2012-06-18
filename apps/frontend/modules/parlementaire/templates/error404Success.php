4d24910f2a819f5bc9dd226cc74ea6663bae3177
Tangui Morlier <tangui@tangui.eu.org>
2012-06-18 19:57:25 +0000

    Ajout d'une redirection vers un autre domaine de cpc
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/trunk/project@2795 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

--- apps/frontend/modules/parlementaire/templates/error404Success.php
+++ apps/frontend/modules/parlementaire/templates/error404Success.php
@@ -1,1 +1,6 @@
-<h1>Nous n'avons pas pu trouver la page demandée</h1>
+<?php
+if (sfConfig::get('app_redirect404tohost')) {
+header("Location: http://".sfConfig::get('app_redirect404tohost').$_SERVER['REQUEST_URI']."\n");
+exit;
+}
+?><h1>Nous n'avons pas pu trouver la page demandée</h1>
