9b37331f1c87873e03f60b44306d734575d7e0b4
Regards Citoyens <contact@regardscitoyens.org>
2012-07-01 21:58:04 +0000

    fix multiple hover sur certains morceaux
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/trunk/project@2866 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

--- web/js/doubleHilight.js
+++ web/js/doubleHilight.js
@@ -8,11 +8,16 @@ $(".dep_map").live("mouseout", function() {
   $(".map"+dep).mouseout();
 })
 /* survol de la map */
-$("area").live("mouseover", function() {
+var d = { running: '0' };
+$("area").live("mouseover", d, function(e) {
   $(this).addClass("hover");
   dep = $(this).attr("id").substring(3,9).replace(/-0$/, "");
   $("#dep"+dep).css("background-color", "#D1EA74");
-  $(".map"+dep).filter(":not(.hover)").mouseover();
+  if (e.data.running == 0) {
+    d.running = 1;
+    $(".map"+dep).filter(":not(.hover)").mouseover();
+    d.running = 0;
+  }
 })
 $("area").live("mouseout", function() {
   $(this).removeClass("hover");
