d3cc98b562f5d6953522e3b31c1f56169423b401
Regards Citoyens <contact@regardscitoyens.org>
2012-07-01 16:19:44 +0000

    arf connerie de guillemet
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/branches/senat/project@2852 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

--- apps/frontend/templates/layout.php
+++ apps/frontend/templates/layout.php
@@ -142,16 +142,5 @@ $menu_citoyen = $selectcitoyen;
 		</div>
       </div>
     </div>
-<script type="text/javascript">
-// Google
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-10423931-6']);
-  _gaq.push(['_trackPageview']);
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-</script>
   </body>
 </html>
