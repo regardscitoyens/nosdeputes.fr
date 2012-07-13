158b3e107bf7794a7d3941b627e6a27bb5e22e6a
Regards Citoyens <contact@regardscitoyens.org>
2012-07-13 07:01:40 +0000

    fix problème images au reload d'une seance juste JO
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/trunk/project@2874 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

--- apps/frontend/modules/plot/templates/_groupes.php
+++ apps/frontend/modules/plot/templates/_groupes.php
@@ -129,10 +129,10 @@ if ($plot == 'total') {
 }
 $Test->xsRender($filename);
 if ($plot == 'total')
-  echo link_to(image_tag('tmp/xspchart/'.$filename, array('alt'=>"Répartition ".$titre, 'style'=>'width: '.$xsize.'px; height: '.$ysize.'px;')), '@top_global');
+  echo link_to(image_tag('tmp/xspchart/'.$filename, array('alt'=>"Répartition ".$titre, 'style'=>'height: '.$ysize.'px;')), '@top_global');
  else if (preg_match('/com/', $plot) && !isset($nolink))
-   echo link_to(image_tag('tmp/xspchart/'.$filename, array('alt'=>"Répartition ".$titre, 'style'=>'width: '.$xsize.'px; height: '.$ysize.'px;')), '@presents_seance?seance='.$seance);
- else echo image_tag('tmp/xspchart/'.$filename, array('alt'=>'Répartition '.$titre, 'style'=>'width: '.$xsize.'px; height: '.$ysize.'px;'));
+   echo link_to(image_tag('tmp/xspchart/'.$filename, array('alt'=>"Répartition ".$titre, 'style'=>'height: '.$ysize.'px;')), '@presents_seance?seance='.$seance);
+ else echo image_tag('tmp/xspchart/'.$filename, array('alt'=>'Répartition '.$titre, 'style'=>'height: '.$ysize.'px;'));
 
 echo include_partial('plot/groupesLegende', array());
 ?>
