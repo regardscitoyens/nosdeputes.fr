de71504b757c0e5b44489c826727c54ae7c390e6
RouxRC <b.ooghe@gmail.com>
2012-07-29 16:32:58 +0000

    fix à détecté comme é
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/trunk/project@2912 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

--- batch/hemicycle/parse_hemicycle.pl
+++ batch/hemicycle/parse_hemicycle.pl
@@ -158,13 +158,13 @@ sub checkout {
     $out .= '"timestamp": "';
     if ($intervenant) {
 	$ts = $cpt;
-	if ($intervenant =~ s/( et|, )(\s*M[mes\.]*|)\s*([A-Zé].*)$//) {
+	if ($intervenant =~ s/( et|, )(\s*M[mes\.]*|)\s*(([A-Z]|é).*)$//) {
 	    foreach $i (split(/ et |, /, $3)) {
 	        $ts++;
 	        print $out.$ts.'", "intervenant": "'.$i."\"}\n";
 	    }
 	}
-	if ($inter2fonction{$intervenant} =~ s/( et|, )(\s*M[mes\.]*|)\s*([A-Zé].*)//g) {
+	if ($inter2fonction{$intervenant} =~ s/( et|, )(\s*M[mes\.]*|)\s*(([A-Z]|é).*)//g) {
             $ts++;
 	    print $out.$ts.'", "intervenant": "'.$3."\"}\n";
 	    $inter2fonction{$intervenant} = '';
@@ -189,7 +189,7 @@ sub setFonction {
     $kfonction =~ s/[^a-z]+/ /gi;
     $intervenant =~ s/\W+$//;
     $fonction2inter{$kfonction} = $intervenant;
-#    print "$fonction ($kfonction)  => $intervenant \n";
+    #print "$fonction ($kfonction)  => $intervenant \n";
     if (!$inter2fonction{$intervenant}) {
 	$inter2fonction{$intervenant} = $fonction;
     }
