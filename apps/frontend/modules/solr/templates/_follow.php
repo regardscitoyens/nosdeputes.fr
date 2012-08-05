cc5752e9a9dd25f209c36067d01e3e04304f2d78
RouxRC <b.ooghe@gmail.com>
2012-08-05 17:57:27 +0000

    mieux là
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/trunk/project@2922 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

--- apps/frontend/modules/solr/templates/_follow.php
+++ apps/frontend/modules/solr/templates/_follow.php
@@ -1,7 +1,4 @@
 <div class="options">
-<?php if (sfConfig::get('app_redirect404tohost') && !isset($norss)) : ?>
-  <div class="mail"><h3 class="aligncenter"><a href="http://<?php echo sfConfig::get('app_redirect404tohost')."/".url_search($query, array()); ?>">Rechercher sur la<br/>précédente législature</a></h3></div>
-<?php endif ?>
   <div class="mail">
   <?php if (!isset($zero)) { ?>
   <h3 class="aligncenter">S'abonner aux résultats<br/>de cette recherche</h3>
@@ -20,5 +17,8 @@
        <td><a href="<?php echo url_for('alerte/create?filter='.urlencode($args).'&query='.urlencode($query)); ?>"><?php echo image_tag('xneth/email.png', 'alt="Email"'); ?></a><br/><a href="<?php echo url_for('alerte/create?filter='.urlencode($args).'&query='.urlencode($query)); ?>">par email</a></td>
        <td><?php if (!isset($norss)) : ?><a href="<?php $newargs_rss = $selected; $newargs_rss['format']['rss'] = 'rss'; if (isset($newargs_rss['date'])) unset($newargs_rss['date']); if (isset($newargs_rss['sort'])) unset($newargs_rss['sort']); echo url_for(url_search($query, $newargs_rss)); ?>"><?php echo image_tag('xneth/rss_obliq.png', 'alt="Flux rss"'); ?></a><br/><a href="<?php echo url_for(url_search($query, $newargs_rss)); ?>">par RSS</a><?php endif; ?></td>
 </tr></table></div>
+<?php if (sfConfig::get('app_redirect404tohost') && !isset($norss)) : ?>
+  <div class="facets"><h3 class="aligncenter"><a href="http://<?php echo sfConfig::get('app_redirect404tohost')."/".url_search($query, array()); ?>">Rechercher sur la<br/>précédente législature</a></h3></div>
+<?php endif ?>
 <?php if (!isset($opendiv) || !$opendiv) echo '</div>'; ?>
 
