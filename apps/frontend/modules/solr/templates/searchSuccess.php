37230d775c931d883137dc2e4d4f616c2e26eced
Regards Citoyens <contact@regardscitoyens.org>
2012-08-06 15:23:53 +0000

    Liens vers legislature suivante
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/trunk/project@2929 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

--- apps/frontend/modules/solr/templates/searchSuccess.php
+++ apps/frontend/modules/solr/templates/searchSuccess.php
@@ -254,16 +254,16 @@ function facet2Human($id, $facet = "") {
 <div id="results_container">
 <div class="options">
 <?php } ?>
-<?php if (sfConfig::get('app_redirect404tohost') || sfConfig::get('fin_legislature')) : ?>
+<?php if (sfConfig::get('app_redirect404tohost') || sfConfig::get('app_host_next_legislature')) : ?>
   <div class="facets">
-  <h3 class="aligncenter"><a href="http://<?php echo sfConfig::get('app_redirect404tohost').url_for(url_search($query, $selected)); ?>">Rechercher sur la<br/>précédente législature</a></h3>
+  <h3 class="aligncenter">Rechercher par législature</h3>
   <ul>
 <?php if (sfConfig::get('app_redirect404tohost')) : ?>
   <li><a href="http://<?php echo sfConfig::get('app_redirect404tohost').url_for(url_search($query, $selected)); ?>"><?php echo sfConfig::get('app_legislature') - 1; ?><sup>ème</sup> législature</a></li>
 <?php endif; ?>
   <li class="selected" style="font-weight: bold"><?php echo sfConfig::get('app_legislature'); ?><sup>ème</sup> législature</li>
-<?php if (sfConfig::get('app_fin_legislature')) : ?>
-  <li><a href="http://<?php echo sfConfig::get('app_redirect404tohost').url_for(url_search($query, $selected)); ?>"><?php echo sfConfig::get('app_legislature') + 1; ?><sup>ème</sup> législature</a></li>
+<?php if (sfConfig::get('app_host_next_legislature')) : ?>
+  <li><a href="http://<?php echo sfConfig::get('app_host_next_legislature').url_for(url_search($query, $selected)); ?>"><?php echo sfConfig::get('app_legislature') + 1; ?><sup>ème</sup> législature</a></li>
 <?php endif; ?>
   </ul>
   </div>
