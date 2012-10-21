a353cfb0a74e2251e3a2059410cb2532ee1b0267
Tangui Morlier <tangui@tangui.eu.org>
2012-10-21 15:49:33 +0000

    Oups c'est mieux avec l'utilisation de $type
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/branches/senat/project@3000 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

--- lib/model/doctrine/Intervention.class.php
+++ lib/model/doctrine/Intervention.class.php
@@ -72,7 +72,7 @@ class Intervention extends BaseIntervention
     }else{
       self::$seances = array();
     }
-    $seance = Doctrine::getTable('Seance')->findOneOrCreateIt('hemicycle', $date, $heure, $session, $commissiontxt);
+    $seance = Doctrine::getTable('Seance')->findOneOrCreateIt($type, $date, $heure, $session, $commissiontxt);
     $id = $this->_set('seance_id', $seance->id);
     self::$seances[$type.$date.$heure.$session.$commissiontxt] = $seance->id;
     return $id;
