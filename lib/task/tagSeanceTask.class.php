b7da2b5a68e5035efdf806145ddcf3232d143733
RouxRC <b.ooghe@gmail.com>
2013-01-05 12:49:13 +0000

    fix bad tags with -
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/trunk/project@3059 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

--- lib/task/tagSeanceTask.class.php
+++ lib/task/tagSeanceTask.class.php
@@ -11,8 +11,8 @@ class tagSeanceTask extends sfBaseTask
     $this->addOption('app', null, sfCommandOption::PARAMETER_OPTIONAL, 'Changes the environment this task is run in', 'frontend');
   }
  
-  protected function count($array, $excludeS = 0, $minsize = 1) {
-    foreach($array as $i) {
+  protected function count($interventions, $excludeS = 0, $minsize = 1) {
+    foreach($interventions as $i) {
       $i = preg_replace('/\([^\)]+\)/', '', $i);
       $i = preg_replace('/&#339;/', 'oe', $i['intervention']);
       foreach(preg_split('/[\s\,\;\.\:\_\(\)\&\#\<\>\']+/i', $i) as $w) {
@@ -58,14 +58,14 @@ class tagSeanceTask extends sfBaseTask
       $minsize = 6;
       $exclmin = 5;
     }
-    $array = $q->fetchArray();
-    $words = $this->count($array, 0, $minsize);
+    $interventions = $q->fetchArray();
+    $words = $this->count($interventions, 0, $minsize);
     $cpt = 0;
     $tot = count($words);
 
-    $exclude = array("action" => 1, "assemblée" => 1, "activité" => 1, "besoin" => 1, "définition" => 1, "adopté" => 1, "député" => 1, "spéciale" => 1, "spécial" => 1, 'rappelle' => 1, 'hong' => 1, "précédente" => 1, "information" => 1, "délai" => 1, 'applicable' => 1, 'expliqué' => 1, 'propose' => 1, 'relative' => 1, 'indique' => 1, 'vingt' => 1, 'janvier' => 1, 'puis' => 1, 'jour' => 1, 'lecture' => 1, 'séance' => 1, 'alinéa'=>1, 'résolution'=>1, 'adoption'=>1, 'collègue'=>1, 'cher'=>1, 'collègues'=>1, 'chers'=>1,'bis'=>1, '1er'=>1, 'rectifié'=>1, 'question'=>1, 'rédactionnel'=>1, 'scrutin'=>1, 'exposer'=>1, 'identiques'=>1, 'identique'=>1, 'commission'=>1, 'adopte'=>1, 'rejette' => 1, 'additionnel' => 1, 'tendant' => 1, 'examiné' => 1, 'examine' => 1, 'rejeté'=> 1, 'avis' => 1, 'suivant'=>1, 'estimé'=>1, 'déclaré'=>1, 'parce'=>1, 'beaucoup'=>1, 'afin'=>1, 'madame'=>1, 'sous'=>1, 'bonne'=>1, 'monsieur'=>1, 'quelle'=>1, 'quinze'=>1, 'lors'=>1, 'là'=>1, 'long'=>1, 'messieurs'=>1, 'ici'=>1, 'trois'=>1, 'êtes'=>1, 'serait'=>1, 'seront'=>1, 'dix'=>1, 'mot'=>1, 'vin'=>1, 'mon'=>1, 'hier'=>1, 'date'=>1, 'cinq'=>1, 'celui'=>1, 'allez' => 1, 'après' => 1, 'bureau' => 1, 'cause' => 1, 'sous-amendement' => 1, 'certaine' => 1, 'chose' => 1, 'code' => 1, 'compris' => 1, 'général' => 1, 'pris' => 1, 'demande' => 1, 'jeudi' => 1, 'jamais' => 1, 'juillet' => 1, 'mois' => 1, 'plusieurs' => 1, 'mardi' => 1, 'mercredi' => 1, 'lundi' => 1, 'quatre' => 1, 'semaine' => 1, 'suppression' => 1, 'semble' => 1, 'souvent' => 1, 'vers' => 1, 'jamais' => 1, 'comité' => 1, 'discussion' => 1, 'liens' => 1, 'lieux' => 1, 'membres' => 1, 'vendredi' => 1, 'dernière' => 1, 'donner' => 1, 'délégation' => 1, 'défendu' => 1, 'défavorable' => 1, 'exemple' => 1, 'favorable' => 1, 'fonction' => 1, 'grand' => 1, 'habitant' => 1, 'haut' => 1, 'juridique' => 1, 'mars' => 1, 'membre' => 1, 'mettre' => 1, 'mise' => 1, 'ministère' => 1, 'mission' => 1, 'niveau' => 1, 'oui' => 1, 'organique' => 1, 'objet' => 1, 'notion' => 1, 'norme' => 1, 'pense' => 1, 'première' => 1, 'prendre' => 1, 'principe' => 1, 'procédure' => 1, 'puisque' => 1, 'rien' => 1, 'sceaux' => 1, 'réponse' => 1, 'spécial' => 1, 'vise' => 1, 'vos' => 1, 'vote' => 1, 'suppresion' => 1, 'urgence' => 1, "rapporteur" => 1, "rapporteure" => 1);
+    $exclude = array("ayant" => 1, "grand-chose" => 1, "après-midi" => 1, "au-delà" => 1, "devant" => 1, "octobre" =>1, "novembre" => 1, "action" => 1, "assemblée" => 1, "activité" => 1, "besoin" => 1, "définition" => 1, "adopté" => 1, "député" => 1, "spéciale" => 1, "spécial" => 1, 'rappelle' => 1, 'hong' => 1, "précédente" => 1, "information" => 1, "délai" => 1, 'applicable' => 1, 'expliqué' => 1, 'propose' => 1, 'relative' => 1, 'indique' => 1, 'vingt' => 1, 'janvier' => 1, 'puis' => 1, 'jour' => 1, 'lecture' => 1, 'séance' => 1, 'alinéa'=>1, 'résolution'=>1, 'adoption'=>1, 'collègue'=>1, 'cher'=>1, 'collègues'=>1, 'chers'=>1,'bis'=>1, '1er'=>1, 'rectifié'=>1, 'question'=>1, 'rédactionnel'=>1, 'scrutin'=>1, 'exposer'=>1, 'identiques'=>1, 'identique'=>1, 'commission'=>1, 'adopte'=>1, 'rejette' => 1, 'additionnel' => 1, 'tendant' => 1, 'examiné' => 1, 'examine' => 1, 'rejeté'=> 1, 'avis' => 1, 'suivant'=>1, 'estimé'=>1, 'déclaré'=>1, 'parce'=>1, 'beaucoup'=>1, 'afin'=>1, 'madame'=>1, 'sous'=>1, 'bonne'=>1, 'monsieur'=>1, 'quelle'=>1, 'quinze'=>1, 'lors'=>1, 'là'=>1, 'long'=>1, 'messieurs'=>1, 'ici'=>1, 'trois'=>1, 'êtes'=>1, 'serait'=>1, 'seront'=>1, 'dix'=>1, 'mot'=>1, 'vin'=>1, 'mon'=>1, 'hier'=>1, 'date'=>1, 'cinq'=>1, 'celui'=>1, 'allez' => 1, 'après' => 1, 'bureau' => 1, 'cause' => 1, 'sous-amendement' => 1, 'certaine' => 1, 'chose' => 1, 'code' => 1, 'compris' => 1, 'général' => 1, 'pris' => 1, 'demande' => 1, 'jeudi' => 1, 'jamais' => 1, 'juillet' => 1, 'mois' => 1, 'plusieurs' => 1, 'mardi' => 1, 'mercredi' => 1, 'lundi' => 1, 'quatre' => 1, 'semaine' => 1, 'suppression' => 1, 'semble' => 1, 'souvent' => 1, 'vers' => 1, 'jamais' => 1, 'comité' => 1, 'discussion' => 1, 'liens' => 1, 'lieux' => 1, 'membres' => 1, 'vendredi' => 1, 'dernière' => 1, 'donner' => 1, 'délégation' => 1, 'défendu' => 1, 'défavorable' => 1, 'exemple' => 1, 'favorable' => 1, 'fonction' => 1, 'grand' => 1, 'habitant' => 1, 'haut' => 1, 'juridique' => 1, 'mars' => 1, 'membre' => 1, 'mettre' => 1, 'mise' => 1, 'ministère' => 1, 'mission' => 1, 'niveau' => 1, 'oui' => 1, 'organique' => 1, 'objet' => 1, 'notion' => 1, 'norme' => 1, 'pense' => 1, 'première' => 1, 'prendre' => 1, 'principe' => 1, 'procédure' => 1, 'puisque' => 1, 'rien' => 1, 'sceaux' => 1, 'réponse' => 1, 'spécial' => 1, 'vise' => 1, 'vos' => 1, 'vote' => 1, 'suppresion' => 1, 'urgence' => 1, "rapporteur" => 1, "rapporteure" => 1);
     $include = array('télévision' => 1, 'dimanche'=>1, 'internet'=>1, 'outre-mer'=>1, 'logement'=>1, 'militaire'=>1, 'taxe'=>1, 'médecin'=>1, 'hôpital'=>1);
-    $exclude_sentences = array('sceaux'=>1, 'commissaire' => 1, 'monsieur' => 1, 'madame'=>1, 'professeur' => 1, 'amendement' => 1, 'règlement' => 1, 'rectificative' => 1, 'rapporteur' => 1);
+    $exclude_sentences = array('vice-président' => 1, 'sceaux'=>1, 'commissaire' => 1, 'monsieur' => 1, 'madame'=>1, 'professeur' => 1, 'amendement' => 1, 'règlement' => 1, 'rectificative' => 1, 'rapporteur' => 1);
 
     foreach(array_keys($words) as $k) {
       if (!isset($include[$k]))
@@ -77,8 +77,8 @@ class tagSeanceTask extends sfBaseTask
     unset($words);
     $q = Doctrine_Query::create();
     $q->select('nom as intervention')->from('Parlementaire o');
-    $array = $q->fetchArray();
-    $words = $this->count($array, 0, $minsize);
+    $interventions = $q->fetchArray();
+    $words = $this->count($interventions, 0, $minsize);
     foreach(array_keys($words) as $k) {
       $exclude[$k] = 1;
     }
@@ -91,20 +91,20 @@ class tagSeanceTask extends sfBaseTask
       
       //Recherche toutes les interventions pour cette séance
       $q = Doctrine_Query::create();
-      $q->select('intervention, id, parlementaire_id')->from('Intervention i')->where('seance_id = ?', $s['id'])->andWhere('( i.parlementaire_id IS NOT NULL OR i.personnalite_id IS NOT NULL )')->andWhere('(i.fonction IS NULL OR i.fonction NOT LIKE ? )', 'président%');
+      $q->select('intervention, id, parlementaire_id')->from('Intervention i')->where('seance_id = ?', $s['id'])->andWhere('( i.parlementaire_id IS NOT NULL OR i.personnalite_id IS NOT NULL )')->andWhere('(i.type = ? OR i.fonction IS NULL OR i.fonction NOT LIKE ?)', array('commission', 'président%'));
 
-      $array = $q->fetchArray();
-      if (!count($array)) {
+      $interventions = $q->fetchArray();
+      if (!count($interventions)) {
 	echo " pas d'intervention trouvée\n";
         continue;
       }
-      $words = $this->count($array, 1, $minsize);
+      $words = $this->count($interventions, 1, $minsize);
       $cpt = 0;
       $tot = count($words);
       $tags = array();
       //Pour les mots le plus populaires non exclus on les gardes
       foreach(array_keys($words) as $k) {
-        if (!isset($exclude[$k])) {
+        if (!isset($exclude[$k]) && !preg_match('/-((il|elle)s|on|ci|le|[nv]ous)$/', $k)) {
           $cpt++;
           $pc = $words[$k]*100/$tot;
           if ($pc < 0.8)
@@ -116,7 +116,7 @@ class tagSeanceTask extends sfBaseTask
       $sentences = null;
       $sent2word = null;
       //On cherche des groupes de mots commums à partir des tags trouvés
-      foreach ($array as $inter) {
+      foreach ($interventions as $inter) {
         $i = null;
         foreach (array_keys($tags) as $tag) {
           if (preg_match('/([^\s\,\.\:\>\;\(\)]*[^\,\.\:\>\;\(\)]{6}'.$tag.'[^\s\,\.\:\<\&\(\)]*)/i', $inter['intervention'], $match)) {
@@ -177,7 +177,7 @@ class tagSeanceTask extends sfBaseTask
       //On cherche maintenant les tags dans les interventions pour les associer
       arsort($tags);
       $tagged = 0;
-      foreach ($array as $inter) {
+      foreach ($interventions as $inter) {
 	if (!$inter['parlementaire_id'])
 	  continue;
 
@@ -202,7 +202,7 @@ class tagSeanceTask extends sfBaseTask
 	$seance->free();
       }
       unset($tags);
-      unset($array);
+      unset($interventions);
       echo " done.";
       unset($s);
       if ($tagged == 0)
