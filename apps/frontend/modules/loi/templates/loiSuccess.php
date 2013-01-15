20bf8c54538458a38dd55b67df475f76efca6e3a
RouxRC <b.ooghe@gmail.com>
2013-01-15 02:36:31 +0000

    expose at the top and click-cut if javascript present
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/trunk/project@3069 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

--- apps/frontend/modules/loi/templates/loiSuccess.php
+++ apps/frontend/modules/loi/templates/loiSuccess.php
@@ -1,17 +1,28 @@
 <?php if (isset($dossier)) echo '<div class="source">'.link_to('Dossier relatif', '@section?id='.$dossier)."</div>"; ?>
 <div class="loi"><h1><?php echo $loi->titre; ?></h1></div>
 <?php if ($loi->source) echo '<p class="source"><a href="'.$loi->source.'" rel="nofollow">Source</a></p><div class="clear"></div>';
-if ($loi->parlementaire_id && $loi->expose) { ?>
-  <div class="loi"><div class="intervenant">
-  <?php $perso = $loi->getParlementaire();
-  if ($perso->getPageLink() && $photo = $perso->hasPhoto()) {
-    echo '<a href="'.url_for($perso->getPageLink()).'">';
-    include_partial('parlementaire/photoParlementaire', array('parlementaire' => $perso, 'height' => 70));
-    echo '</a>';
+if ($loi->expose) {
+  echo '<div class="loi"><h2>Exposé des motifs&nbsp;:</h2>';
+  if ($loi->parlementaire_id) {
+    echo '<div class="intervenant">';
+    $perso = $loi->getParlementaire();
+    if ($perso->getPageLink() && $photo = $perso->hasPhoto()) {
+      echo '<a href="'.url_for($perso->getPageLink()).'">';
+      include_partial('parlementaire/photoParlementaire', array('parlementaire' => $perso, 'height' => 70));
+      echo '</a>';
+    }
+    echo '</div>';
   }
-  echo '</div>';
-  echo myTools::escape_blanks($loi->expose);
-  echo '<div class="auteurloi"><a href="'.url_for($perso->getPageLink()).'">'.$perso->nom.'</a></div></div><br/>';
+  $expose = myTools::escape_blanks($loi->expose);
+  $pos = 0;
+  for ($i=0; $i < 8; $i++)
+    $pos = strpos($expose, '<', $pos+1);
+  echo substr($expose, 0, $pos-1);
+  echo '<div id="expose_court">Lire l\'exposé complet...</div>';
+  echo '<div id="expose_complet">'.substr($expose, $pos).'</div>';
+  if ($loi->parlementaire_id) 
+    echo '<div class="auteurloi"><a href="'.url_for($perso->getPageLink()).'">'.$perso->nom.'</a></div>';
+  echo '<br/><h2>Sommaire&nbsp;:</h2></div>';
 } ?>
 <div class="sommaireloi">
 <?php $nart = 0; $nbart = 0;
@@ -69,16 +80,6 @@ if (isset($soussections)) {
 } ?>
 </div>
 <br/>
-<?php if (!$loi->parlementaire_id && $loi->expose) {
-  echo '<div class="loi"><h2>Exposé des motifs&nbsp;:</h2>';
-  if ($loi->parlementaire_id && $perso = $loi->getParlementaire()) 
-    if ($perso->getPageLink() && $photo = $perso->hasPhoto()) {
-      echo '<div class="intervenant"><a href="'.url_for($perso->getPageLink()).'">';
-      include_partial('parlementaire/photoParlementaire', array('parlementaire' => $perso, 'height' => 70));
-      echo '</a></div>';
-    }
-  echo myTools::escape_blanks($loi->expose).'</div><br/>';
-} ?>
 <div class="commentaires">
   <h3>Derniers commentaires sur <?php echo preg_replace('/<br\/>.*$/', '', $loi->titre); ?> <span class="rss"><a href="<?php echo url_for('@loi_rss_commentaires?loi='.$loi->texteloi_id); ?>"><?php echo image_tag('xneth/rss.png', 'alt="Flux rss"'); ?></a></span></h3>
 <?php if ($loi->nb_commentaires == 0) echo '<p>Cette loi n\'a pas encore inspiré de commentaire aux utilisateurs.</p>';
@@ -88,4 +89,3 @@ else {
     echo '<p class="suivant">'.link_to('Voir les '.$loi->nb_commentaires.' commentaires', '@loi_commentaires?loi='.$loi->texteloi_id).'</p><div class="stopfloat"></div>';
 } ?>
 </div>
-
---
+++ web/js/exposeloi.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  $("#expose_complet").slideUp(0.5);
+  $("#expose_court").show();
+  $("#expose_court").bind("click", function() {
+    $("#expose_court").hide();
+    $("#expose_complet").slideDown(1);
+  });
+});
