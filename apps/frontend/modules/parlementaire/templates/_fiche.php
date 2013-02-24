b517e9cc8b7f0059283f88fabbce7c200700fa6c
RouxRC <b.ooghe@gmail.com>
2013-02-24 23:47:25 +0000

    remove box follow if mandate closed and say so
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/trunk/project@3106 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

--- apps/frontend/modules/parlementaire/templates/_fiche.php
+++ apps/frontend/modules/parlementaire/templates/_fiche.php
@@ -117,12 +117,16 @@ break; } ?></ul></li>
     <div class="b_d_h"><div class="b_d_hg"></div><div class="b_d_hd"></div></div>
     <div class="b_d_cont">
       <div class="b_d_infos">
+       <?php if ($parlementaire->fin_mandat && $parlementaire->fin_mandat >= $parlementaire->debut_mandat) { ?>
+    <h2>Le mandat de ce député est achevé.</h2>
+       <?php } else { ?>
     <h2>Suivre l'activité du député</h2>
 <table width=100% style="text-align: center"><tr>
        <td width=33%><a href="<?php echo url_for('@alerte_parlementaire?slug='.$parlementaire->slug); ?>"><?php echo image_tag('xneth/email.png', 'alt="Email"'); ?></a><br/><a href="<?php echo url_for('@alerte_parlementaire?slug='.$parlementaire->slug); ?>">par email</a></td>
        <td width=33%><a href="<?php echo url_for('@parlementaire_rss?slug='.$parlementaire->slug); ?>"><?php echo image_tag('xneth/rss_obliq.png', 'alt="Flux rss"'); ?></a><br/><a href="<?php echo url_for('@parlementaire_rss?slug='.$parlementaire->slug); ?>">par RSS</a></td>
        <td width=33%><a href="<?php echo url_for('@widget?depute='.$parlementaire->slug); ?>"><?php echo image_tag('xneth/widget.png', 'alt="Flux rss"'); ?></a><br/><a href="<?php echo url_for('@widget?depute='.$parlementaire->slug); ?>">sur mon site</a></td>
 </tr></table>
+       <?php } ?>
       <h2>Champ lexical <small>(<?php
 if (myTools::isFinLegislature()) {
 echo "sur l'ensemble de la législature";
