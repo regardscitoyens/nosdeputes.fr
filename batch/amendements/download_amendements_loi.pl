c7d10c6e0c82890cb0242973e80a08c435f927a5
RouxRC <b.ooghe@gmail.com>
2013-06-01 20:25:17 +0000

    add amendements commission to specific bill download
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/trunk/project@3199 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

--- batch/amendements/download_amendements_loi.pl
+++ batch/amendements/download_amendements_loi.pl
@@ -7,8 +7,26 @@ $count = 0;
 $legislature = shift || 14;
 $loi = shift;
 
-$url = "http://recherche2.assemblee-nationale.fr/amendements/resultats.jsp?typeEcran=avance&chercherDateParNumero=non&NUM_INIT=".$loi."&NUM_AMEND=&AUTEUR=&DESIGNATION_ARTICLE=&DESIGNATION_ALINEA=&SORT_EN_SEANCE=&DELIBERATION=&NUM_PARTIE=&DateDebut=&DateFin=&periode=&LEGISLATURE=".$legislature."Amendements&QueryText=&Scope=TEXTEINTEGRAL&SortField=ORDRE_TEXTE&SortOrder=Asc&searchadvanced=Rechercher&ResultMaxDocs=5000&ResultCount=5000";
+@urls = ("http://recherche2.assemblee-nationale.fr/amendements/resultats.jsp?typeEcran=avance&chercherDateParNumero=non&NUM_INIT=".$loi."&NUM_AMEND=&AUTEUR=&DESIGNATION_ARTICLE=&DESIGNATION_ALINEA=&SORT_EN_SEANCE=&DELIBERATION=&NUM_PARTIE=&DateDebut=&DateFin=&periode=&LEGISLATURE=".$legislature."Amendements&QueryText=&Scope=TEXTEINTEGRAL&SortField=ORDRE_TEXTE&SortOrder=Asc&searchadvanced=Rechercher&ResultMaxDocs=25000&ResultCount=25000");
 
+@organes = ("Affaires%20culturelles%20et%20%E9ducation",
+            "Affaires%20%E9conomiques",
+            "Affaires%20%E9trang%E8res",
+            "Affaires%20sociales",
+            "D%E9fense",
+            "D%E9veloppement%20durable",
+            "Lois",
+            "Finances",
+            "S%E9ance%20publique");
+foreach $organe (@organes) {
+  push(@urls, "http://recherche2.assemblee-nationale.fr/amendements/resultats.jsp?NUM_INIT=".$loi."&LEGISLATURE=".$legislature."&ORGANE=".$organe."&SortField=ORDRE_TEXTE&SortOrder=Asc&searchadvanced=Rechercher&ResultMaxDocs=25000&ResultCount=25000");
+}
+
+my %done;
+
+foreach $url (@urls) {
+
+print "-> Download amendements from $url\n";
 $a = WWW::Mechanize->new();
 $a->get($url);
 $content = $a->content;
@@ -18,7 +36,12 @@ while ($t = $p->get_tag('a')) {
     if ($t->[1]{class} eq 'lienamendement') {
 	$htmfile = $t->[1]{href};
 	next if ($htmfile =~ /(index|javascript)/);
-        $count++;
+    if ($done{$htmfile}) {
+        print "skip already dl\n";
+        next;
+    }
+    $done{$htmfile} = 1;
+    $count++;
 	$a->get($htmfile);
 	$htmfile =~ s/^\s+//gi;
 	$htmfile =~ s/\//_-_/gi;
@@ -31,6 +54,9 @@ while ($t = $p->get_tag('a')) {
 	$a->back();
     }
 }
-print $count." amendements pour les deux dernières semaines\n";
 
+}
+if ($count) {
+    print $count." amendements pour le projet de loi n°$loi\n";
+}
 
