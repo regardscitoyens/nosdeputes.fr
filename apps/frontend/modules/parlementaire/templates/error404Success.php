0ab362a473fe87c0641fe2c461ed50c1470db50e
Regards Citoyens <contact@regardscitoyens.org>
2013-06-04 23:49:50 +0000

    Meilleure gestion des redirections vers l'ancienne version (+update .htaccess)
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/trunk/project@3237 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

--- apps/frontend/modules/parlementaire/templates/error404Success.php
+++ apps/frontend/modules/parlementaire/templates/error404Success.php
@@ -1,6 +1,7 @@
 <?php
-if (sfConfig::get('app_host_previous_legislature')) {
+if (sfConfig::get('app_host_previous_legislature') && !preg_match('/^\/'.sfConfig::get('app_legislature').'\//', $_SERVER['REQUEST_URI'])) {
 header("Location: http://".sfConfig::get('app_host_previous_legislature').$_SERVER['REQUEST_URI']."\n");
 exit;
 }
+header("Status: 404 Not found");
 ?><h1>Nous n'avons pas pu trouver la page demandée</h1>
