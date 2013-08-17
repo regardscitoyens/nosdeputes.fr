ffd97997fa71422dc39df609a7a1501aa70d3bda
Regards Citoyens <contact@regardscitoyens.org>
2013-08-17 19:35:06 +0000

    update also proposition de résolution
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/trunk/project@3276 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

--- batch/documents/reload_latest_ppl.sh
+++ batch/documents/reload_latest_ppl.sh
@@ -2,7 +2,7 @@
 
 . ../../bin/db.inc
 
-for url in `echo "SELECT source FROM texteloi WHERE date > DATE_SUB(CURDATE(), INTERVAL 90 DAY) AND type = 'Proposition de loi' ORDER BY numero"  | mysql $MYSQLID $DBNAME | grep -v source`; do
+for url in `echo "SELECT source FROM texteloi WHERE date > DATE_SUB(CURDATE(), INTERVAL 90 DAY) AND type LIKE 'Proposition%' ORDER BY numero"  | mysql $MYSQLID $DBNAME | grep -v source`; do
   file=`echo $url | sed 's/\//_/g'`
   perl download_one.pl $url
   perl parse_metas.pl html/$file > out/$file
