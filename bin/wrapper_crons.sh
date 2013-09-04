0bc901e259d96ca70831739974c7b9054dadff22
Tangui Morlier <tangui@tangui.eu.org>
2013-09-04 14:56:15 +0000

    Ajout de la possibilité de communiquer avec un PAD
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/trunk/project@3280 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

--- bin/wrapper_crons.sh
+++ bin/wrapper_crons.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+. bin/db.inc
+
 command=`echo $2 | sed 's#^.*\/\([^\/]\+\)$#\\1#'`
 tmpout=cron-$command-`date +%d%m%Y-%HH%Mm%S`
 tmpoutput=$tmpout
@@ -10,6 +12,12 @@ while [ -f /tmp/$tmpoutput ]; do
 done
 
 "$@" >> /tmp/$tmpoutput 2>&1
+
+if test "$PADBOT" && test "$PADURL" ; then
+date > /tmp/$tmpoutput.date
+cat /tmp/$tmpoutput.date /tmp/$tmpoutput | grep -v '^==' | $PADBOT $PADURL write
+rm /tmp/$tmpoutput.date
+fi
 
 cat /tmp/$tmpoutput
 outsize=`ls -l /tmp/$tmpoutput | tail -n 1 | awk '{print $5}'`
