84ee5fc37b44d940106b4c0201b9d8b189112183
Regards Citoyens <contact@regardscitoyens.org>
2013-09-08 16:13:28 +0000

    Appels externes pour les gros textes
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/branches/senat/project@3290 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

--- apps/frontend/modules/api/templates/amendementsSuccess.php
+++ apps/frontend/modules/api/templates/amendementsSuccess.php
@@ -1,4 +1,12 @@
 <?php
 
-$task->run(array('format' => $format, 'loi_id'=>$loi));
+exec("php symfony print:dumpAmendementsLoiCsv $loi $format", $output,$ret);
+if ($ret) {
+        foreach($output as $o){
+                print $o;
+        }
+}else{
+        $task->run(array('format' => $format, 'loi_id'=>$loi));
+}
+
 
