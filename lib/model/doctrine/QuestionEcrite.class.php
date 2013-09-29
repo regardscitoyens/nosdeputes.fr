83b0e9c6a886e8ed1fc4098c7153df50144bc124
Regards Citoyens <contact@regardscitoyens.org>
2013-09-29 15:56:44 +0000

    fix noms courts ministères
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/trunk/project@3306 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

--- lib/model/doctrine/QuestionEcrite.class.php
+++ lib/model/doctrine/QuestionEcrite.class.php
@@ -54,16 +54,16 @@ class QuestionEcrite extends BaseQuestionEcrite
       $depute->free();
     }
   }
-  public function uniqueMinistere() 
+  public function uniqueMinistere()
   {
     $ministere = 'Ministère d';
-    if (preg_match('/((Droits|Réforme)?\s+(d[eu]s?\s*|l[\'a]\s*)+[\wàéëêèïîôöûüÉ]+)/', $this->ministere, $match)) $ministre = $match[1];
-    else if (preg_match('/(Affaires|Espace)(\s+[\wàéëêèïîôöûüÉ]+)/', $this->ministere, $match)) $ministre = $match[1].$match[2];
+    if (preg_match('/((Droits|Réforme)?\s+(d[eu]s?\s+|l[\'a]\s*)+[\wàéëêâèïîôöûüÉ]+)/', $this->ministere, $match)) $ministre = $match[1];
+    else if (preg_match('/(Affaires|Espace|Personnes)(\s+[\wàéëêèïîôöûüÉâ]+)/', $this->ministere, $match)) $ministre = $match[1].$match[2];
     else {
       $ministre = preg_replace('/^.*\/\s*([\wàéëêèïîôöûüÉ]+)$/', '\\1', $this->ministere);
       $ministre = preg_replace('/^([\wàéëêèïîôöûüÉ]+)[,\s].*$/', '\\1', $ministre);
     }
-    if (preg_match('/^(Droits|Aînés|Affaires|Sports|Transports|Solidarités)/', $ministre)) $ministere .= 'es ';
+    if (preg_match('/^(Droits|Aînés|Personnes|Affaires|Sports|Transports|Solidarités)/', $ministre)) $ministere .= 'es ';
     else if (preg_match('/^[AEÉIOU]/', $ministre)) $ministere .= 'e l\'';
     else if (preg_match('/^(Famille|Santé|Réforme|Coopération|Culture|Défense|Justice|Consommation|Prospective|Solidarité)/', $ministre)) $ministere .= 'e la ';
     else $ministere .= 'u ';
