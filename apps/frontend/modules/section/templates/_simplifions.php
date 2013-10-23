92c2c65e1194d33430574a4ce7ee4e96465a3d4f
Regards Citoyens <contact@regardscitoyens.org>
2013-10-23 09:41:58 +0000

    Exceptions dans l'affichage des textes en débat
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/trunk/project@3315 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

--- apps/frontend/modules/section/templates/_simplifions.php
+++ apps/frontend/modules/section/templates/_simplifions.php
@@ -2,7 +2,7 @@
 <div>
 <h2>Les dossiers en discussion sur "Simplifions la loi 2.0"</h2>
 <ul>
-<?php $done = array(); foreach($lois as $l) if (!isset($done[$l['Texteloi']['id_dossier_an']]) || preg_match('/^100[45]$/', $l['texteloi_id'])) : $done[$l['Texteloi']['id_dossier_an']] = 1; ?>
+<?php $done = array(); foreach($lois as $l) if ($l['texteloi_id'] != "1004" && (!isset($done[$l['Texteloi']['id_dossier_an']]) || preg_match('/^110[89]$/', $l['texteloi_id']))) : $done[$l['Texteloi']['id_dossier_an']] = 1; ?>
 <li><?php echo link_to(strip_tags($l['titre']), "@loi?loi=".$l['texteloi_id']); if ($l['nb_commentaires'] > 0) { echo ' (<span class="list_com">'.$l['nb_commentaires'].' commentaire'; if ($l['nb_commentaires'] > 1) echo 's'; echo '</span>)'; } ?></li>
 <?php endif; ?>
 </ul>
