95d021586873cd190fe869bf22773e83aac45865
Regards Citoyens <contact@regardscitoyens.org>
2014-01-05 18:58:25 +0000

    fix broken and missing presences internat
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/trunk/project@3340 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

--- batch/presences/parse_orga_inter.pl
+++ batch/presences/parse_orga_inter.pl
@@ -29,6 +29,12 @@ $html =~ s/&ccedil;/ç/ig;
 $html =~ s/<\/b>\s*<b>/ /gi;
 $html =~ s/<\/b>/<\/b>\n/g;
 $html =~ s/<b>/\n<b>/g;
+$titre =~ s/’/\'/g;
+$html =~ s/&#8217;/'/g;
+$html =~ s/&#8211;/-/g;
+$html =~ s/&#8209;/-/g;
+$html =~ s/\xc2\x92/'/g;
+$html =~ s/\xc2\x96/-/g;
 
 sub findDate($) {
 	$_ = shift;
@@ -42,12 +48,11 @@ sub findDate($) {
 	if (/\D(2\d{3})\D/) {
 		$year = $1;
 	}
-	while(/\D+(\d+)\D+(janvier|f[^v ]*vrier|mars|avril|mai|juin|juillet|ao[^t ]*t|septembre|octobre|novembre|d[^c ]*cembre)/gi) {
+	while(/\D+(\d+)[^\d<>]+(janvier|f[^v ]*vrier|mars|avril|mai|juin|juillet|ao[^t ]*t|septembre|octobre|novembre|d[^c ]*cembre)/gi) {
 		$jours[$cpt] = $1;
 		$mois[$cpt--] = $2;
 	}
 	if ($cpt > -1) {
-
 		$fin = $jours[0] = $jours[1];
 		$mois[0] = $mois[1];
 		if (/(\d+)\D{1,10}$fin\D/) {
@@ -57,13 +62,13 @@ sub findDate($) {
 
 	@datesrange = sort (join('-', datize($jours[0]." ".$mois[0]." ".$year)), join('-', datize($jours[1]." ".$mois[1]." ".$year)));
 	@dates =();
-	
+
 	my ( $year, $month, $day )  = split /-/, $datesrange[0];
 	return () if($year < 2000);
 	my ($eyear, $emonth, $eday) = split /-/, $datesrange[1];
 
 	my $end_date   = mktime( 0, 0, 0, $eday, $emonth -1, $eyear - 1900 );
-	while ( 1 ) { 
+	while ( 1 ) {
 	    my $date = mktime( 0, 0, 0, $day, $month - 1, $year - 1900 );
 	    ($sec, $min, $hour, $day, $month, $year, $wday, $yday) = localtime($date);
 	    $year += 1900; $month += 1;
@@ -76,37 +81,34 @@ sub findDate($) {
 }
 
 foreach (split /\n/, $html) {
-	if (/^<b>/) {
-		$titre = $_;
-		$titre =~ s/<[^>]*>//g;
-		$titre =~ s/&nbsp;/ /g;
-		$titre =~ s/\s+/ /g;
-		$titre =~ s/^\s*//;
-		$titre =~ s/\s*$//;
-		$titre =~ s/’/\'/g;
-		$titre =~ s/&#8217;/'/g;
-		$titre =~ s/&#8211;/-/g;
-		$titre =~ s/&#8209;/-/g;
-		$titre =~ s/\xc2\x92/'/g;
-		$titre =~ s/\xc2\x96/-/g;
-		@date = findDate($_);
-		$organisme = '';
-		if($titre =~ /groupe d'amitié/i) {
-		    $titre =~ s/\s+\-\s+/-/g;
-		    $titre =~ s/- France/-France/gi;
-		    $titre =~ s/Groupe d'amitié France \/ /Groupe d'amitié France-/gi;
-		    $titre =~ s/Royaume Uni/Royaume-Uni/gi;
-		    $titre =~ s/Île Maurice/Île-Maurice/gi;
-		    $titre =~ s/Union des Comores/France-Comores/gi;
-		    $titre =~ s/Cap Vert/Cap-Vert/gi;
-		    $titre =~ s/Burkina Faso/Burkina-Faso/gi;
-		    $titre =~ s/France-Union des Comores/France-Comores/gi;
-		    $organisme = lc($1) if ($titre =~ /(groupe d'amitié [^:.,\( ]*) ?/i);
-		    $organisme =~ s/(\S*)-france/France-$1/i;
-		}
-		next;
+	if (/^<b>/ && ! /fiches_id.(\d+).asp">/) {
+	  @try = findDate($_);
+      if (0+@try) {
+        @date = @try;
+        $titre = $_;
+        $titre =~ s/<[^>]*>//g;
+        $titre =~ s/&nbsp;/ /g;
+        $titre =~ s/\s+/ /g;
+        $titre =~ s/^\s*//;
+        $titre =~ s/\s*$//;
+        $organisme = '';
+        if($titre =~ /groupe d'amitié/i) {
+            $titre =~ s/\s+\-\s+/-/g;
+            $titre =~ s/- France/-France/gi;
+            $titre =~ s/Groupe d'amitié France \/ /Groupe d'amitié France-/gi;
+            $titre =~ s/Royaume Uni/Royaume-Uni/gi;
+            $titre =~ s/Île Maurice/Île-Maurice/gi;
+            $titre =~ s/Union des Comores/France-Comores/gi;
+            $titre =~ s/Cap Vert/Cap-Vert/gi;
+            $titre =~ s/Burkina Faso/Burkina-Faso/gi;
+            $titre =~ s/France-Union des Comores/France-Comores/gi;
+            $organisme = lc($1) if ($titre =~ /(groupe d'amitié [^:.,\( ]*) ?/i);
+            $organisme =~ s/(\S*)-france/France-$1/i;
+        }
+        %id = ();
+        next;
+      }
 	}
-	%id = ();
 	while (/fiches_id.(\d+).asp">([^<]*)<\/a>/g) {
 		$nom = $2; $id = $1;
 		$nom =~ s/\xc2\x92/'/g;
