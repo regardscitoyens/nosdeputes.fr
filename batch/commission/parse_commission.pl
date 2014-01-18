49908939e707dc68fecb1fcab1337b81ad4d5a93
Regards Citoyens <contact@regardscitoyens.org>
2014-01-18 19:47:49 +0000

    fix missing last n from fonctions
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/trunk/project@3365 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

--- batch/commission/parse_commission.pl
+++ batch/commission/parse_commission.pl
@@ -129,7 +129,7 @@ sub setFonction {
     $fonction =~ s/[^a-zàâéèêëîïôùûü]+$//i;
     $fonction =~ s/<[^>]+>\s*//g;
     $fonction =~ s/<[^>]*$//;
-    $fonction =~ s/[(n°\s]+$//;
+    $fonction =~ s/(n°|[(\s]+)$//;
     $fonction =~ s/\s+[0-9][0-9]?\s*$//;
     my $kfonction = lc($fonction);
     $kfonction =~ s/[^a-zéàè]+/ /gi;
