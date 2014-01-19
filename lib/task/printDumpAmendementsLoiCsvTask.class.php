4441522357224e6be31ed0a2f2d23f70c359377c
Regards Citoyens <contact@regardscitoyens.org>
2014-01-19 20:04:47 +0000

    Ajout des groupes parlementaires signataires
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/trunk/project@3369 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

--- lib/task/printDumpAmendementsLoiCsvTask.class.php
+++ lib/task/printDumpAmendementsLoiCsvTask.class.php
@@ -5,8 +5,8 @@ class printDumpAmendementsLoiCsvTask extends sfBaseTask {
     $this->namespace = 'print';
     $this->name = 'dumpAmendementsLoiCsv';
     $this->briefDescription = 'dump un csv contenant tous les amendements sur un texte de loi';
-    $this->addArgument('loi_id', sfCommandArgument::REQUIRED, 'Numero de loi'); 
-    $this->addArgument('format', sfCommandArgument::REQUIRED, 'Numero de loi'); 
+    $this->addArgument('loi_id', sfCommandArgument::REQUIRED, 'Numero de loi');
+    $this->addArgument('format', sfCommandArgument::REQUIRED, 'Numero de loi');
     $this->addOption('env', null, sfCommandOption::PARAMETER_OPTIONAL, 'Changes the environment this task is run in', 'dev');
     $this->addOption('app', null, sfCommandOption::PARAMETER_OPTIONAL, 'Changes the environment this task is run in', 'frontend');
  }
@@ -28,9 +28,12 @@ class printDumpAmendementsLoiCsvTask extends sfBaseTask {
     $res = array('amendements' => array());
     foreach ($amendements as $a) {
       $parlslugs = array();
-      foreach (Doctrine_Query::create()->select('p.slug')->from('Parlementaire p, ParlementaireAmendement pa')->where('p.id = pa.parlementaire_id')->andWhere('pa.amendement_id = ?', $a['id'])->orderBy('pa.numero_signataire')->fetchArray() as $s)
+      $parlgroup = array();
+      foreach (Doctrine_Query::create()->select('p.slug, p.groupe_acronyme')->from('Parlementaire p, ParlementaireAmendement pa')->where('p.id = pa.parlementaire_id')->andWhere('pa.amendement_id = ?', $a['id'])->orderBy('pa.numero_signataire')->fetchArray() as $s)
         $parlslugs[] = $s['slug'];
+        $parlgroup[] = $s['groupe_acronyme'];
       $a['parlementaires'] = myTools::array2hash($parlslugs, 'parlementaire');
+      $a['groupes_parlementaires'] = myTools::array2hash($parlgroup, 'groupe');
       $a['url_nosdeputes'] = preg_replace('#http://symfony/#', sfConfig::get('app_base_url'), url_for('@amendement?loi='.$loi.'&numero='.$a['numero'], 'absolute=true'));
       unset($a['num']);
       foreach(array_keys($a) as $key)
@@ -44,7 +47,7 @@ class printDumpAmendementsLoiCsvTask extends sfBaseTask {
         foreach(array_keys($champs) as $key)
           echo "$key;";
         echo "\n";
-        myTools::depile_csv($res, $breakline, array('parlementaire' => 1));
+        myTools::depile_csv($res, $breakline, array('parlementaire' => 1, 'groupe'=>1));
 	break;
       case 'xml':
         myTools::depile_xml($res, $breakline);
