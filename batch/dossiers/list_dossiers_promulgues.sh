109882b0053cddfba938f5ed2fa394bd71a6b3ef
Tangui Morlier <tangui@tangui.eu.org>
2014-03-16 23:09:57 +0000

    Prise en compte de la législature
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/trunk/project@3412 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

--- batch/dossiers/list_dossiers_promulgues.sh
+++ batch/dossiers/list_dossiers_promulgues.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+. ../../bin/db.inc
 
-curl -s "http://www.assemblee-nationale.fr/14/documents/index-promulgations.asp" | iconv -f ISO88591 -t UTF-8 | grep "/dossiers/" | sed 's|.* href="/14/dossiers/|http://www.assemblee-nationale.fr/14/dossiers/|' | sed 's/asp["#].*/asp/'
+curl -s "http://www.assemblee-nationale.fr/$LEGISLATURE/documents/index-promulgations.asp" | iconv -f ISO88591 -t UTF-8 | grep "/dossiers/" | sed 's|.* href="/'$LEGISLATURE'/dossiers/|http://www.assemblee-nationale.fr/'$LEGISLATURE'/dossiers/|' | sed 's/asp["#].*/asp/'
