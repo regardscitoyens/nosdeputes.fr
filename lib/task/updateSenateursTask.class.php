d380c6efbfabc7fae3ba4ef291c8a6efc751a8b9
Regards Citoyens <contact@regardscitoyens.org>
2015-02-27 16:33:23 +0000

    Vieille modification
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/branches/senat/project@3475 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

--- lib/task/updateSenateursTask.class.php
+++ lib/task/updateSenateursTask.class.php
@@ -47,6 +47,7 @@ class updateSenateursTask extends sfBaseTask
             }
 	    $parl = Doctrine::getTable('Parlementaire')->findOneByNom($json->nom);
 	    if (!$parl) {
+		echo "WARNGIN: New senateurs : ".$json->nom." \n";
 	      $parl = new Parlementaire();
 	      $parl->type = 'senateur';
 	      $parl->nom = $json->nom;
