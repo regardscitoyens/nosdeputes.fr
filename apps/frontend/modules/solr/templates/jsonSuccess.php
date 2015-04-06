38a257d42138ecd23f5de1e769c1111e37127de8
Regards Citoyens <contact@regardscitoyens.org>
2015-04-06 19:04:46 +0000

    fix json also for NS
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/branches/senat/project@3497 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

--- apps/frontend/modules/solr/templates/jsonSuccess.php
+++ apps/frontend/modules/solr/templates/jsonSuccess.php
@@ -8,12 +8,12 @@ if ($timefacet) {
 if($parlfacet) { 
 	$exportfacet = null; 
 	if(isset($facet['parlementaires'])) 
-		$exportfacet = $facet['parlementaires']; 
+		$exportfacet = $facet['parlementaires']['values']; 
 } 
 if ($tagsfacet) { 
 	$exportfacet = null; 
 	if (isset($facet['tag'])) 
-		$exportfacet = $facet['tag']; 
+		$exportfacet = $facet['tag']['values']; 
 } 
 if (isset($exportfacet)) { 
 	print(json_encode($exportfacet)); 
@@ -21,32 +21,31 @@ if (isset($exportfacet)) {
 }
  
 if (!$tags) : ?> 
-{ "start": <?php echo $results['start'] + 1; ?>, "end" : <?php if ($results['numFound'] == $results['end'] - 1) echo $results['end'] - 1; else echo $results['end']; ?>, "last_result" : <?php echo $results['numFound'];  ?>, "results" : {<?php
+{ "start": <?php echo $results['start'] + 1; ?>, "end": <?php if ($results['numFound'] == $results['end'] - 1) echo $results['end'] - 1; else echo $results['end']; ?>, "last_result" : <?php echo $results['numFound'];  ?>, "results": [<?php
 	    $nb = 0;
 foreach ($results['docs'] as $record)
 {
   if ($nb)
-    echo ",";
+    echo ", ";
   else
     $nb = 1;
-  echo "[document_type:\"".get_class($record['object'])."\",";
-  echo "document_id:".$record['object']->id.",";
-  echo "document_url:\"".sfConfig::get('app_baseurl').url_for('@api_document?format='.$format.'&class='.get_class($record['object']).'&id='.$record['object']->id)."\"]";
+  echo '{"document_type": "'.get_class($record['object']).'", ';
+  echo '"document_id": '.$record['object']->id.', ';
+  echo '"document_url": "'.sfConfig::get('app_baseurl').url_for('@api_document?format='.$format.'&class='.get_class($record['object']).'&id='.$record['object']->id).'"}';
 }
-?> }}<?php return;
+?> ]}<?php return;
 endif;
 ?>
-{
-<?php
-    $cpt = 0;
+[
+<?php $cpt = 0;
 foreach(array_keys($facet) as $k)
   if (isset($facet[$k]['values']) && count($facet[$k]['values']))
     foreach($facet[$k]['values'] as $value => $nb) 
       if ($nb) {
 	if ($cpt)
-	  echo ",";
+	  echo ", ";
 	else
 	  $cpt = 1;
-	echo "[\"tag_type\":\"$k\";\"tag_nom\":\"$value\",\"nb\":$nb]\n";
-      }
-  ?>}
+	echo '{"tag_type": "'.$k.'", "tag_nom": "'.$value.'", "nb": '.$nb."}\n";
+      } ?>
+]
