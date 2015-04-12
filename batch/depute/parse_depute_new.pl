21f58a3d85c1720bc885ae0ab10e8037db7dd960
Regards Citoyens <contact@regardscitoyens.org>
2015-04-12 16:11:49 +0000

    fix noms famille le mener + le calennec
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/trunk/project@3510 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

--- batch/depute/parse_depute_new.pl
+++ batch/depute/parse_depute_new.pl
@@ -286,12 +286,12 @@ if ((join " ", keys %{$depute{'mails'}}) =~ /(\S+)\@assemblee/) {
         $clogin =~ s/[ce]/./ig;
         $clogin =~ s/\.+/.+/g;
         for($i = 0 ; $i <= $#noms ; $i++) {
-            next if (lc($noms[$i]) eq "de");
+            next if ($noms[$i] =~ /^[ld]e/i);
             $tmpnom = lc($noms[$i]);
             $tmpnom =~ s/[àÀéÉèÈêÊëËîÎïÏôÔùÙûÛçÇ]/./ig;
             $tmpnom =~ s/\.+/.+/g;
             if ($login =~ /$tmpnom/i) {
-                if ($nomdep =~ /.(\s[l][ea]s?\s)?(\S*$tmpnom.*$)/i) {
+                if ($nomdep =~ /.\s([l][ea]s?\s)?(\S*?$tmpnom.*$)/i) {
                     $depute{'nom_de_famille'} = $1.$2;
                     last;
                 }
