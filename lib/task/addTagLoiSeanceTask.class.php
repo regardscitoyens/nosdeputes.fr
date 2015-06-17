04d2fb5f018e383ccf2047127d2cba4f751ff908
Regards Citoyens <contact@regardscitoyens.org>
2015-06-17 16:39:49 +0000

    tagging law on interventions should only apply to interv in a specific section
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/branches/senat/project@3567 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

--- lib/task/addTagLoiSeanceTask.class.php
+++ lib/task/addTagLoiSeanceTask.class.php
@@ -7,6 +7,7 @@ class addTagLoiSeanceTask extends sfBaseTask {
     $this->briefDescription = 'Corrige le tagging pour un numéro de loi mal enregistré de la forme xxxxyyyy';
     $this->addArgument('loi', sfCommandArgument::REQUIRED, 'numéro de loi');
     $this->addArgument('seance', sfCommandArgument::REQUIRED, 'id de la séance');
+    $this->addArgument('dossier', sfCommandArgument::REQUIRED, 'id du dossier a tagger');
     $this->addOption('env', null, sfCommandOption::PARAMETER_OPTIONAL, 'Changes the environment this task is run in', 'test');
     $this->addOption('app', null, sfCommandOption::PARAMETER_OPTIONAL, 'Changes the environment this task is run in', 'frontend');
  }
@@ -15,8 +16,10 @@ class addTagLoiSeanceTask extends sfBaseTask {
     $manager = new sfDatabaseManager($this->configuration);
     $interventions = Doctrine::getTable('Intervention')->findBySeanceId($arguments['seance']);
     if ($interventions) foreach($interventions as $i) {
-      $i->setLois($arguments['loi']);
-      $i->save();
+      if ($i->section_id == $arguments['dossier'] || $i->Section->section_id == $arguments['dossier']) {
+        $i->addTagLois(array($arguments['loi']));
+        $i->save();
+      }
     }
   }
 }
