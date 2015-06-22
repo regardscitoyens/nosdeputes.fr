04b5591136e5f1eb8ef998759037922dafe92b66
Regards Citoyens <contact@regardscitoyens.org>
2015-06-22 01:31:29 +0000

    remove empty html before parsing
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/trunk/project@3573 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

--- batch/commission/compute_latest.sh
+++ batch/commission/compute_latest.sh
@@ -6,6 +6,11 @@ if [ ! -d html ] ; then mkdir html; fi
 if [ ! -d out ] ; then mkdir out; fi
 
 for file in $(perl download_commission.pl $LEGISLATURE); do
+    if grep "La page à laquelle vous souhaitez accéder n'existe pas." "html/$file"; then
+        echo "...removing empty file."
+        rm "html/$file"
+        continue
+    fi
 	echo try ... ;
 	perl parse_commission.pl html/$file > out/$file ;
 	perl parse_presents.pl html/$file > presents/$file ;
