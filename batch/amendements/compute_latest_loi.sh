fc7fe82874d0486d3e42590264d65fd6d7e6bead
Regards Citoyens <contact@regardscitoyens.org>
2015-10-04 19:49:17 +0000

    silent crons
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/trunk/project@3594 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

--- batch/amendements/compute_latest_loi.sh
+++ batch/amendements/compute_latest_loi.sh
@@ -16,7 +16,7 @@ bash get_ids_loi.sh "$loi" | while read line; do
   id_dossier=$(echo $line | awk -F ";" '{print $1}')
   id_examen=$(echo $line | awk -F ";" '{print $2}')
   curl -sL "http://www2.assemblee-nationale.fr/recherche/query_amendements?typeDocument=amendement&idExamen=$id_examen&idDossierLegislatif=$id_dossier&rows=100000&tri=ordreTexteasc&typeRes=liste" |
-    python -m json.tool |
+    python -m json.tool 2> /dev/null |
     grep -P "http://\S+/amendements/" |
     awk -F "|" '{print $7}' |
     while read url; do
