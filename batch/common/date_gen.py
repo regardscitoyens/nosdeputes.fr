67ec495c7416422764dc13d173eaec1ff8d072e6
RouxRC <b.ooghe@gmail.com>
2016-05-08 02:59:48 +0000

    simplify code + add option to skip first date to avoid reloading last jo everytime
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/branches/senat/project@3681 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

--- batch/common/date_gen.py
+++ batch/common/date_gen.py
@@ -1,50 +1,40 @@
 #!/usr/bin/env python
 # -*- coding: utf8 -*-
-# Usage 'date_gen.py from_date [to_date]'
+# Usage 'date_gen.py from_date [to_date] [-s/--skip-first]'
 # Ce script utilisera la date d'aujourd'hui si "to_date" n'est pas précisé
-import re, sys, calendar
-from datetime import date, time, datetime
+import re, sys
+from datetime import date, timedelta
 
-from_date = sys.argv[1]
+re_date = re.compile('^([0-9]{4})-([0-9]{2})-([0-9]{2})$')
 
-reg = {}
-reg['date'] = '^([0-9]{4})-([0-9]{2})-([0-9]{2})$'
-
-if re.search(reg['date'], from_date) is not None:
-  m = re.search(reg['date'], from_date)
-  from_date = datetime(int(m.group(1)), int(m.group(2)), int(m.group(3))).date()
+if len(sys.argv) > 1 and re_date.search(sys.argv[1]):
+  m = re_date.search(sys.argv[1])
+  from_date = date(int(m.group(1)), int(m.group(2)), int(m.group(3)))
 else:
   sys.exit('Le 1er argument doit être la date de début (de la forme 2016-04-13)')
 
+def test_skiparg(i, fromdate):
+    if len(sys.argv) > i and sys.argv[i] in ['-s', '--skip-first']:
+        fromdate += timedelta(days=1)
+        i += 1
+    return i, fromdate
+
+idx, from_date = test_skiparg(2, from_date)
 try:
-  to_date = sys.argv[2]
-  if re.search(reg['date'], to_date) is not None:
-    m = re.search(reg['date'], to_date)
-    to_date = datetime(int(m.group(1)), int(m.group(2)), int(m.group(3))).date()
+  to_date = sys.argv[idx]
+  if re_date.search(to_date):
+    m = re_date.search(to_date)
+    to_date = date(int(m.group(1)), int(m.group(2)), int(m.group(3)))
   else:
     sys.exit('Le 2nd argument (optionnel) doit être la date de fin (de la forme 2016-04-13)')
 except IndexError:
-  to_date = datetime.now().date()
+  to_date = date.today()
+idx, from_date = test_skiparg(idx+1, from_date)
 
-cal = calendar.Calendar()
+if to_date < from_date:
+  sys.exit('La date de début doit être antérieure à la date de fin')
 
-for year in range(from_date.year, to_date.year+1):
-
-  # 1ère année
-  if from_date.year == year:
-    start_month = from_date.month
-  else:
-    start_month = 1
-
-  # dernière année
-  if to_date.year == year:
-    stop_month = to_date.month
-  else:
-    stop_month = 12
-
-  for month in range(start_month, stop_month+1):
-
-    for day in cal.itermonthdates(year, month):
-
-      if day >= from_date and day.month == month and day <= to_date:
-        print(day)
+date = from_date
+while date <= to_date:
+  print(date)
+  date += timedelta(days=1)
--- batch/jo/compute_latest.sh
+++ batch/jo/compute_latest.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Usage bash compute_latest.sh chamber
 # "chamber" peut prendre pour valeur : "an" ou "senat"
-for date in $(python ../common/date_gen.py $(find ./html -name "coms_$1_*" | sed -e 's/.*_//' | sed -e 's/\..*//' | sort | tail -1)) ; do python parse_jo.py "$1" "$date" ; done
+for date in $(python ../common/date_gen.py $(find ./html -name "coms_$1_*" | sed -e 's/[^0-9-]//g' | sort | tail -1) --skip-first) ; do python parse_jo.py "$1" "$date" ; done
