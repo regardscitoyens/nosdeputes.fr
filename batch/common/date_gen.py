a7c457c63c5242693f636a9703baeeb1f1988757
RouxRC <b.ooghe@gmail.com>
2016-05-08 02:58:00 +0000

    add option to skip first date to avoid reloading last jo everytime
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/trunk/project@3680 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

--- batch/common/date_gen.py
+++ batch/common/date_gen.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf8 -*-
-# Usage 'date_gen.py from_date [to_date]'
+# Usage 'date_gen.py from_date [to_date] [-s/--skip-first]'
 # Ce script utilisera la date d'aujourd'hui si "to_date" n'est pas précisé
 import re, sys
 from datetime import date, timedelta
@@ -13,8 +13,15 @@ if len(sys.argv) > 1 and re_date.search(sys.argv[1]):
 else:
   sys.exit('Le 1er argument doit être la date de début (de la forme 2016-04-13)')
 
+def test_skiparg(i, fromdate):
+    if len(sys.argv) > i and sys.argv[i] in ['-s', '--skip-first']:
+        fromdate += timedelta(days=1)
+        i += 1
+    return i, fromdate
+
+idx, from_date = test_skiparg(2, from_date)
 try:
-  to_date = sys.argv[2]
+  to_date = sys.argv[idx]
   if re_date.search(to_date):
     m = re_date.search(to_date)
     to_date = date(int(m.group(1)), int(m.group(2)), int(m.group(3)))
@@ -22,6 +29,7 @@ try:
     sys.exit('Le 2nd argument (optionnel) doit être la date de fin (de la forme 2016-04-13)')
 except IndexError:
   to_date = date.today()
+idx, from_date = test_skiparg(idx+1, from_date)
 
 if to_date < from_date:
   sys.exit('La date de début doit être antérieure à la date de fin')
