71df998de38dd064e30c1a13277d506da3cc18dd
Regards Citoyens <contact@regardscitoyens.org>
2016-10-02 17:08:28 +0000

    add option to limit to one specific seance
    git-svn-id: https://cpc.regardscitoyens.org/svn/cpc/trunk/project@3695 9954c6d0-a799-495f-bc23-bbe1ca3f12d5

--- lib/task/fixPersoToParlTask.class.php
+++ lib/task/fixPersoToParlTask.class.php
@@ -7,7 +7,8 @@ class fixPersoToParlTask extends sfBaseTask {
     $this->briefDescription = "Supprime une personnalité et réaffecte ses interventions (et présences) à un parlementaire";
     $this->addArgument('perso', sfCommandArgument::REQUIRED, 'id ou nom de la personnalite');
     $this->addArgument('parl', sfCommandArgument::REQUIRED, 'id ou nom du parlementaire');
-    $this->addArgument('update', sfCommandArgument::OPTIONAL, 'Optionnel : remplir pour passer de simple affichage a modifs réelles');
+    $this->addArgument('seance', sfCommandArgument::OPTIONAL, 'Optionnel : remplir pour activer modifs réelles sur seance indiquée');
+    $this->addArgument('fonction', sfCommandArgument::OPTIONAL, 'Optionnel : remplir pour donner une fonction au parlementaire sur les interventions modifiées');
     $this->addOption('env', null, sfCommandOption::PARAMETER_OPTIONAL, 'Changes the environment this task is run in', 'prod');
     $this->addOption('app', null, sfCommandOption::PARAMETER_OPTIONAL, 'Changes the environment this task is run in', 'frontend');
  }
@@ -17,24 +18,28 @@ class fixPersoToParlTask extends sfBaseTask {
     $perso = Doctrine::getTable('Personnalite')->findOneByIdOrName($arguments['perso']);
     $parl = Doctrine::getTable('Parlementaire')->findOneByIdOrName($arguments['parl']);
     if (!$perso || !$parl) return;
-    $fct = (preg_match('/, (.+)$/', $perso->nom, $m) ? $m[1] : null);
+    $fct = ($arguments['fonction'] ? $arguments['fonction'] : (preg_match('/, (.+)$/', $perso->nom, $m) ? $m[1] : null));
     print "FOUND perso: ".$perso->nom." ".$fct."\n";
     print "FOUND parl : ".$parl->nom."\n";
-    if ($arguments['update'])
+    if ($arguments['seance'])
       print "Applying changes...\n";
     else print "Impacted interventions:\n";
     $interventions = Doctrine::getTable('Intervention')->findByPersonnalite($perso->id);
     if ($interventions) {
+      $rmperso = true;
       foreach($interventions as $i) {
         print $i->id." ".$i->fonction." http://nosdeputes.fr/14/seance/".$i->seance_id.'#inter_'.$i->getMd5()."\n";
-        if ($arguments['update']) {
+        if ($arguments['seance'] && $i->seance_id == $arguments['seance']) {
           $i->setParlementaire($parl);
           if ($fct) $i->setFonction($fct);
           $i->save();
           $parl = Doctrine::getTable('Parlementaire')->findOneByIdOrName($arguments['parl']);
         }
+        if ($i->seance_id != $arguments['seance']) {
+          $rmperso = false;
+        }
       }
-      if ($arguments['update']) {
+      if ($arguments['seance'] && $rmperso) {
         $query = Doctrine_Query::create()
           ->delete('Personnalite p')
           ->where('p.id = ?', $perso->id);
