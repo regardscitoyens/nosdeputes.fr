361d0b94b0698e9450eb294871b62af5147e4318
RouxRC <b.ooghe@gmail.com>
2017-01-08 19:07:18 +0100

    fix ppls

--- batch/documents/reload_all_ppls.sh
+++ batch/documents/reload_all_ppls.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-curl -sL "http://www2.assemblee-nationale.fr/documents/liste/(ajax)/1/(limit)/10000/(type)/propositions-loi" | sed 's|14/ta/ta0080/|14/propositions/pion0733/|' | grep "documents/notice/14/pro" all-ppls | sed 's/^.*href="//' | sed 's/"><i.*$//' | sed 's|/documents/notice/14/propositions/pion\([0-9]\+\)/(index)/propositions-loi|http://www.assemblee-nationale.fr/14/propositions/pion\1.asp|' > all-ppls
+curl -sL "http://www2.assemblee-nationale.fr/documents/liste/(ajax)/1/(limit)/10000/(type)/propositions-loi" | sed 's|14/ta/ta0080/|14/propositions/pion0733/|' | grep "documents/notice/14/pro" | sed 's/^.*href="//' | sed 's/"><i.*$//' | sed 's|/documents/notice/14/propositions/pion\([0-9]\+\)/(index)/propositions-loi|http://www.assemblee-nationale.fr/14/propositions/pion\1.asp|' > all-ppls
 
 cat all-ppls | while read url; do
   file=`echo $url | sed 's/\//_/g'`
   perl download_one.pl $url
   perl parse_metas.pl html/$file > out/$file
   mv html/$file ppl/$file
-done;
+done
 
 curl -sL "http://www.assemblee-nationale.fr/14/documents/index-resolutions.asp" > /tmp/all-pprs
 curl -sL "http://www.assemblee-nationale.fr/14/documents/index-enquete-resolution.asp" >> /tmp/all-pprs
@@ -21,5 +21,5 @@ cat all-pprs | while read url; do
   perl download_one.pl $url
   perl parse_metas.pl html/$file > out/$file
   mv html/$file ppr/$file
-done;
+done
 
