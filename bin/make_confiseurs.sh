b295e5e3e00a028b30b8f4bb68f8fe6d798e6d53
Tangui Morlier <tangui@tangui.eu.org>
2017-02-16 23:53:10 +0100

    - Conserve l'année passée en parametre (plutot que d'en faire qu'un incrément) - msg d'erreur si pas de parametre - clean le fichier des lignes vides

--- bin/make_confiseurs.sh
+++ bin/make_confiseurs.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 
-source $(dirname $0)/bin/db.inc
+source $(dirname $0)/../bin/db.inc
 
 COMMISSIONS="9, 10, 11, 12, 13, 14, 15, 16, 20"
 GROUPES="2, 3, 4, 5, 6, 7, 8, 427, 428, 858, 1002, 1128, 1023, 1118"
 
-year=$1
+YEAR=$1
+year=$YEAR
+if ! test "$year"; then
+	echo "ERROR: $0 <annee_debut_session>";
+	exit 1;
+fi
 
-CSVDIR=$(dirname $0)/../$CSVDIR/
+CSVDIR=$(dirname $0)/../csv
 mkdir -p $CSVDIR
 
 echo "SELECT pa.id as id, pa.nom as nom, pa.nom_de_famille as nom_de_famille, pa.groupe_acronyme as groupe, pa.nom_circo, pa.debut_mandat as debut_mandat, pa.fin_mandat as fin_mandat, o.slug as commission FROM parlementaire pa LEFT JOIN parlementaire_organisme po ON ( po.parlementaire_id = pa.id ) JOIN organisme o ON ( o.id = po.organisme_id ) WHERE o.id IN ("$COMMISSIONS") ORDER BY id" | mysql $MYSQLID $DBNAME | sed 's/\t/;/g' | iconv --from-code=ISO-8859-1 --to-code=UTF-8 > $CSVDIR/deputes.txt
 
 echo 'SELECT distinct(date), id FROM seance WHERE DAYOFWEEK(date) = 4 AND YEAR(date) IN ( 2012, 2013, 2014, 2015, 2016, 2017 ) AND type = "hemicycle" AND ( moment LIKE "1ère%" OR moment LIKE "08:%" OR moment LIKE "09:%" OR moment LIKE "10:%" OR moment LIKE "11:%" OR moment LIKE "12:%" OR moment LIKE "13:%" ) order by date desc' | mysql $MYSQLID $DBNAME | sed 's/\t/;/g' | iconv --from-code=ISO-8859-1 --to-code=UTF-8 > $CSVDIR/seances_hemicycle_merc_matin.txt
 
-rm $CSVDIR/presences_deputes_mercredimatin.csv $CSVDIR/commissions_mercredimatin.csv
+rm -f $CSVDIR/presences_deputes_mercredimatin.csv $CSVDIR/commissions_mercredimatin.csv
 
 for month in 10 11 12 01 02 03 04 05 06; do
 
@@ -31,8 +36,6 @@ for month in 10 11 12 01 02 03 04 05 06; do
     fi
     year2=$year
   fi
-
-  echo $year'-'$month'-01->'$year2'-'$month2'-01 : $CSVDIR/'$year'-'$month'.txt'
 
   echo 'SELECT pa.id as id, "'$year'-'$month'", count(DISTINCT(p.date)) AS n_'$year'_'$month' FROM parlementaire pa LEFT JOIN presence p ON ( pa.id = p.parlementaire_id ) LEFT JOIN seance s ON ( s.id = p.seance_id ) WHERE DAYOFWEEK(p.date) = 4 AND MONTH(p.date) = '$month' AND YEAR(p.date) = '$year' AND s.type = "commission" AND s.organisme_id IN ('$COMMISSIONS') AND ( s.moment LIKE "1ère%" OR s.moment LIKE "08:%" OR s.moment LIKE "09:%" OR s.moment LIKE "10:%" OR s.moment LIKE "11:%" OR s.moment LIKE "12:%" OR s.moment LIKE "13:%" ) AND pa.debut_mandat <= CAST("'$year'-'$month'-01" as DATE) AND ( pa.fin_mandat IS NULL OR pa.fin_mandat >= CAST("'$year2'-'$month2'-01" as DATE) ) GROUP BY pa.id ORDER BY pa.id' | mysql $MYSQLID $DBNAME | sed 's/\t/;/g' | iconv --from-code=ISO-8859-1 --to-code=UTF-8 > $CSVDIR/$year-$month.txt
 
@@ -65,4 +68,7 @@ tail -n +2 $CSVDIR/exoneres.txt | sed 's/;.*//' | while read exo ; do
 	sed -i "s/^$exo;.*//" $CSVDIR/presences_deputes_mercredimatin_with_commissions.sorted.csv
 done
 
+grep -i "[a-z]" $CSVDIR/presences_deputes_mercredimatin_with_commissions.sorted.csv > $CSVDIR/$YEAR"_presences_deputes_mercredimatin_with_commissions.csv"
 
+
+echo "Sanction généré dans $CSVDIR/"$YEAR"_presences_deputes_mercredimatin_with_commissions.csv";
