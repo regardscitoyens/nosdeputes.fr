7d6d786e0fe6001342ac41188fae7c6e68706fc5
Benjamin Ooghe-Tabanou <benjamin.ooghe@sciences-po.fr>
2017-04-01 19:28:49 +0200

    script to load all first auteurs

---
+++ bin/load_amdmts_first_auteurs.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd batch/amendements
+bash runAmdtOD.sh
+cd -
+while ls batch/amendements/OpenDataAN/amdts_* | grep [a-z] > /dev/null ; do
+	php symfony cc --env=test  --app=frontend > /dev/null
+	php symfony update:Amdmts
+done
