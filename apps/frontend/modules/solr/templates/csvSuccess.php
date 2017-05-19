2c6130ffc176193b7974975fd3c979e2b087a8bd
RouxRC <b.ooghe@gmail.com>
2017-05-19 20:24:32 +0200

    allow facets exports in csv api

--- apps/frontend/modules/solr/templates/csvSuccess.php
+++ apps/frontend/modules/solr/templates/csvSuccess.php
@@ -1,4 +1,15 @@
-<?php if (!$tags) :?>
+<?php
+if($parlfacet) {
+    if(isset($facet['parlementaires']))
+        $tags = $facet;
+}
+if ($tagsfacet) {
+    if (isset($facet['tag']))
+        $tags = $facet;
+}
+
+
+if (!$tags) :?>
 type document;document id;url vers document;resultats <?php echo $results['start'] + 1; ?> à <?php if ($results['numFound'] == $results['end'] - 1) echo $results['end'] - 1; else echo $results['end']; ?> sur <?php echo $results['numFound']; ?>
 
 <?php
@@ -21,4 +32,4 @@ foreach(array_keys($facet) as $k)
   if (isset($facet[$k]['values']) && count($facet[$k]['values']))
     foreach($facet[$k]['values'] as $value => $nb)
      if ($nb)
-       echo "$k:$value:$nb\n";
+       echo "$k;$value;$nb\n";
