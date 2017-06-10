d46d30337eb2083464046be9fefeb43597f54f41
RouxRC <b.ooghe@gmail.com>
2017-06-10 17:59:40 +0200

    ensure js works on maps

--- web/js/doubleHilight.js
+++ web/js/doubleHilight.js
@@ -1,29 +1,33 @@
-/* survol du txt */
-$(".dep_map").on("mouseover", function() {
-  dep = $(this).attr("id").substring(3);
-  $(".map"+dep).mouseover();
-})
-$(".dep_map").on("mouseout", function() {
-  dep = $(this).attr("id").substring(3);
-  $(".map"+dep).mouseout();
-})
-/* survol de la map */
-var d = { running: '0' };
-$("area").on("mouseover", d, function(e) {
-  $(this).addClass("hover");
-  dep = $(this).attr("id").substring(3,9).replace(/-0$/, "");
-  $(".dep"+dep).css("background-color", "#D1EA74");
-  $(".dep"+dep).css("opacity", 0.8);
-  if (e.data.running == 0) {
-    d.running = 1;
-    $(".map"+dep).filter(":not(.hover)").mouseover();
-    d.running = 0;
-  }
-})
-$("area").on("mouseout", function() {
-  $(this).removeClass("hover");
-  dep = $(this).attr("id").substring(3,9).replace(/-0$/, "");
-  $(".dep"+dep).css("background-color", "#fff");
-  $(".dep"+dep).css("opacity", 1);
-  $(".map"+dep).filter(".hover").mouseout();
-})
+$(document).ready(function(){
+  /* survol du txt */
+  $(".dep_map").on("mouseover", function() {
+    dep = $(this).attr("id").substring(3);
+  console.log(dep);
+    $(".map"+dep).mouseover();
+  });
+  $(".dep_map").on("mouseout", function() {
+    dep = $(this).attr("id").substring(3);
+    $(".map"+dep).mouseout();
+  });
+  /* survol de la map */
+  var d = { running: '0' };
+  $("area").on("mouseover", d, function(e) {
+    $(this).addClass("hover");
+    dep = $(this).attr("id").substring(3,9).replace(/-0$/, "");
+  console.log(dep, $(this));
+    $(".dep"+dep).css("background-color", "#D1EA74");
+    $(".dep"+dep).css("opacity", 0.8);
+    if (e.data.running == 0) {
+      d.running = 1;
+      $(".map"+dep).filter(":not(.hover)").mouseover();
+      d.running = 0;
+    }
+  });
+  $("area").on("mouseout", function() {
+    $(this).removeClass("hover");
+    dep = $(this).attr("id").substring(3,9).replace(/-0$/, "");
+    $(".dep"+dep).css("background-color", "#fff");
+    $(".dep"+dep).css("opacity", 1);
+    $(".map"+dep).filter(".hover").mouseout();
+  });
+});
