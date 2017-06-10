2b3bc405f50dfe95c50ce024e1be3574caf9da9a
RouxRC <b.ooghe@gmail.com>
2017-06-10 17:36:55 +0200

    better check IPs

--- web/frontend_dev.php
+++ web/frontend_dev.php
@@ -4,7 +4,7 @@ require_once(dirname(__FILE__).'/../config/ProjectConfiguration.class.php');
 $configuration = ProjectConfiguration::getApplicationConfiguration('frontend', 'dev', true);
 
 // this check prevents access to debug front controllers that are deployed by accident to production servers.
-if (!in_array(@$_SERVER['REMOTE_ADDR'], myTools::getAdminIPs()))
+if (!in_array(@$_SERVER['HTTP_CF_CONNECTING_IP'], myTools::getAdminIPs()) && !in_array(@$_SERVER['REMOTE_ADDR'], myTools::getAdminIPs()))
 {
   die('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
 }
