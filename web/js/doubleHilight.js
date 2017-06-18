f46b924c2ade500be78dc5bdfa67c3c9f99966b1
RouxRC <b.ooghe@gmail.com>
2017-06-18 02:40:20 +0200

    better fixes :s

--- web/js/doubleHilight.js
+++ web/js/doubleHilight.js
@@ -3,19 +3,21 @@ $(document).ready(function(){
   $(".dep_map").on("mouseover", function() {
     dep = $(this).attr("id").substring(3);
     $(".map"+dep).mouseover();
-    $(".departement"+dep).addClass('hover');
+    if ($(".departement"+dep).length)
+      $(".departement"+dep).attr('class', $(".departement"+dep).attr('class')+' hover');
   });
   $(".dep_map").on("mouseout", function() {
     dep = $(this).attr("id").substring(3);
     $(".map"+dep).mouseout();
-    $(".departement"+dep).removeClass('hover');
+    if ($(".departement"+dep).length)
+      $(".departement"+dep).attr('class', $(".departement"+dep).attr('class').replace(/ hover/, ''));
   });
   /* survol de la map ou du svg */
   var d = { running: '0' };
   $("area, .departement").on("mouseover", d, function(e) {
     $(this).addClass("hover");
     dep = $(this).attr("id").replace(/^(d|map)/, "").replace(/-0$/, "");
-    $(".dep"+dep+", #dep"+dep).css("background-color", "#D1EA74");
+    $(".dep"+dep+", #dep"+dep).addClass('hover');
     if (e.data.running == 0) {
       d.running = 1;
       $(".map"+dep).filter(":not(.hover)").mouseover();
@@ -25,7 +27,7 @@ $(document).ready(function(){
   $("area, .departement, .departement path").on("mouseout", function() {
     $(this).removeClass("hover");
     dep = $(this).attr("id").replace(/^(d|map)/, "").replace(/-0$/, "");
-    $(".dep"+dep+", #dep"+dep).css("background-color", "#fff");
+    $(".dep"+dep+", #dep"+dep).removeClass('hover');
     $(".map"+dep).filter(".hover").mouseout();
   });
 });
