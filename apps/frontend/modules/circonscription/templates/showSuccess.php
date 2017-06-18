bd5480f5d64b63d33df6571ad962acf9c932d527
RouxRC <b.ooghe@gmail.com>
2017-06-18 03:00:51 +0200

    resize circos map

--- apps/frontend/modules/circonscription/templates/showSuccess.php
+++ apps/frontend/modules/circonscription/templates/showSuccess.php
@@ -2,7 +2,7 @@
 <h1>Les députés par circonscription</h1>
 <div id="carte_circo">
   <h2><?php echo $circo.' ('.$departement_num.')'; ?></h2>
-  <?php include_partial('map', array('num'=>strtolower($departement_num), 'size' => 400)); ?>
+  <?php include_partial('map', array('num'=>strtolower($departement_num), 'size' => 500)); ?>
   <p><?php echo $total; ?> députés trouvés :</p>
   <div class="list_circo">
     <?php include_partial('parlementaire/table', array('deputes' => $parlementaires, 'circo' => $departement_num)); ?>
