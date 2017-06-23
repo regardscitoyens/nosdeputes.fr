c9fc92f193279b6912daf57939322e4396ea4cc3
RouxRC <b.ooghe@gmail.com>
2017-06-23 20:22:22 +0200

    [WIP] prepare map of circos etranger with khartis (cf #28)

--- apps/frontend/modules/circonscription/templates/showSuccess.php
+++ apps/frontend/modules/circonscription/templates/showSuccess.php
@@ -1,10 +1,15 @@
-<?php $sf_response->setTitle($circo.' ('.$departement_num.') : Les députés par circonscription'); ?>
+<?php
+$sf_response->setTitle($circo.' ('.$departement_num.') : Les députés par circonscription');
+$listOptions = array('deputes' => $parlementaires, 'circo' => $departement_num);
+if ($departement_num == "999")
+  $listOptions["list"] = true;
+?>
 <h1>Les députés par circonscription</h1>
 <div id="carte_circo">
-  <h2><?php echo $circo.' ('.$departement_num.')'; ?></h2>
-  <?php include_partial('map', array('num'=>strtolower($departement_num), 'size' => 500)); ?>
-  <p><?php echo $total; ?> députés trouvés :</p>
-  <div class="list_circo">
-    <?php include_partial('parlementaire/table', array('deputes' => $parlementaires, 'circo' => $departement_num)); ?>
+  <h2><?php echo $circo.' ('.$departement_num.')'; ?><br/><small><?php echo $total; ?> députés</small></h2>
+  <?php if ($departement_num != "999") include_partial('map', array('num'=>strtolower($departement_num), 'size' => 550)); ?>
+  <div class="list_<?php echo ($departement_num == "999" ? 'table' : 'circo'); ?>">
+    <?php include_partial('parlementaire/table', $listOptions); ?>
   </div>
+  <?php if ($departement_num == "999") echo file_get_contents('circos-francais-etranger.svg'); ?>
 </div>
