da65fab60d6918002917a47a4c9486ce3f14c20b
Tangui Morlier <tangui@tangui.eu.org>
2017-06-25 20:49:30 +0200

    Set PATH_APP

--- bin/generate_dbinc.sh
+++ bin/generate_dbinc.sh
@@ -4,4 +4,6 @@ DBNAME=$(grep 'dsn:' config/databases.yml | sed 's/.*dbname=//'  | sed "s/'.*//"
 USER=$(grep 'username:' config/databases.yml | sed 's/.*username: *//')
 PASS=$(grep 'password:' config/databases.yml | sed 's/.*password: *//')
 LEGI=$(grep ' legislature:' config/app.yml | sed 's/.*legislature: *//')
-cat bin/db.inc.example | sed "s/MYSQLID=.*/MYSQLID=\"-u $USER -p$PASS\"/" | sed "s/DBNAME=.*/DBNAME=$DBNAME/" | sed "s/.*EGISLATURE=.*/LEGISLATURE=$LEGI/" > bin/db.inc
+PATHAPP=$(pwd)"/"$(dirname $0)"/../"
+
+cat bin/db.inc.example | sed "s/MYSQLID=.*/MYSQLID=\"-u $USER -p$PASS\"/" | sed "s|PATH_APP=.*|PATH_APP=$PATHAPP|" | sed "s/DBNAME=.*/DBNAME=$DBNAME/" | sed "s/.*EGISLATURE=.*/LEGISLATURE=$LEGI/" > bin/db.inc
