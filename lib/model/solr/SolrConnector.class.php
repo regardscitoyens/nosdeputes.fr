72935efae57881d189e79e00a825628ca21a2632
Tangui Morlier <tangui@tangui.eu.org>
2017-06-26 17:41:41 +0200

    Suppression du paramtre not_utf8 qui n'est plus utilisé (au profit d'un parametre dans le app.yml

--- lib/model/solr/SolrConnector.class.php
+++ lib/model/solr/SolrConnector.class.php
@@ -105,7 +105,7 @@ class SolrConnector extends sfLogger
   public function search($queryString, $params = array(), $offset = 0, $maxHits = 0) {
     if($maxHits == 0)
         $maxHits = sfConfig::get('app_solr_max_hits', 256);
-    if ((isset($params['not_utf8']) && $params['not_utf8']) || !sfConfig::get('app_solr_utf8', true))  {
+    if (!sfConfig::get('app_solr_utf8', true))  {
 	    $queryString = utf8_decode($queryString);
     }
     $response = $this->solr->search($queryString, $offset, $maxHits, $params);
