ac878c86b911a84f3b6d15a6e9c025ff3e512e7f
RouxRC <b.ooghe@gmail.com>
2017-07-04 22:42:23 +0200

    remove CR congrès from commissions and add to hemicycle

--- batch/commission/download_commission.pl
+++ batch/commission/download_commission.pl
@@ -14,7 +14,7 @@ $p = HTML::TokeParser->new(\$content);
 while ($t = $p->get_tag('a')) {
   $txt = $p->get_text('/a');
   $curl = $file = $t->[1]{href};
-  if ($txt =~ /compte rendu|e nationale \~/i && $curl !~ /(\/cri\/2|\(typeDoc\))/ && $curl =~ /nationale\.fr\/$legislature\//) {
+  if ($txt =~ /compte rendu|e nationale \~/i && $curl !~ /(\/cri\/(2|congres)|\(typeDoc\))/ && $curl =~ /nationale\.fr\/$legislature\//) {
     $ok = 1;
     $file =~ s/\//_/gi;
     $file =~ s/\#.*//;
