cfacd34278c9e031cc819055eb7e75463d8b9695
RouxRC <b.ooghe@gmail.com>
2017-07-09 04:15:22 +0200

    create missing dir

--- batch/documents/reload_latest_ppl.sh
+++ batch/documents/reload_latest_ppl.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 . ../../bin/db.inc
+mkdir -p html
 
 for url in `echo "SELECT source FROM texteloi WHERE date > DATE_SUB(CURDATE(), INTERVAL 90 DAY) AND type LIKE 'Proposition%' ORDER BY numero"  | mysql $MYSQLID $DBNAME | grep -v source`; do
   file=`echo $url | sed 's/\//_/g'`
