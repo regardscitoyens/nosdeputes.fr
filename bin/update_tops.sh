20d01f87bf60f7be9e67e6cd7a3261eabd718080
RouxRC <b.ooghe@gmail.com>
2017-07-16 04:12:38 +0200

    no future tops

--- bin/update_tops.sh
+++ bin/update_tops.sh
@@ -6,17 +6,18 @@ start=$(($LEGISLATURE * 5 + 1942))
 
 php symfony top:Deputes
 for month in 06 07 08 09 10 11 12; do
+  if [ $start -eq `date +%Y` ] && [ $month -gt `date +%m` ]; then
+    break
+  fi
   php symfony top:Deputes $start-$month-01
 done
 for year in `seq $(($start + 1)) $(($start + 5))`; do
-  if test $year -gt `date +%Y`; then
+  if [ $year -gt `date +%Y` ]; then
     break
   fi
   for month in 01 02 03 04 05 06 07 08 09 10 11 12; do
-    if test $year -eq `date +%Y`; then
-     if test $month -gt `date +%m`; then
+    if [ $year -eq `date +%Y` ] && [ $month -gt `date +%m` ]; then
       break
-     fi
     fi
     php symfony top:Deputes $year-$month-01
   done
