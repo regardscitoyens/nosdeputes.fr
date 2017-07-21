6cf2d2886cf9e6ac8f42c1f4718c5c2367e22f37
RouxRC <b.ooghe@gmail.com>
2017-07-21 03:31:15 +0200

    remove warning in CR commissions

--- apps/frontend/modules/intervention/templates/seanceSuccess.php
+++ apps/frontend/modules/intervention/templates/seanceSuccess.php
@@ -113,7 +113,7 @@ if ($intervention->section_id && !$sections[$intervention->section_id]->titre) {
       $didascalie = 1;
       $didascalietitre = 0;
       $comparableinter = trim(strtolower(preg_replace('/<[^>]+>/i', '', $intervention->intervention)));
-      if ($comparableinter === trim(strtolower($sections[$intervention->section_id]->titre)) ||
+      if (($intervention->section_id && $comparableinter === trim(strtolower($sections[$intervention->section_id]->titre))) ||
           preg_match('/^(suspension|reprise|rappels? au)/', $comparableinter))
         $didascalietitre = 1;
     } ?>
