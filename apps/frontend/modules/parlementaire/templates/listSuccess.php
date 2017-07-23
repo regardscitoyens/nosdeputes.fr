d313dbf6de410a397ba550a895a99e256a6c930a
Tangui Morlier <tangui@tangui.eu.org>
2017-07-24 00:13:27 +0200

    Proposition d'ajout de lien vers les législatures précédentes depuis le listing des députés

--- apps/frontend/modules/parlementaire/templates/listSuccess.php
+++ apps/frontend/modules/parlementaire/templates/listSuccess.php
@@ -1,9 +1,10 @@
 <h1>Tous les députés par ordre alphabétique</h1>
 <?php $sf_response->setTitle('Liste de tous les députés à l\'Assemblée nationale - NosDéputés.fr'); ?>
-<p>Les <?php echo $total; ?> députés de la <?php echo sfConfig::get('app_legislature', 13); ?><sup>ème</sup> législature (<?php echo $actifs; ?> en cours de mandat)&nbsp;:</p>
+<p>Voici <?php echo $total; ?> députés de la <?php echo sfConfig::get('app_legislature', 13); ?><sup>ème</sup> législature (<?php echo $actifs; ?> en cours de mandat). Les informations relatives aux députés des deux législatures précédentes restent accessibles. Cliquez sur les liens suivants pour accéder aux députés <a href="https://2007-2012.nosdeputes.fr/deputes">élus entre 2007 et 2012</a>, et ceux <a href="https://2012-2017.nosdeputes.fr/deputes">élus entre 2012 et 2017</a>.</p>
 <center><h4><?php foreach (myTools::getCurrentGroupesInfos() as $gpe)
   echo '&nbsp; '.link_to(str_replace(' ', '&nbsp;', $gpe[0].' (<b').' class="c_'.strtolower($gpe[1]).'">'.$gpe[1].'</b>)', '@list_parlementaires_groupe?acro='.$gpe[1]).'&nbsp; '; ?>
 </h4></center>
+
 <div class="liste"><?php
 $listlettres = array_keys($parlementaires);
 foreach($listlettres as $i) {
@@ -20,4 +21,3 @@ foreach($listlettres as $i) {
 
  ?>
 </div>
-
