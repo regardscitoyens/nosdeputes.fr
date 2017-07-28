2267e659ba125b92bc9f180116d4ca6e898bdc02
RouxRC <b.ooghe@gmail.com>
2017-07-28 02:33:32 +0200

    add scripts to check most parsing issues

---
+++ batch/commission/check_parsed_cr.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+JSON=$1
+
+echo "Intervenants:"
+echo "-------------"
+grep -v '"intervenant": ""' $JSON   |
+  sed 's/^.*"intervenant": "//'     |
+  sed 's/",.*"fonction": "/\t\t|  /'|
+  sed 's/".*$//'                    |
+  sort  | uniq -c
+echo "-------------"
+echo
+echo
+
+echo "Didascalies :"
+echo "-------------"
+grep '"intervenant": ""' $JSON      |
+  sed 's/^.*"intervention": "/-> /' |
+  sed 's/".*$//'                    |
+  sort -u
+echo "-------------"
+echo
+echo
+
+echo "Meme interv :"
+interv="START"
+cat $JSON | while read line; do
+  newinterv=$(echo $line            |
+    sed 's/^.*"intervenant": "//'   |
+    sed 's/",.*"fonction": "/, /'   |
+    sed 's/".*$//'
+  )
+  newtext=$(echo $line              |
+    sed 's/^.*"intervention": "//'  |
+    sed 's/".*$//'
+  )
+  if [ "$interv" = "$newinterv" ]; then
+    echo "-------------"
+    echo "$interv: $text"
+    echo "$newinterv: $newtext"
+  fi
+  interv=$newinterv
+  text=$newtext
+done
+echo "-------------"
+echo
+echo
+
+
---
+++ batch/hemicycle/check_parsed_cri.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+JSON=$1
+
+echo "Sommaire :"
+echo "-------------"
+cat $JSON                   |
+  sed 's/^.*"contexte": "//'|
+  sed 's/".*$//'            |
+  grep . | uniq
+echo "-------------"
+echo
+echo
+
+echo "Intervenants:"
+echo "-------------"
+grep -v '"intervenant": ""' $JSON   |
+  sed 's/^.*"intervenant": "//'     |
+  sed 's/",.*"fonction": "/\t\t|  /'|
+  sed 's/".*$//'                    |
+  sort  | uniq -c
+echo "-------------"
+echo
+echo
+
+echo "Didascalies :"
+echo "-------------"
+grep '"intervenant": ""' $JSON      |
+  sed 's/^.*"intervention": "/-> /' |
+  sed 's/".*$//'                    |
+  sort -u
+echo "-------------"
+echo
+echo
+
+echo "Parenthèses :"
+echo "-------------"
+grep '(' $JSON                          |
+  sed 's/^.*"contexte": "//'            |
+  sed 's/",.*"intervention": "/  |  /'  |
+  sed 's/".*$//'                        |
+  sort -u
+echo "-------------"
+echo
+echo
+
+echo "Meme interv :"
+interv="START"
+cat $JSON | while read line; do
+  newinterv=$(echo $line            |
+    sed 's/^.*"intervenant": "//'   |
+    sed 's/",.*"fonction": "/, /'   |
+    sed 's/".*$//'
+  )
+  newtext=$(echo $line              |
+    sed 's/^.*"intervention": "//'  |
+    sed 's/".*$//'
+  )
+  if [ "$interv" = "$newinterv" ]; then
+    echo "-------------"
+    echo "$interv: $text"
+    echo "$newinterv: $newtext"
+  fi
+  interv=$newinterv
+  text=$newtext
+done
+echo "-------------"
+echo
+echo
+
+
