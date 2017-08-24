4bc4f4ef1e0481479366b75244b0be5d5ffa07c9
RouxRC <b.ooghe@gmail.com>
2017-08-25 00:17:21 +0200

    add dynamic js table handling for list orgas

--- apps/frontend/modules/parlementaire/templates/listOrganismesTypeSuccess.php
+++ apps/frontend/modules/parlementaire/templates/listOrganismesTypeSuccess.php
@@ -1,5 +1,10 @@
 <h1><?php echo $title; ?></h1>
 <table class="list_orgas">
+<thead><tr>
+  <th class="orga">Intitulé</th>
+  <th>Membres</th>
+  <?php if ($type == "parlementaire") echo "<th>Réunions publiques</th>"; ?>
+</tr></thead>
 <tbody>
 <?php $permas = myTools::getCommissionsPermanentes();
 if ($type == "groupe") {
@@ -20,9 +25,9 @@ foreach($organismes as $o) :
   $name = ucfirst(in_array($o["slug"], $permas) ? str_replace("commission", "commission <small>(permanente)</small>", $o["nom"]) : $o["nom"]); ?>
 <tr>
   <td class="orga"><a href="<?php echo url_for('@list_parlementaires_organisme?slug='. $o["slug"]); ?>"><?php echo $name; ?></a></td>
-  <td><?php if ($o["membres"]) echo $o["membres"]." membre".($o["membres"] > 1 ? "s" : ""); ?></td>
+  <td><?php if ($o["membres"]) echo $o["membres"]; ?></td>
   <?php if ($type == "parlementaire") { ?>
-  <td><?php if ($o["reunions"]) echo $o["reunions"]." réunion".($o["reunions"] > 1 ? "s" : ""); ?></td>
+  <td><?php if ($o["reunions"]) echo $o["reunions"]; ?></td>
   <?php } ?>
 </tr>
 <?php endforeach; ?>
@@ -34,3 +39,22 @@ foreach($organismes as $o) :
   echo '</div>';
 } ?>
 <p><a href="<?php echo url_for('@list_organismes'); ?>">Retour à la liste des types d'organismes</a></p>
+<script type="text/javascript">
+$('.list_orgas').bind('dynatable:init', function(e, dynatable) {
+  dynatable.sorts.clear();
+});
+$(".list_orgas").dynatable({
+  features: {
+    paginate: false,
+    pushState: false,
+    search: false,
+    recordCount: false
+  },
+  readers: {
+    _attributeReader: function(cell, record) {
+      var $cell = $(cell).html();
+      return Number($cell) || $cell;
+    }
+  }
+});
+</script>
