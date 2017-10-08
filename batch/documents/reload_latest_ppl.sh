da94a374e8b37e49424a60d7e8d3ed5015780e3c
RouxRC <b.ooghe@gmail.com>
2017-10-08 14:03:03 +0200

    do not reload unmodified ppl

--- batch/documents/reload_latest_ppl.sh
+++ batch/documents/reload_latest_ppl.sh
@@ -6,8 +6,11 @@ mkdir -p html
 for url in `echo "SELECT source FROM texteloi WHERE date > DATE_SUB(CURDATE(), INTERVAL 90 DAY) AND type LIKE 'Proposition%' ORDER BY numero"  | mysql $MYSQLID $DBNAME | grep -v source`; do
   file=`echo $url | sed 's/\//_/g'`
   perl download_one.pl $url
-  perl parse_metas.pl html/$file > out/$file
-  mv html/$file ppl/$file
+  if ! test -e ppl/$file || diff html/$file ppl/$file | grep . > /dev/null; then
+    perl parse_metas.pl html/$file > out/$file
+    mv html/$file ppl/$file
+  else
+    rm -f html/$file
+  fi
 done;
-
 
