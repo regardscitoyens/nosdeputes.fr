4762e947c7a0922136a59f6b7cc25953cbbfcf04
RouxRC <b.ooghe@gmail.com>
2017-10-08 21:19:34 +0200

    remvove javascript links

--- batch/commission/parse_commission.pl
+++ batch/commission/parse_commission.pl
@@ -389,6 +389,7 @@ $string =~ s/<t([rdh])( (row|col)span=["\d]+)*[^>]*>/<t\1\2>/gi;
 $string =~ s/\n+\s*(<\/?t(able|[rdh]))/\1/gi;
 $string =~ s/(<\/table>)\s*(<table)/\1\n\2/gi;
 $string =~ s/(<img[^>]*)[\n\r]+([^>]*>)/\1 \2/gi;
+$string =~ s/<a[^>]*href="javascript:[^"]*"[^>]*>([^<]*)<\/a>/\1/gi;
 $string =~ s/-->(-->)+/-->/g;
 #$string =~ s/<!-- \.(.*?)\. -->//g;
 
