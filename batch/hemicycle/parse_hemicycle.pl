31f7f933a87a6ede3fc61cac82e42460cdaa74ea
RouxRC <b.ooghe@gmail.com>
2017-10-19 21:57:14 +0200

    fix wrongly detected interv

--- batch/hemicycle/parse_hemicycle.pl
+++ batch/hemicycle/parse_hemicycle.pl
@@ -526,7 +526,7 @@ foreach $line (split /\n/, $string)
                     $intervenant_url = $oldintervenant_url;
                     next;
                 }
-            }elsif ($line =~ s/^\s*\|\s*(M[^\|\/\:]+)[\|\/\:]// || $line =~ s/^\s*(M[\.Mmle]+(\s+([dl][eaus'\s]+)*[^\.:\s]{2,}){1,4})[\.\:]//) {
+            }elsif ($line =~ s/^\s*\|\s*(M[^\|\/\:]+)[\|\/\:]// || ($line !~ /^[^.]+ d..?nonce/ && $line =~ s/^\s*(M[\.Mmle]+(\s+([dl][eaus'\s]+)*[^\.:\s]{2,}){1,4})[\.\:]//)) {
                 if ($line) {
                     checkout();
                     $majIntervenant = 1;
