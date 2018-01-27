0ca864f2dff5bf9bb9531889bb86bc3b50a94ec6
RouxRC <b.ooghe@gmail.com>
2018-01-27 22:38:58 +0100

    include old mandats in all mps output

--- apps/frontend/modules/api/actions/actions.class.php
+++ apps/frontend/modules/api/actions/actions.class.php
@@ -196,8 +196,8 @@ class apiActions extends sfActions
     if ($request->getParameter('current') == true) {
       $query->where('fin_mandat IS NULL OR debut_mandat > fin_mandat');
       $this->multi['responsabilite'] = 1;
-      $this->multi['mandat'] = 1;
     }
+    $this->multi['mandat'] = 1;
     $this->multi['site'] = 1;
     $this->multi['email'] = 1;
     $this->multi['adresse'] = 1;
@@ -311,10 +311,10 @@ class apiActions extends sfActions
     if ($light != 2) {
       $res['adresses'] = myTools::array2hash(unserialize($parl->adresses), 'adresse');
       $res['collaborateurs'] = myTools::array2hash(unserialize($parl->collaborateurs), 'collaborateur');
-      $res['anciens_mandats'] = myTools::array2hash(unserialize($parl->anciens_mandats), 'mandat');
       $res['autres_mandats'] = myTools::array2hash(unserialize($parl->autres_mandats), 'mandat');
       $res['anciens_autres_mandats'] = myTools::array2hash(unserialize($parl->anciens_autres_mandats), 'mandat');
     }
+    $res['anciens_mandats'] = myTools::array2hash(unserialize($parl->anciens_mandats), 'mandat');
     $res['profession'] = $parl->profession;
     $res['place_en_hemicycle'] = $parl->place_hemicycle;
     $res['url_an'] = $parl->url_an;
