177a37c6ed391341a5121b6ef582823e2b5ec041
Benjamin Ooghe-Tabanou <benjamin.ooghe@sciences-po.fr>
2018-02-01 19:30:00 +0100

    add amdmts subjects cleaner to parser from LFDLL

--- batch/amendements/compute_latest.sh
+++ batch/amendements/compute_latest.sh
@@ -11,7 +11,7 @@ perl download_amendements.pl $LEGISLATURE > /tmp/download_amendements.log
 
 for file in `ls html`; do
   fileout=$(echo $file | sed 's/html/json/' | sed 's/\.asp/\.xml/')
-  perl cut_amdmt.pl html/$file > json/$fileout
+  perl cut_amdmt.pl html/$file | python clean_subjects_amdmts.py > json/$fileout
   if test -e loaded/$fileout && ! diff {json,loaded}/$fileout | grep . > /dev/null; then
     rm -f json/$fileout
   fi
