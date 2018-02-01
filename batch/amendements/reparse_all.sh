0206efbccafa39a8251a9aef538b34d73e989fa7
RouxRC <b.ooghe@gmail.com>
2018-02-01 19:43:45 +0100

    use clean subjects in reparse all

--- batch/amendements/reparse_all.sh
+++ batch/amendements/reparse_all.sh
@@ -4,7 +4,10 @@ rm -rf json
 mkdir -p json
 
 yml=$1;
+ct=0
 for url in `ls html`; do
-	perl parse_amdmt.pl html/$url $yml > json/$url
+    echo $ct $url
+	perl parse_amdmt.pl html/$url $yml  | python clean_subjects_amdmts.py > json/$url
+    ct=$((ct + 1))
 done
 
