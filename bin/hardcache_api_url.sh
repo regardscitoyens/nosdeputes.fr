9d9a27af5847e2e0c932ff4d075fecea4fefc7b9
RouxRC <b.ooghe@gmail.com>
2018-02-21 11:27:37 +0100

    add script to hardcache api

---
+++ bin/hardcache_api_url.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+URL=$(echo $1 | sed -r 's#/(xml|json|csv)$##')
+DIR=$(echo $URL | sed 's#^.*nosdeputes.fr#web#')
+TMPFILE=/tmp/hard_cache_api.$$
+
+mkdir -p $DIR
+
+for f in xml json csv; do
+  rm $DIR/$f.html 2> /dev/null
+  curl -qL "$URL/$f?_sf_ignore_cache=$$" > $TMPFILE
+  mv $TMPFILE $DIR/$f.html
+done
+
+echo $URL $DIR
