2e35744bba51d3262081c1c844f01cd331ab48ad
RouxRC <b.ooghe@gmail.com>
2018-04-21 00:44:34 +0200

    fix non fonction specific case

--- batch/commission/parse_commission.pl
+++ batch/commission/parse_commission.pl
@@ -656,7 +656,7 @@ foreach $line (split /\n/, $string)
         checkout();
         $interv1 = $1;
 	    $extrainterv = $2.$3;
-        if ($extrainterv =~ s/(\/A \w+i\W*\/)//) {
+        if ($extrainterv =~ s/(Cooking Budgets|\/A \w+i\W*\/)//) {
             $line = $1.$line;
         }
         $found = $majIntervenant = 1;
