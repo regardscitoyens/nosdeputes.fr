076c4a6b4614e262dc7e862b1e4518ed4703ce5e
RouxRC <b.ooghe@gmail.com>
2018-05-22 06:01:17 +0200

    fix typo AN

--- batch/commission/parse_commission.pl
+++ batch/commission/parse_commission.pl
@@ -306,6 +306,7 @@ sub setIntervenant {
     $intervenant =~ s/Yvan Koustoff/Yvan Kouskoff/i;
     $intervenant =~ s/Joachim Son-Forge\b/Joachim Son-Forget/i;
     $intervenant =~ s/Mikaël Nogal/Mickaël Nogal/i;
+    $intervenant =~ s/Célia Delavergne/Célia de Lavergne/i;
     $intervenant =~ s/Sébastien Huygue/Sébastien Huyghe/i;
     $intervenant =~ s/Julien Hubert-Laferrière/Hubert Julien-Laferrière/i;
     $intervenant =~ s/François Gaill/Françoise Gaill/i;
