edc24d5c2c57ad10b5bb0e374764b2970d0bfde9
RouxRC <b.ooghe@gmail.com>
2018-06-17 23:02:07 +0200

    amendements date in DB are actually publication date not dépôt's

--- apps/frontend/modules/amendement/templates/showSuccess.php
+++ apps/frontend/modules/amendement/templates/showSuccess.php
@@ -30,7 +30,7 @@
     if ($sous['sort'] === 'Adopté') echo '(Adopté)</strong> ';
   } ?></p>
 <?php } ?>
-<p>Déposé le <?php echo myTools::displayDate($amendement->date); ?> par : <span id="liste_deputes"><?php echo preg_replace('/(M[.mle]+)\s+/', '\\1&nbsp;', $amendement->getSignataires(1)); ?>.</span></p>
+<p>Publié le <?php echo myTools::displayDate($amendement->date); ?> par : <span id="liste_deputes"><?php echo preg_replace('/(M[.mle]+)\s+/', '\\1&nbsp;', $amendement->getSignataires(1)); ?>.</span></p>
 <div class="signataires">
   <div class="photos"><p>
 <?php $deputes = $amendement->Parlementaires;
