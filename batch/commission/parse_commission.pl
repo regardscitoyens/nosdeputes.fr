91ec91f93fd6750d203ee457a4015faf4526bb44
RouxRC <b.ooghe@gmail.com>
2018-09-23 20:26:45 +0200

    fix typos AN

--- batch/commission/parse_commission.pl
+++ batch/commission/parse_commission.pl
@@ -315,6 +315,9 @@ sub setIntervenant {
     $intervenant =~ s/Aurélien Taquet/Adrien Taquet/i;
     $intervenant =~ s/Pierre Dharéville/Pierre Dharréville/i;
     $intervenant =~ s/Jean-Jean-/Jean-/i;
+    $intervenant =~ s/Stanslas Guerini/Stanislas Guerini/i;
+    $intervenant =~ s/Natalie Pouzyreff/Natalia Pouzyreff/i;
+    $intervenant =~ s/Loïc Prudhomme/Loïc Prud'homme/i;
     $intervenant =~ s/Arnaud Villa/Arnaud Viala/i;
     $intervenant =~ s/Yannick Marignac/Yves Marignac/i;
     $intervenant =~ s/Nicolas Trisse/Nicole Trisse/i;
