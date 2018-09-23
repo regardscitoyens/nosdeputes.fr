fe7d5b40752d1722b87ec2d66785dfe677a5b93a
RouxRC <b.ooghe@gmail.com>
2018-09-24 01:12:22 +0200

    adjust accounting presences for special cases of mise_au_point
    cf https://github.com/regardscitoyens/nosdeputes.fr/pull/115#issuecomment-418172211

--- lib/model/doctrine/ParlementaireScrutin.class.php
+++ lib/model/doctrine/ParlementaireScrutin.class.php
@@ -18,7 +18,13 @@ class ParlementaireScrutin extends BaseParlementaireScrutin
       $this->parlementaire_groupe_acronyme,
       'scrutin',
       $this->Scrutin->getLinkSource(),
-      $this->position && $this->position != "nonVotant" && !$this->par_delegation
+    // on compte une présence quand toutes les conditions suivantes sont vérifiées :
+    // cf https://github.com/regardscitoyens/nosdeputes.fr/pull/115#issuecomment-418172211
+    // - on a effectivement une position et pas seulement une mise au point
+    // - la position n'est pas nonVotant (ce qui correspond aux présidents de Séance + systématiquement au président de l'Assemblée même en son absence cf https://github.com/regardscitoyens/nosdeputes.fr/pull/115#issuecomment-413740999 )
+    // - l'éventuelle mise au point n'indique pas non plus nonVotant
+    // - le vote n'était pas par délégation
+      $this->position && $this->position !== "nonVotant" && $this->mise_au_point_position !== "nonVotant" && !$this->par_delegation
     );
   }
 
