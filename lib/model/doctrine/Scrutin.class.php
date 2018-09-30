5efb9c50ecf04552c4fdf6fea1e3387e86a471c6
RouxRC <b.ooghe@gmail.com>
2018-09-30 21:48:06 +0200

    update date debut délégations according to AN's answer
    cf #115

--- lib/model/doctrine/Scrutin.class.php
+++ lib/model/doctrine/Scrutin.class.php
@@ -4,7 +4,7 @@ class Scrutin extends BaseScrutin
 {
   // Date début délégations (cf https://github.com/regardscitoyens/nosdeputes.fr/pull/115#issuecomment-421844588 )
   // On ne génère pas de preuve de présence à partir des votes avant cette date sauf si le scrutin a des délégations (3 cas particuliers de solennel)
-  const DEBUT_DELEGATIONS = '2018-02-13';
+  const DEBUT_DELEGATIONS = '2018-03-20';
   // Anticipe potentiel recul de la transparence en matière de publicité des délégations
   const FIN_DELEGATIONS = '9999-99-99';
 
