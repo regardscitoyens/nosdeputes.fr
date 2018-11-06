ed519ac05db603e94cf61a7caac51ced40fff291
RouxRC <b.ooghe@gmail.com>
2018-11-06 13:16:30 +0100

    remove old encoding piece

--- batch/questions/download_one.pl
+++ batch/questions/download_one.pl
@@ -14,9 +14,9 @@ $file = uri_escape($a->uri);
 print " saving $url ... " if ($verbose);
 open FILE, ">:utf8", "html/$file";
 $content = $a->content;
-if ($content =~ s/iso-8859-1/utf-8/gi) {
-  $content = decode("windows-1252", $content);
-}
+#if ($content =~ s/iso-8859-1/utf-8/gi) {
+ # $content = decode("windows-1252", $content);
+#}
 print FILE $content;
 close FILE;
 print "$file downloaded.\n" if ($verbose);
