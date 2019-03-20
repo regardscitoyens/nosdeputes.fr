109cbc316ce7e215cda96a56b42a454809e4e6be
RouxRC <b.ooghe@gmail.com>
2019-03-20 10:37:51 +0100

    woupsie

--- batch/scrutin/parse_scrutins.py
+++ batch/scrutin/parse_scrutins.py
@@ -146,9 +146,8 @@ ERREURS_AN = {
 
 MISSING_HISTOGPES = {
     "PA721632": "UAI",
-    "PA719665": "LREM",
-    "PA722090": "LREM",
-    "PA719664": "LREM"
+    "PA719664": "LREM",
+    "PA722090": "LREM"
 }
 
 def parse_scrutin(data, seances, groupes, histo_groupes):
