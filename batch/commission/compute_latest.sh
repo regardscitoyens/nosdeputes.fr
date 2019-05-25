27897ff6ebbe782c00ba6b67924c118c680e687e
RouxRC <b.ooghe@gmail.com>
2019-05-25 12:56:42 +0200

    silent bad links found on AN

--- batch/commission/compute_latest.sh
+++ batch/commission/compute_latest.sh
@@ -6,7 +6,11 @@ mkdir -p html out presents loaded
 
 for file in $(perl download_commission.pl $LEGISLATURE); do
     if grep "compte rendu .* sera .*é ultérieurement\|Document en attente de mise en ligne.\|La page à laquelle vous souhaitez accéder n'existe pas.\|HTTP Error 503" "html/$file" > /dev/null; then
-        echo "...removing empty file $file" | grep -v "http:__www.assemblee-nationale.fr_15_europe_c-rendus_c0"
+        if echo $file | grep -P "_\d+_cr[^_]*_(\d+)-(\d+)_c\d+" > /dev/null; then
+            echo "...removing empty file $file" | grep -v "http:__www.assemblee-nationale.fr_15_europe_c-rendus_c0" | grep -P "_\d+_cr[^_]*_(\d+)-(\d+)_c\1\2"
+        else
+            echo "...removing empty file $file" | grep -v "http:__www.assemblee-nationale.fr_15_europe_c-rendus_c0"
+        fi
         rm "html/$file"
         continue
     elif grep "Ce compte rendu sera disponible dès que possible\." "html/$file" > /dev/null; then
