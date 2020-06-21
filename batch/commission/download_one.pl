d438d6eafabe9b5e028c20dea6229f273fdd56a4
RouxRC <b.ooghe@gmail.com>
2020-06-21 22:17:24 +0200

    fix download presences special orgs post download comms change

--- batch/commission/download_one.pl
+++ batch/commission/download_one.pl
@@ -4,13 +4,14 @@ use WWW::Mechanize;
 
 $url = shift;
 $url =~ s/^\s+//gi;
+$oldstyle = shift;
 
 $a = WWW::Mechanize->new();
 $a->get($url);
 $content = $a->content;
 
-if ($content !~ /href="(\/dyn\/opendata\/[^"]+\.html)"/) {
-  print STDERR "WARNING: opendata raw html url not found for $curl\n";
+if (!$oldstyle && $content !~ /href="(\/dyn\/opendata\/[^"]+\.html)"/) {
+  print STDERR "WARNING: opendata raw html url not found for $url\n";
   exit();
 }
 $raw_url = "http://www.assemblee-nationale.fr$1";
