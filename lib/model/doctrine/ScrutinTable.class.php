bee7484f7c2e1e4c96964d900ab3b87a6055dd6c
Tangui Morlier <tangui@tangui.eu.org>
2020-07-20 23:46:11 +0200

    Plus de logique à mettre les données statiques dans la Table

--- lib/model/doctrine/ScrutinTable.class.php
+++ lib/model/doctrine/ScrutinTable.class.php
@@ -8,4 +8,18 @@ class ScrutinTable extends Doctrine_Table
     {
         return Doctrine_Core::getTable('Scrutin');
     }
+
+    // Date début délégations (cf https://github.com/regardscitoyens/nosdeputes.fr/pull/115#issuecomment-421844588 )
+    // On ne génère pas de preuve de présence à partir des votes avant cette date sauf si le scrutin a des délégations (3 cas particuliers de solennel)
+    const DEBUT_DELEGATIONS = '2018-03-20';
+    // Anticipe potentiel recul de la transparence en matière de publicité des délégations
+    const FIN_DELEGATIONS = '9999-99-99';
+    const DELEGATIONS_INDEX_DEBUT = 0;
+    const DELEGATIONS_INDEX_FIN = 1;
+    public function getDelegationsRanges() {
+        return array(
+            array(self::DEBUT_DELEGATIONS, self::FIN_DELEGATIONS),
+        );
+    }
+
 }
