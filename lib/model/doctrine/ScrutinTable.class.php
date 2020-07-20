238a3050919eacb45463a3f0b60bf3b3c318a136
Tangui Morlier <tangui@tangui.eu.org>
2020-07-21 00:05:38 +0200

    Ajout de la période permettant d'exclure les votes durant le covid faute de transparence des délégations

--- lib/model/doctrine/ScrutinTable.class.php
+++ lib/model/doctrine/ScrutinTable.class.php
@@ -11,14 +11,13 @@ class ScrutinTable extends Doctrine_Table
 
     // Date début délégations (cf https://github.com/regardscitoyens/nosdeputes.fr/pull/115#issuecomment-421844588 )
     // On ne génère pas de preuve de présence à partir des votes avant cette date sauf si le scrutin a des délégations (3 cas particuliers de solennel)
-    const DEBUT_DELEGATIONS = '2018-03-20';
-    // Anticipe potentiel recul de la transparence en matière de publicité des délégations
-    const FIN_DELEGATIONS = '9999-99-99';
+    // Avec le covid, les votes par délégations ont été systématisés et leur publication a été arrêtée
     const DELEGATIONS_INDEX_DEBUT = 0;
     const DELEGATIONS_INDEX_FIN = 1;
     public function getDelegationsRanges() {
         return array(
-            array(self::DEBUT_DELEGATIONS, self::FIN_DELEGATIONS),
+            array('2018-03-20', '2020-03-10'), //début de la publication des délégations de vote
+            array('2020-06-29', '9999-99-99')  //suspension pendant le covid de la publication des délégations
         );
     }
 
