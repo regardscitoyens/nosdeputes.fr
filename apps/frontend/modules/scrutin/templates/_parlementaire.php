04383fe0a7feef5b6559233577ebe0b47e3f2487
Damien <damien@dam.io>
2020-11-04 10:18:45 +0100

    indent -> space

--- apps/frontend/modules/scrutin/templates/_parlementaire.php
+++ apps/frontend/modules/scrutin/templates/_parlementaire.php
@@ -3,13 +3,13 @@
   foreach ($votes as $v) {
 ?>
 <li>
-	<a href="<?= $v->getScrutin()->getURL() ?>">
-		<?= myTools::displayVeryShortDate($v->getScrutin()->date) ?> : <?= $v->getScrutin()->titre ?>
-		(<strong class="vote-<?= $v->position ?>"><?= $v->position ?></strong>)
-		<?php if ($v->mise_au_point_position) { ?>
-			(mise au point: <strong class="vote-<?= $v->mise_au_point_position ?>"><?= $v->getHumanPositionMiseAuPoint() ?></strong>)
-		<?php } ?>
-	</a>
+    <a href="<?= $v->getScrutin()->getURL() ?>">
+        <?= myTools::displayVeryShortDate($v->getScrutin()->date) ?> : <?= $v->getScrutin()->titre ?>
+        (<strong class="vote-<?= $v->position ?>"><?= $v->position ?></strong>)
+        <?php if ($v->mise_au_point_position) { ?>
+            (mise au point: <strong class="vote-<?= $v->mise_au_point_position ?>"><?= $v->getHumanPositionMiseAuPoint() ?></strong>)
+        <?php } ?>
+    </a>
 </li>
 <?php
   }
