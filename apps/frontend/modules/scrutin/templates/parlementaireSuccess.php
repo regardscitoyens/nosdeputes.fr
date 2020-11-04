a597515f509122fbc9f49bd60ed9de32de04a60a
Damien <damien@dam.io>
2020-11-04 07:15:33 +0100

    remove duplicate code

--- apps/frontend/modules/scrutin/templates/parlementaireSuccess.php
+++ apps/frontend/modules/scrutin/templates/parlementaireSuccess.php
@@ -1,15 +1,7 @@
 <?php
-echo include_component('parlementaire', 'header', array('parlementaire' => $parlementaire, 'titre' => "Votes") )
+echo include_component('parlementaire', 'header', array('parlementaire' => $parlementaire, 'titre' => "Votes") );
 ?>
 
-<?php 
-  foreach ($scrutins as $s) {
-?>
-<li>
-	<a href="<?= $s->getScrutin()->getURL() ?>">
-		<?= myTools::displayVeryShortDate($s->getScrutin()->date) ?> : <?= $s->getScrutin()->titre ?> (<?= $s->position ?>)
-	</a>
-</li>
 <?php
-  }
+echo include_component('scrutin', 'parlementaire', array('parlementaire' => $parlementaire));
 ?>
