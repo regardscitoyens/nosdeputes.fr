ee50ff4f71287ee5a9f15374f9fd0cc80a89a4ed
Damien <damien@dam.io>
2020-11-04 07:27:58 +0100

    little fix

--- apps/frontend/modules/scrutin/templates/_parlementaire.php
+++ apps/frontend/modules/scrutin/templates/_parlementaire.php
@@ -4,7 +4,7 @@
 <li>
 	<a href="<?= $s->getScrutin()->getURL() ?>">
 		<?= myTools::displayVeryShortDate($s->getScrutin()->date) ?> : <?= $s->getScrutin()->titre ?>
-		<strong class="vote-<?= $s->position ?>">(<?= $s->position ?>)</strong>
+		(<strong class="vote-<?= $s->position ?>"><?= $s->position ?></strong>)
 	</a>
 </li>
 <?php
