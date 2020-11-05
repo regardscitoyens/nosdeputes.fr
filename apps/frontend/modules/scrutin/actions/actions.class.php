f3c54f634e69178496cbc6347cc513617af86ffe
Damien <damien@dam.io>
2020-11-05 10:03:32 +0100

    put position before title

--- apps/frontend/modules/scrutin/actions/actions.class.php
+++ apps/frontend/modules/scrutin/actions/actions.class.php
@@ -10,6 +10,6 @@ class scrutinActions extends sfActions
   public function executeParlementaire(sfWebRequest $request) {
     $this->parlementaire = Doctrine::getTable('Parlementaire')->findOneBySlug($request->getParameter('slug'));
     $this->forward404Unless($this->parlementaire);
-    $this->response->setTitle("Votes de ".$this->parlementaire->nom);
+    myTools::setPageTitle("Votes de ".$this->parlementaire->nom, $this->response);
   }
 }
--- apps/frontend/modules/scrutin/templates/_parlementaire.php
+++ apps/frontend/modules/scrutin/templates/_parlementaire.php
@@ -4,11 +4,20 @@
 ?>
 <li>
     <a href="<?= $v->getScrutin()->getURL() ?>">
-        <?= myTools::displayVeryShortDate($v->getScrutin()->date) ?> : <?= $v->getScrutin()->titre ?>
-        (<strong class="vote-<?= $v->position ?>"><?= $v->position ?></strong>)
+        <?= myTools::displayVeryShortDate($v->getScrutin()->date) ?> :
+
+        <strong class="vote-<?= $v->position ?>"><?= $v->position ?></strong>
+
         <?php if ($v->mise_au_point_position) { ?>
             (mise au point: <strong class="vote-<?= $v->mise_au_point_position ?>"><?= $v->getHumanPositionMiseAuPoint() ?></strong>)
         <?php } ?>
+
+
+        <?php if ($v->position == 'abstention' || $v->position == 'nonVotant') { ?>
+            sur
+        <?php } ?>
+
+        <?= $v->getScrutin()->titre ?>
     </a>
 </li>
 <?php
