f483ac29b3f0797cbd49670d4c85651388fad1f5
Damien <damien@dam.io>
2020-11-06 11:53:47 +0100

    remove duplicate code

--- apps/frontend/modules/scrutin/templates/parlementaireSuccess.php
+++ apps/frontend/modules/scrutin/templates/parlementaireSuccess.php
@@ -5,12 +5,26 @@ echo include_component('parlementaire', 'header', array('parlementaire' => $parl
 
 <table>
     <tr>
-        <td>
-            <h2>Votes sur l'ensemble</h2>
+        <?php
+        $columns = array(
+            'votes-ensemble',
+            'votes-autres',
+        );
+        foreach ($columns as $c) {
+        ?>
+
+        <td classs="<?= $c ?>">
+
+            <?php if ($c == 'votes-ensemble') { ?>
+                <h2>Votes sur l'ensemble</h2>
+            <?php } else { ?>
+                <h2>Autres votes</h2>
+            <?php } ?>
+
             <ul>
             <?php 
               foreach ($scrutins as $s) {
-                if ($s->isOnWholeText()) {
+                if ($c == 'votes-autres' || $s->isOnWholeText()) {
             ?>
             <li>
                 <a href="<?= $s->getURL() ?>">
@@ -50,51 +64,7 @@ echo include_component('parlementaire', 'header', array('parlementaire' => $parl
             ?>
             </ul>
         </td>
-        <td style="border-left: 1px solid black;">
-            <h2>Autres votes</h2>
-            <ul>
-            <?php 
-              foreach ($scrutins as $s) {
-                if (!$s->isOnWholeText()) {
-            ?>
-            <li>
-                <a href="<?= $s->getURL() ?>">
-                    <?= myTools::displayVeryShortDate($s->date) ?> :
-
-                    <?php
-                    $vote = null;
-                    foreach ($votes as $v) {
-                        if ($v->scrutin_id == $s->id) {
-                            $vote = $v; 
-                            break;
-                        }
-                    }
-                    ?>
-
-                    <?php if ($vote) { ?>
-                        <strong class="vote-<?= $vote->position ?>"><?= $vote->getHumanPosition() ?></strong>
-
-                        <?php if ($vote->mise_au_point_position) { ?>
-                            (mise au point: <strong class="vote-<?= $vote->mise_au_point_position ?>"><?= $vote->getHumanPositionMiseAuPoint() ?></strong>)
-                        <?php } ?>
-
-                        <?php if ($vote->position == 'abstention' || $vote->position == 'nonVotant') { ?>
-                            sur
-                        <?php } ?>
-
-                    <?php }  else { ?>
-                        <strong class="vote-absent">n'as pas voté</strong> sur
-                    <?php } ?>
-
-                    <?= $s->titre ?>
-                </a>
-            </li>
-            <?php
-                }
-              }
-            ?>
-            </ul>
-        </td>
+    <?php } ?>
     </tr>
 </table>
 
@@ -104,3 +74,7 @@ td {
     padding: 10px;
     vertical-align: top;
 }
+td.votes-autres {
+    border-left: 1px solid black;
+}
+</style>
