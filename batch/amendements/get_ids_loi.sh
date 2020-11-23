469eb2b076b529cadac6486c86014a74f935ed09
Damien <damien@dam.io>
2020-11-23 06:32:52 +0100

    draft: adapt get_ids_loi to the new amdts search engine (issue #158)
    the output is not the same as before, so it still needs so adjustments but it's a start

--- batch/amendements/get_ids_loi.sh
+++ batch/amendements/get_ids_loi.sh
@@ -4,20 +4,21 @@
 
 loi=$1
 
-id_dossier=$(curl -sL "http://www2.assemblee-nationale.fr/recherche/amendements" |
-  grep "<option.*Texte.* $loi[,)]" |
-  sed -r 's/^.*value="([0-9]+)".*$/\1/')
+id_dossier=$(curl -sL "http://www.assemblee-nationale.fr/dyn/15/amendements" |
+  grep "<option.*DLR.L..N.*$loi" |
+  sed -r 's/^.*value="(\w*?)".*$/\1/')
 
-if ! echo "$id_dossier" | grep -P "^\d+$" > /dev/null; then
+if ! echo "$id_dossier" > /dev/null; then
   echo "Cannot find a dossier id for loi $loi in http://www2.assemblee-nationale.fr/recherche/amendements"
   exit 1
 fi
 
-curl -sL "http://www2.assemblee-nationale.fr/recherche/query_amendements?leg=$LEGISLATURE&typeDocument=amendement&idDossierLegislatif=$id_dossier&typeRes=facettes" |
-  grep "examen" |
-  sed 's/{/\n/g' |
-  grep "[lL]ect.* - $loi -" |
-  sed -r 's/^.*val":"([0-9]+)".*$/\1/' |
+>&2 echo "id dossier: $id_dossier"
+
+curl -sL "http://www.assemblee-nationale.fr/dyn/api/amendements/facets?dossier_legislatif=$id_dossier" | jq . |
+  grep "EXAN" |
+  grep "[lL]ect.* - $loi " |
+  sed -r 's/^.*?"(.*?)":.*$/\1/' |
   while read id_examen; do
     echo "$id_dossier;$id_examen"
   done 
