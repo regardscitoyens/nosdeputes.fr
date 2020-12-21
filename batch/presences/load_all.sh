6a31c9092999e8404563d3e5eaa6a81d78b27764
RouxRC <b.ooghe@gmail.com>
2020-12-21 01:33:27 +0100

    disable disappeared pages until fix #99

--- batch/presences/load_all.sh
+++ batch/presences/load_all.sh
@@ -2,9 +2,9 @@
 
 source ../../bin/db.inc
 mkdir -p json
-perl parse_orga_inter.pl http://www.assemblee-nationale.fr/international/francophonie-seminaires-APF.asp "Section française de l'assemblée parlementaire de la francophonie" > json/apf.json
-perl parse_orga_inter.pl http://www.assemblee-nationale.fr/$LEGISLATURE/international/groupes-amitie-activites.asp "Groupes d'amitié" "groupes" > json/grpeamitie.json
-split -l 500 json/grpeamitie.json json/grpeamitie_
-rm json/grpeamitie.json
-perl parse_orga_inter.pl http://www.assemblee-nationale.fr/$LEGISLATURE/international/uip-activites.asp "Groupe français de l'union interparlementaire" > json/uip.json
-perl parse_orga_inter.pl http://www.assemblee-nationale.fr/international/conseil-europe/activites.asp "Délégation française au Conseil de l'Europe" > json/conseil-europe.json
+perl parse_orga_inter.pl "http://www2.assemblee-nationale.fr/layout/set/ajax/content/view/ajax/42953/(titre)/true/(ajax)/true" "Section française de l'assemblée parlementaire de la francophonie" > json/apf.json
+#perl parse_orga_inter.pl http://www.assemblee-nationale.fr/$LEGISLATURE/international/groupes-amitie-activites.asp "Groupes d'amitié" "groupes" > json/grpeamitie.json
+#split -l 500 json/grpeamitie.json json/grpeamitie_
+#rm json/grpeamitie.json
+#perl parse_orga_inter.pl http://www.assemblee-nationale.fr/$LEGISLATURE/international/uip-activites.asp "Groupe français de l'union interparlementaire" > json/uip.json
+#perl parse_orga_inter.pl http://www.assemblee-nationale.fr/international/conseil-europe/activites.asp "Délégation française au Conseil de l'Europe" > json/conseil-europe.json
