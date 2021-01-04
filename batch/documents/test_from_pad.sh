1a4b743cc78dde7fb3a5c011c274c66d2073a236
Tangui Morlier <tangui@tangui.eu.org>
2021-01-04 10:17:17 +0100

    nom du fichier json en base64 : pas top mais le plus rapide à faire (pour décoder : echo "FICHIER" | base64 -d)

--- batch/documents/test_from_pad.sh
+++ batch/documents/test_from_pad.sh
@@ -3,5 +3,5 @@
 bash download_from_opendata.sh
 
 curl -s https://pad.regardscitoyens.org/p/documents-manquants/export/txt | sed 's/_/\//g' | grep assemblee-nationale | sed 's/.*http/http/' | sed 's/asp.*/asp/' | while read url ; do
-    bash parse_url_from_opendata.sh $url test.json
+    bash parse_url_from_opendata.sh $url outtest/$(echo $url | base64 -w 0 )".json"
 done
