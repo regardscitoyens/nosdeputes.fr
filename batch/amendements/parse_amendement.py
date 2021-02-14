05af316f4e3cd5c7d07402b6bf4d389bb990ee46
RouxRC <b.ooghe@gmail.com>
2021-02-14 01:37:25 +0100

    fix dates {} from OpenData

--- batch/amendements/parse_amendement.py
+++ batch/amendements/parse_amendement.py
@@ -90,7 +90,7 @@ try:
     else:
         sort = data['cycleDeVie']['sort'] or data['cycleDeVie']['etatDesTraitements']['sousEtat'].get('libelle', None) or data['cycleDeVie']['etatDesTraitements']['etat'].get('libelle', '')
     amd['sort'] = extractSort(sort)
-    amd['date'] = data['cycleDeVie']['dateDepot']
+    amd['date'] = data['cycleDeVie']['dateDepot'] or ""
 
     amd['source'] = htmlurl
     organe = data['identification']['prefixeOrganeExamen']
