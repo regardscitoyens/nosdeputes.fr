e605b95e6b115c2e73bc0c869e95a538e4139deb
Tangui Morlier <tangui@tangui.eu.org>
2021-09-19 23:13:46 +0200

    Permière version d'un parseur basé sur les xml opendata

---
+++ batch/hemicycle/parse_hemicycle.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+import sys
+import bs4
+import json
+
+def xml2json(s):
+    global timestamp
+    timestamp = 0
+    soup = bs4.BeautifulSoup(s, features="lxml")
+    intervention_vierge = {"intervenant": "", "contexte": ""}
+    intervention_vierge["source"] =  "https://www.assemblee-nationale.fr/dyn/15/comptes-rendus/seance/"+soup.uid.string
+    m = soup.metadonnees
+    dateseance = str(m.dateseance.string)
+    intervention_vierge["date"] = "%04d-%02d-%02d" % (int(dateseance[0:4]), int(dateseance[4:6]), int(dateseance[6:8]))
+    intervention_vierge["heure"] = "%02d:%02d" % (int(dateseance[8:10]), int(dateseance[10:12]))
+    intervention_vierge["session"] = str(m.session.string)[-9:].replace('-', '')
+    contextes = ['']
+    numeros_lois = None
+    intervenant2fonction = {}
+    for p in soup.find_all(['paragraphe', 'point']):
+        #Gestion des titres/contextes et numéros de loi
+        if p.name == "point" and p.texte.string and p['nivpoint']:
+            contextes = contextes[:int(p['nivpoint']) -1 ]
+            if not contextes:
+                contextes = []
+            contextes.append(p.texte.string)
+        if p['valeur'] and p['valeur'][0:9] == ' (n[[o]] ':
+            numeros_lois = p['valeur'][9:-1].replace(' ', '')
+        #Gestion des interventions
+        intervention = intervention_vierge.copy()
+        intervention["contexte"] = " > ".join(contextes)
+        if numeros_lois:
+            intervention['numeros_loi'] = numeros_lois
+        intervention["source"] += "#"+p['id_syceron']
+        if len(p.orateurs):
+            intervention["intervenant"] = p.orateurs.orateur.nom.get_text()
+            if p['id_mandat'] and p['id_mandat'] != "-1":
+                intervention["intervenant_url"] = "https://www2.assemblee-nationale.fr/deputes/fiche/OMC_"+p['id_acteur']
+            if p.orateurs.orateur.qualite and p.orateurs.orateur.qualite.string:
+                intervention['fonction'] = p.orateurs.orateur.qualite.get_text()
+                if not intervenant2fonction.get(intervention["intervenant"]) and intervention['fonction']:
+                    intervenant2fonction[intervention["intervenant"]] = intervention['fonction']
+            elif intervention["intervenant"] == "Mme la présidente":
+                intervention['fonction'] = "présidente"
+            elif intervention["intervenant"] == "M le président":
+                intervention['fonction'] = "président"
+            else:
+                intervention['fonction'] = intervenant2fonction.get(intervention["intervenant"], "")
+                    
+        texte = "<p>"
+        isdidascalie = False
+        for t in p.texte.childGenerator():
+            t_string = str(t.string)
+            if isinstance(t,  bs4.element.NavigableString):
+                texte += t.string
+            elif t.name == 'br':
+                texte += "</p><p>"
+            #Cas des didascalies
+            elif t.name == 'italique' and (t_string[0] == '(' or isdidascalie):
+                isdidascalie = True
+                intervention["intervention"] = texte+"</p>"
+                printintervention(intervention)
+                didascalie = intervention_vierge.copy()
+                didascalie['intervention'] = "<p>"+t.get_text()+"</p>"
+                didascalie['intervention'] = didascalie['intervention'].replace('(', '').replace(')', '')
+                printintervention(didascalie)
+                if t_string[-1] == ')':
+                    isdidascalie = False
+                texte = "<p>"
+            elif t.string and t_string != '\n':
+                texte += t.string
+            texte = texte.replace('\n', '')
+        texte += "</p>"
+        intervention["intervention"] = texte            
+        printintervention(intervention)
+
+def printintervention(i):
+    global timestamp
+    if i['intervention'] == '<p></p>' or i['intervention'] == '<p> </p>':
+        return
+    timestamp += 10
+    i['timestamp'] = timestamp
+    print(json.dumps(i))
+
+content_file = sys.argv[1]
+with open(content_file, encoding='utf-8') as f:
+    xml2json(f.read())
