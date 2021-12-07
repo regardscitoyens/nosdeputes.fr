91f7ec450c1fa8e4f524e079f3366fdb5842b384
Tangui Morlier <tangui@tangui.eu.org>
2021-12-07 22:16:08 +0100

    Meilleure gestion des didascalies du début et de la fin

--- batch/commission/parse_commission.py
+++ batch/commission/parse_commission.py
@@ -130,8 +130,11 @@ def html2json(s):
             continue;
         p_str = str(p)
         p_str = p_str.replace('\xa0', ' ')
-        if p_str.find('<p>—  1') == 0:
+        if (p_str.find('<p>–') == 0 or p_str.find('<p><b>—') == 0 or p_str.find('<p>—') == 0 or p_str.find('<p><b>—') == 0) and (p_str.find('–<') > 0 or p_str.find('—<') > 0):
             continue
+        if p_str.find('<p>*</p>') == 0 :
+            if (intervenant):
+                new_intervention()
         if p_str.find('<i>(') > 0 and p_str.find(')</i>') > 0 :
             didascalie = re.findall(r'(.*)(<i>\([^)]*\)</i>)( *.? *</p>)', p_str)
             if(didascalie):
@@ -248,6 +251,8 @@ def new_intervention():
     intervention = intervention.replace('<h3></h3>', '')
     intervention = intervention.replace('<p></p>', '')
     intervention = intervention.replace('<p> </p>', '')
+    intervention = re.sub(r'([^> ])<b>', r'\1 <b>', intervention)
+    intervention = re.sub(r'</b>([^< \.])', r'</b> \1', intervention)
     [intervenant, fonction] = getIntervenantFonction(intervenant)
     timestamp += 10
     curtimestamp = timestamp
