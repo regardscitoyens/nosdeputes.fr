afa323058bea6d23344f4cfecce3eee797a0d46d
RouxRC <b.ooghe@gmail.com>
2022-03-07 10:13:04 +0100

    warnings in stderr

--- batch/commission/parse_commission.py
+++ batch/commission/parse_commission.py
@@ -158,7 +158,7 @@ def html2json(s):
     try:
         p_tags = soup.find(class_="assnatSection2").find_all(['p', 'h1', 'h2', 'h3', 'h3', 'table'], recursive=False)
     except AttributeError:
-        print("ERROR: "+ sys.argv[1]+" n'a pas de section assnatSection2 permettant d'identifier le corps du compte-rendu. Merci de l'ajouter à la main")
+        print("ERROR: "+ sys.argv[1]+" n'a pas de section assnatSection2 permettant d'identifier le corps du compte-rendu. Merci de l'ajouter à la main", file=sys.stderr)
         exit(2)
 
     intervention = ''
