28d90bc21ed5ed7a039ab7f6b9dfdaef98e4f679
RouxRC <b.ooghe@gmail.com>
2022-04-19 00:28:42 +0200

    cleanup bad tags

--- lib/task/tagSeanceTask.class.php
+++ lib/task/tagSeanceTask.class.php
@@ -14,6 +14,7 @@ class tagSeanceTask extends sfBaseTask
   protected function wordize($interventions, $excludeS = 0, $minsize = 1) {
     $words = array();
     foreach($interventions as $i) {
+      $i = preg_replace('/\x91/', '', $i);
       $i = preg_replace('/<\/?[a-z][^>]+>/i', '', $i);
       $i = preg_replace('/\([^\)]+\)/', '', $i);
       $i = preg_replace('/&#339;/', 'oe', $i['intervention']);
