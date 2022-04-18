aa130c04c3fdb02794ee191f0192dd28e63f163c
RouxRC <b.ooghe@gmail.com>
2022-04-18 20:05:53 +0200

    fix check missing hémicycles

--- batch/hemicycle/check_missing.sh
+++ batch/hemicycle/check_missing.sh
@@ -10,33 +10,39 @@ function download {
 }
 
 echo "Downloading Débats from OpenData AN..."
-rm -f SyceronBrut.xml*
-wget -q http://data.assemblee-nationale.fr/static/openData/repository/VP/syceronbrut/SyceronBrut.xml.zip -O SyceronBrut.xml.zip
+rm -f xml
+wget -q https://data.assemblee-nationale.fr/static/openData/repository/$LEGISLATURE/vp/syceronbrut/syseron.xml.zip -O SyceronBrut.xml.zip
 unzip SyceronBrut.xml.zip > /dev/null
 echo "Extracting list of Séances from OpenData AN..."
-less SyceronBrut.xml                                                |
-  sed -r "s/(<DateSeance>)/\n\1/g"                                  |
-  grep '<DateSeance>'                                               |
-  sed -r 's/<DateSeance>(....)(..)(..)(..)(..).*$/\1-\2-\3 \4:\5/'  |
+cat xml/compteRendu/*                                               |
+  grep '<dateSeance>'                                               |
+  sed -r 's/^.*<dateSeance>(....)(..)(..)(..)(..).*$/\1-\2-\3 \4:\5/'  |
   sort -n > all_seances_dates_opendataAN
-
 rm -f SyceronBrut.xml*
 
+
 echo "Downloading list of Séances from AN website..."
-download "http://www.assemblee-nationale.fr/$LEGISLATURE/debats/" > all_seances_lists_AN.tmp
-grep '/'$LEGISLATURE'/cri/[^\"]\+\.asp\"' all_seances_lists_AN.tmp    |
-  sed 's|^.*href="|'"$ANroot"'|'                                        |
-  sed 's/\.asp".*$/.asp/' > all_seances_urls_AN.tmp
-grep '/'$LEGISLATURE'/cri/[^\"\.]\+\"' all_seances_lists_AN.tmp |
-  sed 's|^.*href="|'"$ANroot"'|'                                |
-  sed 's|/".*$|/|'                                              |
-  while read listUrl; do
-    download "$listUrl" | grep 'class="seance".*href="[^"]\+\.asp"' |
-      iconv -f iso-8859-15 -t utf-8                                 |
-      sed 's|^.*href="|'"$listUrl"'|'                               |
-      sed 's/\.asp".*$/.asp/' >> all_seances_urls_AN.tmp
-  done
-sort -u all_seances_urls_AN.tmp > all_seances_urls_AN
+rm -f all_seances_searchAN.tmp
+# Recherche de "de" filtrée sur les Compte-rendus depuis https://www2.assemblee-nationale.fr/recherche/resultats_recherche
+CRURL="https://www2.assemblee-nationale.fr/recherche/resultats_recherche/(tri)/date/(legislature)/15/(query)/eyJxIjoidHlwZURvY3VtZW50OlwiY29tcHRlIHJlbmR1XCIgYW5kIGNvbnRlbnU6ZGUiLCJyb3dzIjoxMCwic3RhcnQiOjAsInd0IjoicGhwIiwiaGwiOiJmYWxzZSIsImZsIjoic2NvcmUsdXJsLHRpdHJlLHVybERvc3NpZXJMZWdpc2xhdGlmLHRpdHJlRG9zc2llckxlZ2lzbGF0aWYsdGV4dGVRdWVzdGlvbix0eXBlRG9jdW1lbnQsc3NUeXBlRG9jdW1lbnQscnVicmlxdWUsdGV0ZUFuYWx5c2UsbW90c0NsZXMsYXV0ZXVyLGRhdGVEZXBvdCxzaWduYXRhaXJlc0FtZW5kZW1lbnQsZGVzaWduYXRpb25BcnRpY2xlLHNvbW1haXJlLHNvcnQifQ=="
+while [ ! -z "$CRURL" ]; do
+  download "$CRURL" > all_seances_searchAN_list.tmp
+  cat all_seances_searchAN_list.tmp |
+    tr "\t\n" " "               |
+    sed 's/<d/\n<d/g'           |
+    grep 'Accédez au document'  |
+    sed 's/^.*https\?/http/'    |
+    sed 's/\s*">.*$//'          |
+    grep '/cri/\(20\|NaN\)'     |
+    sed 's|dyn/comptes-rendus/seance/redirect?url=/||'    |
+    grep -v '/NaN' >> all_seances_searchAN.tmp
+  CRURL=$(grep '"text">Suivant' all_seances_searchAN_list.tmp |
+            head -1                                       |
+            sed -r 's|^.*href="([^"]+)".*$|https://www2.assemblee-nationale.fr\1|')
+done
+sort -u all_seances_searchAN.tmp > all_seances_searchAN
+rm -f all_seances_searchAN*.tmp
+
 
 echo "Extracting list of Séances from NosDéputés..."
 echo 'SELECT s.date, s.moment, i.source
@@ -50,27 +56,28 @@ echo 'SELECT s.date, s.moment, i.source
 awk -F "\t" '{print $1" "$2}' all_seances_ND.tmp > all_seances_dates_ND
 awk -F "\t" '{print $3}' all_seances_ND.tmp |
   sed 's/#.*$//'                            |
-  sort > all_seances_urls_ND
+  sort > all_seances_ND
 
 echo "Analysing diff..."
-extra=$(diff all_seances_urls_AN all_seances_urls_ND | grep "^>" | wc -l)
+extra=$(diff all_seances_searchAN all_seances_ND | grep "^>" | wc -l)
 if [ $extra -gt 0 ]; then
   echo "- NosDéputés has $extra Séances not found on AN's website (how is that possible?):"
-  diff all_seances_urls_AN all_seances_urls_ND  |
-    grep "^>"                                   |
+  diff all_seances_searchAN all_seances_ND  |
+    grep "^>"                               |
     sed 's/^> //'
   echo
 fi
 
-missing=$(diff all_seances_urls_AN all_seances_urls_ND | grep "^<" | wc -l)
+missing=$(diff all_seances_searchAN all_seances_ND | grep "^<" | wc -l)
 if [ $missing -gt 0 ]; then
   echo "There are $missing Séances missing, reloading them:"
-  diff all_seances_urls_AN all_seances_urls_ND  |
+  diff all_seances_searchAN all_seances_ND      |
     grep "^<"                                   |
     sed 's/^< //'                               |
     while read SEurl; do
       SEfile=$(echo "$SEurl" | sed 's|/|_|g')
-      perl download_one.pl "$SEurl" 2>/dev/null && perl parse_hemicycle.pl "html/$SEfile" > "out/$SEfile"
+      echo $SEurl
+      #perl download_one.pl "$SEurl" 2>/dev/null && perl parse_hemicycle.pl "html/$SEfile" > "out/$SEfile"
     done
   echo 'All missing Séances reloaded and parsed, run "php symfony load:Hemicycle" to complete'
 fi
