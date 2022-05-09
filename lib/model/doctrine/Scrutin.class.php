df6c6d5235442b81ffc5d1e480176d1bccdf5e6d
RouxRC <b.ooghe@gmail.com>
2022-05-09 17:34:09 +0200

    fix unfound scrutin

--- lib/model/doctrine/Scrutin.class.php
+++ lib/model/doctrine/Scrutin.class.php
@@ -55,10 +55,10 @@ class Scrutin extends BaseScrutin
       $text = $inter->intervention;
       if (!$source) $source = $inter->source;
       $mv = preg_match_all('/nombre de votants(?:<\/td><td>|[,\s]*)(\d+)/i', $text, $match_votant);
-      $mp = preg_match_all('/pour l\'(?:adoption|approbation)(?:<\/td><td>|[,\s]*)(\d+)\D/i', $text, $match_pour);
+      $mp = preg_match_all('/pour l(?:\'adoption|\'approbation|la suspension)(?:<\/td><td>|[,\s]*)(\d+)\D/i', $text, $match_pour);
       $mc = preg_match_all('/contre(?:<\/td><td>|[,\s]*)(\d+)/i', $text, $match_contre);
 
-      if (preg_match("/Majorité requise pour l'adoption/", $text) && $mp != 0 && intval(end($match_pour[1])) == $this->nombre_votants && intval(end($match_pour[1])) == $this->nombre_pours) {
+      if (preg_match("/Majorité (absolue|requise pour l'adoption)/", $text) && $mp != 0 && intval(end($match_pour[1])) == $this->nombre_votants && intval(end($match_pour[1])) == $this->nombre_pours) {
         $found = TRUE;
         $inter->addTag("scrutin:numero={$this->numero}");
         $inter->save();
