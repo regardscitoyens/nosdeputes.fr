d826395bab01bc5a75f20064d93c64ee202e53ba
RouxRC <b.ooghe@gmail.com>
2022-09-26 17:13:17 +0200

    make wrapper crons write in config dir + better date format

--- bin/wrapper_crons.sh
+++ bin/wrapper_crons.sh
@@ -3,29 +3,29 @@
 . $(dirname $0)/db.inc
 
 command=`echo $2 | sed 's#^.*\/\([^\/]\+\)$#\\1#'`
-tmpout=cron-$command-$LEGISLATURE-`date +%d%m%Y-%HH%Mm%S`
+tmpout=cron-$command-$LEGISLATURE-`date +%y%m%d-%HH%Mm%S`
 tmpoutput=$tmpout
 ct=0
-while [ -f /tmp/$tmpoutput ]; do
+while [ -f $PATH_LOGS_CRON/$tmpoutput ]; do
   tmpoutput=$tmpout-$ct
   ct=$(($ct + 1))
 done
 sleep 5
 
-"$@" >> /tmp/$tmpoutput 2>&1
+"$@" >> $PATH_LOGS_CRON/$tmpoutput 2>&1
 
 if test "$PADBOT" && test "$PADURL" ; then
-echo > /tmp/$tmpoutput.date
-date >> /tmp/$tmpoutput.date
-echo "---------------------" >> /tmp/$tmpoutput.date
-echo >> /tmp/$tmpoutput.date
-cat /tmp/$tmpoutput.date /tmp/$tmpoutput | grep -v '^==' | $PADBOT $PADURL write
-rm /tmp/$tmpoutput.date
+echo > $PATH_LOGS_CRON/$tmpoutput.date
+date >> $PATH_LOGS_CRON/$tmpoutput.date
+echo "---------------------" >> $PATH_LOGS_CRON/$tmpoutput.date
+echo >> $PATH_LOGS_CRON/$tmpoutput.date
+cat $PATH_LOGS_CRON/$tmpoutput.date $PATH_LOGS_CRON/$tmpoutput | grep -v '^==' | $PADBOT $PADURL write
+rm $PATH_LOGS_CRON/$tmpoutput.date
 fi
 
-cat /tmp/$tmpoutput
-outsize=`ls -l /tmp/$tmpoutput | tail -n 1 | awk '{print $5}'`
+cat $PATH_LOGS_CRON/$tmpoutput
+outsize=`ls -l $PATH_LOGS_CRON/$tmpoutput | tail -n 1 | awk '{print $5}'`
 if [ "$outsize" -eq 0 ]; then
-  rm -f /tmp/$tmpoutput
+  rm -f $PATH_LOGS_CRON/$tmpoutput
 fi
 
