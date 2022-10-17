13b22b0077f275485d1b878d4abbc7fdb5411302
RouxRC <b.ooghe@gmail.com>
2022-10-17 03:03:48 +0200

    update compute_one amdmt

--- batch/amendements/compute_one.sh
+++ batch/amendements/compute_one.sh
@@ -3,8 +3,8 @@
 mkdir -p html json loaded
 
 AMDTFILE=$(perl download_one.pl $1 | awk '{print $1}')
-perl cut_amdmt.pl html/$AMDTFILE | python clean_subjects_amdmts.py > json/$AMDTFILE
-if test -e loaded/$AMDTFILE && ! diff {json,loaded}/$AMDTFILE | grep . > /dev/null; then
-  rm -f json/$AMDTFILE
-fi
+python parse_amendement.py html/$AMDTFILE > json/$AMDTFILE
+#if test -e loaded/$AMDTFILE && ! diff {json,loaded}/$AMDTFILE | grep . > /dev/null; then
+#  rm -f json/$AMDTFILE
+#fi
 
