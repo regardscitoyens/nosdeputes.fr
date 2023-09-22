cfa52f43e1045ecc54c8cab88a1cd4606af718e1
RouxRC <b.ooghe@gmail.com>
2023-09-22 16:45:54 +0200

    lighter verbosity bis

--- batch/documents/compute_one_from_id_opendata.sh
+++ batch/documents/compute_one_from_id_opendata.sh
@@ -9,7 +9,7 @@ mkdir -p opendata/html opendata/document
 wget -q -N -O opendata/html/$1.html "http://www.assemblee-nationale.fr/dyn/opendata/$1.html"
 wget -q -N -O opendata/document/$1.json "http://www.assemblee-nationale.fr/dyn/opendata/$1.json"
 
-if ! test -s opendata/document/$1.json || ! python -m json.tool opendata/document/$1.json; then
+if ! test -s opendata/document/$1.json || ! python -m json.tool opendata/document/$1.json > /dev/null; then
   echo "WARNING http://www.assemblee-nationale.fr/dyn/opendata/$1.html seems unavailable"
 else
   python3 parse_opendata_documents.py opendata/document/$1.json > out/$1.json
