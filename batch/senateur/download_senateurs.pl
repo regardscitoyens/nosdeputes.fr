2f006d7c4be980eed36baee59d3df6b0c8bd69b6
Tangui Morlier <tangui@tangui.eu.org>
2024-03-18 22:42:04 +0100

    Base le parsing sur archive.org

--- batch/senateur/download_senateurs.pl
+++ batch/senateur/download_senateurs.pl
@@ -9,14 +9,14 @@ $|=1;
 
 $verbose = shift || 0;
 %done = ();
-$done{'http://www.senat.fr/senateur/ginesta_georges15447f.html'} = 1;
-$done{'http://www.senat.fr/senateur/dubois_emile000322.html'} = 1;
+$done{'/senateur/ginesta_georges15447f.html'} = 1;
+$done{'/senateur/dubois_emile000322.html'} = 1;
 sub download_fiche {
 	$uri = shift;
 	return if ($done{$uri});
 	$done{$uri} = 1;
   	eval {
-		$a->get("http://senat.fr".$uri);
+		$a->get("https://web.archive.org/web/20230101000000/https://www.senat.fr".$uri);
 		1;
 	} or return "ERROR: "."$@";
 	$file = uri_escape($a->uri());
@@ -50,13 +50,15 @@ sub find_senateurs {
 	$content = $a->content;
 	$p = HTML::TokeParser->new(\$content);
 	while ($t = $p->get_tag('a')) {
-	    if ($t->[1]{href} =~ /\/(senateur|senfic)\//) {
+        $href = $t->[1]{href};
+        $href =~ s/.*www.senat.fr//;
+	    if ($href =~ /\/(senateur|senfic)\//) {
 		$i = 3;
 		while ($i) {
 				$i--;
-				if (download_fiche($t->[1]{href}) =~ /^ERROR: /) {
+				if (download_fiche($href) =~ /^ERROR: /) {
 				    if (!$i) {
-					print STDERR "Error downloading ".$t->[1]{href}."\n";
+					print STDERR "Error downloading ".$href."\n";
 				    }
 				    sleep 1;
 				}else{
@@ -67,23 +69,23 @@ sub find_senateurs {
 	}
 }
 
-find_senateurs("http://www.senat.fr/senateurs/senatl.html");
-find_senateurs("http://www.senat.fr/senateurs/news.html");
-#find_senateurs("http://www.senat.fr/senateurs/news_2010-2011.html");
+find_senateurs("https://web.archive.org/web/20230101000000/https://www.senat.fr/senateurs/senatl.html");
+find_senateurs("https://web.archive.org/web/20230101000000/https://www.senat.fr/senateurs/news.html");
+#find_senateurs("https://web.archive.org/web/20230101000000/https://www.senat.fr/senateurs/news_2010-2011.html");
 
 #Enable below on first load
-#find_senateurs("http://www.senat.fr/senateurs/news_2009-2010.html");
-#find_senateurs("http://www.senat.fr/senateurs/news_2008-2009.html");
-#find_senateurs("http://www.senat.fr/senateurs/news_2007-2008.html");
-#find_senateurs("http://www.senat.fr/senateurs/news_2006-2007.html");
-#find_senateurs("http://www.senat.fr/senateurs/news_2005-2006.html");
-#find_senateurs("http://www.senat.fr/senateurs/news_2004-2005.html");
-#find_senateurs("http://www.senat.fr/anciens-senateurs-5eme-republique/senatl.html");
-find_senateurs("http://www.senat.fr/anciens-senateurs-5eme-republique/ump.html");
-find_senateurs("http://www.senat.fr/anciens-senateurs-5eme-republique/soc.html");
-find_senateurs("http://www.senat.fr/anciens-senateurs-5eme-republique/uc.html");
-find_senateurs("http://www.senat.fr/anciens-senateurs-5eme-republique/rdse.html");
-find_senateurs("http://www.senat.fr/anciens-senateurs-5eme-republique/crc.html");
-find_senateurs("http://www.senat.fr/anciens-senateurs-5eme-republique/ni.html");
-find_senateurs("http://www.senat.fr/anciens-senateurs-5eme-republique/lrem.html");
-find_senateurs("http://www.senat.fr/anciens-senateurs-5eme-republique/rtli.html");
+#find_senateurs("https://web.archive.org/web/20230101000000/https://www.senat.fr/senateurs/news_2009-2010.html");
+#find_senateurs("https://web.archive.org/web/20230101000000/https://www.senat.fr/senateurs/news_2008-2009.html");
+#find_senateurs("https://web.archive.org/web/20230101000000/https://www.senat.fr/senateurs/news_2007-2008.html");
+#find_senateurs("https://web.archive.org/web/20230101000000/https://www.senat.fr/senateurs/news_2006-2007.html");
+#find_senateurs("https://web.archive.org/web/20230101000000/https://www.senat.fr/senateurs/news_2005-2006.html");
+#find_senateurs("https://web.archive.org/web/20230101000000/https://www.senat.fr/senateurs/news_2004-2005.html");
+#find_senateurs("https://web.archive.org/web/20230101000000/https://www.senat.fr/anciens-senateurs-5eme-republique/senatl.html");
+find_senateurs("https://web.archive.org/web/20230101000000/https://www.senat.fr/anciens-senateurs-5eme-republique/ump.html");
+find_senateurs("https://web.archive.org/web/20230101000000/https://www.senat.fr/anciens-senateurs-5eme-republique/soc.html");
+find_senateurs("https://web.archive.org/web/20230101000000/https://www.senat.fr/anciens-senateurs-5eme-republique/uc.html");
+find_senateurs("https://web.archive.org/web/20230101000000/https://www.senat.fr/anciens-senateurs-5eme-republique/rdse.html");
+find_senateurs("https://web.archive.org/web/20230101000000/https://www.senat.fr/anciens-senateurs-5eme-republique/crc.html");
+find_senateurs("https://web.archive.org/web/20230101000000/https://www.senat.fr/anciens-senateurs-5eme-republique/ni.html");
+find_senateurs("https://web.archive.org/web/20230101000000/https://www.senat.fr/anciens-senateurs-5eme-republique/lrem.html");
+find_senateurs("https://web.archive.org/web/20230101000000/https://www.senat.fr/anciens-senateurs-5eme-republique/rtli.html");
